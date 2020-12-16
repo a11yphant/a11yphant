939166a22023899e9b9bae5b8f0b1461efc76137
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-16 16:48:30 +0100

    feat(api): Add service to dummy module

--- services/api/src/hello-world/hello-world.module.ts
+++ services/api/src/hello-world/hello-world.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldResolver } from './hello-world.resolver';
+import { HelloWorldService } from './hello-world.service';
 
 @Module({
-  providers: [HelloWorldResolver],
+  providers: [HelloWorldResolver, HelloWorldService],
 })
 export class HelloWorldModule {}
