1ddd3721251ddea48917e2ead5e6991800b7c297
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-16 16:01:31 +0100

    chore(api): Add GraphQL to NestJS project

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [() => ({ debug: process.env.NODE_ENV !== "production" })],
+    }),
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        debug: configService.get<boolean>("debug"),
+        playground: configService.get<boolean>("debug"),
+        autoSchemaFile: "schema.gql",
+        context: ({ req }) => ({ ...req }),
+        cors: {
+          credentials: true,
+          origin: true,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
