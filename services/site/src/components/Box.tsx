531f8f0c496b3d2fe8bd4d939ad10a6891207202
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-18 15:34:36 +0100

    chore(site): Move pages and components to /src

---
+++ services/site/src/components/Box.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Box: React.FunctionComponent = ({children}) => {
+    return <div>
+        {children}
+    </div>
+}
+
+export default Box
---
+++ services/site/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Box from "app/components/Box";
+
+const Home = () => {
+    return <Box> <h1>A11y Challenges</h1> </Box>
+}
+
+export default Home
