a3d7ce9bd2cbcde830753c9cd859942a708c3015
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2020-12-19 15:42:04 +0100

    style(api): run linter

--- services/api/src/hello-world/hello-world.service.spec.ts
+++ services/api/src/hello-world/hello-world.service.spec.ts
@@ -1,8 +1,8 @@
-import { HelloWorldService } from "./hello-world.service"
+import { HelloWorldService } from './hello-world.service';
 
 describe('hello world', () => {
-    it('returns hello world items', () => {
-        const service = new HelloWorldService();
-        expect(service.findAll()).toBeTruthy();
-    })
+  it('returns hello world items', () => {
+    const service = new HelloWorldService();
+    expect(service.findAll()).toBeTruthy();
+  });
 });
