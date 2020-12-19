5f7a0dd55b95dcdc548854af70c617b866a36e22
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-19 14:22:24 +0100

    feature(site): Add tailwind

--- services/site/src/components/Box.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Box: React.FunctionComponent = ({children}) => {
-    return <div>
-        {children}
-    </div>
-}
-
-export default Box
--- services/site/src/pages/index.tsx
+++ services/site/src/pages/index.tsx
@@ -1,8 +1,13 @@
-import React from "react"
-import Box from "app/components/Box";
+import React from "react";
+import Card from "app/components/Card";
 
 const Home = () => {
-    return <Box> <h1>A11y Challenges</h1> </Box>
+    return <main className="flex justify-center items-center w-screen h-screen">
+        <Card heading={<h1>A11y Challenges</h1>}>
+            <p>Welcome to a11y-challenges.cool! This is a project created by Michael Brandstätter, Thomas Dax,
+                Daniela Kubesch and Luca Pircher during their MMT Master degree program at FH Salzburg</p>
+        </Card>
+    </main>
 }
 
 export default Home
