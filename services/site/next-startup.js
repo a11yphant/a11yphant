82983663e8ee835b6ed952e20e4e5676f7fe89cb
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-19 16:41:49 +0100

    chore(site): Add script for starting next with port from env

---
+++ services/site/next-startup.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const existingCommands = ['start', 'dev', 'build', 'telemetry']
+const errorMessage = 'No or wrong command. Options: start | dev | build | telemetry'
+
+if(!(process.argv.length >= 3)) {
+    throw new Error(errorMessage)
+}
+
+const [,, command, ...args] = process.argv;
+const port = process.env.SITE_PORT || 3001;
+
+if(!existingCommands.includes(command)) {
+    throw new Error(errorMessage)
+}
+
+let cli;
+switch (command) {
+    case 'start':
+        cli = require('next/dist/cli/next-start')
+        cli.nextStart([
+            '-p', port,
+            ...args
+        ])
+        break;
+    case 'dev':
+        cli = require('next/dist/cli/next-dev')
+        cli.nextDev([
+            '-p', port,
+            ...args
+        ])
+        break;
+    case 'build':
+        cli = require('next/dist/cli/next-build')
+        cli.nextBuild([
+            '-p', port,
+            ...args
+        ])
+        break;
+    case 'telemetry':
+        cli = require('next/dist/cli/next-telemetry')
+        cli.nextTelemetry([
+            '-p', port,
+            ...args
+        ])
+        break;
+}
