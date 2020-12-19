3a8eddd4827b66d166a0224f1d408588fc53c08b
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-19 19:27:19 +0100

    style(api): Fix linting errors

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
