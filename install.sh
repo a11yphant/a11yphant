4b2786df315c79a1006aaca29b37bf48beb14766
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2020-12-19 20:01:18 +0100

    chore(root): Add npm and nvm commands to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
+source ~/.nvm/nvm.sh
 
 # jump to project root
 cd $(dirname $0)
+
+# use correct npm and install dependencies
+nvm install
+nvm use
+npm install
 
 # create .env from .env.example
 if [ ! -f ./.env ]; then
