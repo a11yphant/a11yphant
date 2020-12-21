cd8572b410d1b0a109c739a9d35e9ff72f0a0447
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2020-12-21 15:24:13 +0100

    feat(submission-checker): add dummy controller to initiate evaluation of snippet

--- services/submission-checker/src/app.module.ts
+++ services/submission-checker/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BrowserService } from './browser.service';
+import { CheckSubmissionController } from './check-submission.controller';
 import { CheckSubmissionService } from './check-submission.service';
 import submissionRenderer from './config/submission-renderer';
 import { RendererModule } from './renderer/renderer.module';
@@ -13,7 +14,9 @@ import { WebdriverFactory } from './webdriver.factory';
     }),
     RendererModule
   ],
-  controllers: [],
+  controllers: [
+    CheckSubmissionController
+  ],
   providers: [
     BrowserService,
     CheckSubmissionService,
