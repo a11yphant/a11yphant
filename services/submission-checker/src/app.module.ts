b76162dd6484ffdf2ca701d3f23d214eb1fa6645
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2020-12-22 09:25:16 +0100

    feat(submission-checker): setup config module

--- services/submission-checker/src/app.module.ts
+++ services/submission-checker/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
