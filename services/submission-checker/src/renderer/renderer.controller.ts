816fd7c66794034a13996f067b8ca0f479ac1c41
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2020-12-22 11:48:27 +0100

    feat(submission-checker): remove unused param in renderer controller

--- services/submission-checker/src/renderer/renderer.controller.ts
+++ services/submission-checker/src/renderer/renderer.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('render')
 export class RendererController {
   @Get(':id')
-  show(@Res() response: Response, @Param('id') id: string) {
+  show(@Res() response: Response) {
     response.send(`
             <!DOCTYPE html>
             <html lang="en">
