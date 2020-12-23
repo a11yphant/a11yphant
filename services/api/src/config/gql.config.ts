881a7362bd652cbf8fee18f10b225a6286891c6a
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2020-12-23 10:48:27 +0100

    feat(api): allow activating schema introspection via configuration

--- services/api/src/config/gql.config.ts
+++ services/api/src/config/gql.config.ts
@@ -4,4 +4,5 @@ export default registerAs("gql", () => ({
   debug: Boolean(+process.env.API_GRAPHQL_DEBUG) || false,
   playground: Boolean(+process.env.API_GRAPHQL_PLAYGROUND) || false,
   inMemorySchema: process.env.NODE_ENV !== "development",
+  schemaIntrospection: Boolean(+process.env.API_GRAPHQL_SCHEMA_INTROSPECTION) || false,
 }));
