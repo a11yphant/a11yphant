b40fdc376b997e19b40973e45406a4d1e278195e
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-01 16:33:20 +0100

    feat(database-migrations): use correct handler name

--- terraform/database_migration.tf
+++ terraform/database_migration.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "database_migration" {
    s3_key    = aws_s3_bucket_object.database_migration_code_zip.id
    source_code_hash = data.external.database_migration_code_zip.result.hash
 
-   handler = "entrypoint.handle"
+   handler = "entrypoint.handler"
    runtime = "nodejs12.x"
    timeout = 60
 
@@ -81,4 +81,24 @@ EOF
 resource "aws_iam_role_policy_attachment" "database_migration_lambda_logs" {
   role       = aws_iam_role.database_migration_role.name
   policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_lambda_permission" "allow_prisma_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.database_migration.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.prisma.arn
+}
+
+resource "aws_s3_bucket_notification" "prisma_changed_notification" {
+  bucket = aws_s3_bucket.prisma.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.database_migration.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".zip"
+  }
+
+  depends_on = [aws_lambda_permission.allow_prisma_bucket]
 }
