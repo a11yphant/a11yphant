d71df774c07928fbf957f8459dc5245268f2bcf4
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-01 14:28:43 +0100

    feat(database-migration): read bucket and object from event

--- services/database-migration/entrypoint.js
+++ services/database-migration/entrypoint.js
@@ -1,13 +1,20 @@
 const { exec } = require("child_process");
 const unzipper = require("unzipper");
-const s3 = require("aws-sdk").S3();
+const aws = require("aws-sdk");
 
-const migrationsBucket = process.env.S3_MIGRATION_BUCKET;
-const migrationZipObjectKey = process.env.S3_MIGRATION_ZIP_OBJECT_KEY;
+const s3 = new aws.S3();
 
-exports.handler = () => {
-  const zipStream = s3.getObject({ Bucket: migrationsBucket, Key: migrationZipObjectKey }).createReadStream();
+exports.handler = (_, event, callback) => {
+  console.log("Starting migration");
+
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const zipStream = s3.getObject({ Bucket: bucket, Key: key }).createReadStream();
+
   zipStream.pipe(unzipper.Extract({ path: "/tmp/prisma" })).on("finish", () => {
+    console.log("Retrieved files from S3");
     exec("node_modules/.bin/prisma migrate --preview-feature --schema /tmp/prisma/schema.prisma");
+    console.log("Migration finished");
+    callback();
   });
 };
