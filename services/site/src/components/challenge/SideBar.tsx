a6e526ff4c9318cd6bdffb626e63710cf6e6a5bd
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-02-02 10:21:01 +0100

    feat(sidebar): add fonts and styling

--- services/site/src/components/challenge/SideBar.tsx
+++ services/site/src/components/challenge/SideBar.tsx
@@ -4,18 +4,39 @@ interface SideBarProps {
 
 interface SideBarSection {
   title: string;
-  content: React.ReactNode;
+  content: SideBarContent;
+}
+
+interface SideBarContent {
+  text: string[];
+  tldr: string;
+  requirements: string[];
 }
 
 const SideBar: React.FunctionComponent<SideBarProps> = ({ sections }) => {
   return (
-    <aside className="h-screen w-1/5 border-2 rounded-lg border-primary m-4 relative box-border">
-      <button className="border-l-2 border-b-2 border-primary p-4 absolute -top-px -right-px box-border">Back</button>
+    <aside className="h-screen w-1/4 border-2 rounded-lg border-primary m-4 px-8 relative box-border">
+      <button className="border-l-2 border-b-2 border-primary p-4 absolute -top-px -right-px box-border text-2xl">Back</button>
       {sections.map((section) => {
         return (
           <div key={section.title}>
-            <h3>{section.title}</h3>
-            <div>{section.content}</div>
+            <h3 className="text-primary font-bold p-4 mb-10 text-center">{section.title}</h3>
+            <div>
+              {section.content.text.map((paragraph) => (
+                <p key={paragraph}>{paragraph}</p>
+              ))}
+              <p className="text-primary font-bold mt-8">{section.content.tldr}</p>
+            </div>
+            <h3 className="text-primary font-bold mt-10 mb-8 text-center">Requirements</h3>
+            <div>
+              <ul>
+                {section.content.requirements.map((requirement) => (
+                  <li key={requirement} className="text-primary my-2">
+                    {requirement}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         );
       })}
