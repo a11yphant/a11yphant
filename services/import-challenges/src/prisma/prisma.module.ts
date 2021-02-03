d55d1f0a92b7cf08b8b751113828e98f4b1bb0df
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-03 16:46:22 +0100

    feat(import-challenges): use DI for logger in prisma module

--- services/import-challenges/src/prisma/prisma.module.ts
+++ services/import-challenges/src/prisma/prisma.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [PrismaService],
+  providers: [PrismaService, Logger],
   exports: [PrismaService],
 })
 export class PrismaModule {}
