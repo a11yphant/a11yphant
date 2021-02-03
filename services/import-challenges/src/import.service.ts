97ca267f71aca9f62fd5291fc083cad257f65e14
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-03 19:50:54 +0100

    refactor(import-challenges): simplify import service

--- services/import-challenges/src/import.service.ts
+++ services/import-challenges/src/import.service.ts
@@ -40,6 +40,18 @@ export class ImportService {
   }
 
   public async importChallenge(challenge: RawChallenge): Promise<void> {
+    await this.upsertChallenge(challenge);
+
+    await this.upsertLevelsForChallenge(challenge.levels, challenge.id);
+
+    for (const level of challenge.levels) {
+      await this.upsertRequirementsForLevel(level.requirements, level.id);
+      await this.upsertResourcesForLevel(level.resources, level.id);
+      await this.upsertHintsForLevel(level.hints, level.id);
+    }
+  }
+
+  private async upsertChallenge(challenge: RawChallenge): Promise<void> {
     await this.prisma.challenge.upsert({
       where: { id: challenge.id },
       create: {
@@ -50,104 +62,85 @@ export class ImportService {
         name: challenge.name,
       },
     });
+  }
 
+  private async upsertLevelsForChallenge(
+    levels,
+    challengeId: string,
+  ): Promise<void> {
     await Promise.all(
-      challenge.levels.map(async (level) => {
+      levels.map(async (level) => {
         await this.prisma.level.upsert({
           where: { id: level.id },
           create: {
             id: level.id,
             tldr: level.tldr,
             instructions: level.instructions,
-            challengeId: challenge.id,
+            challengeId,
           },
           update: {
             tldr: level.tldr,
             instructions: level.instructions,
-            challengeId: challenge.id,
+            challengeId,
           },
         });
       }),
     );
+  }
 
+  private async upsertRequirementsForLevel(
+    requirements,
+    levelId: string,
+  ): Promise<void> {
     await Promise.all(
-      challenge.levels
-        .map((level) => {
-          return {
-            ...level,
-            requirements: level.requirements.map((requirement) => ({
-              ...requirement,
-              levelId: level.id,
-            })),
-          };
-        })
-        .flatMap((level) => level.requirements)
-        .map(async (requirement) => {
-          await this.prisma.requirement.upsert({
-            where: { id: requirement.id },
-            create: {
-              id: requirement.id,
-              title: requirement.title,
-              levelId: requirement.levelId,
-            },
-            update: { title: requirement.title, levelId: requirement.levelId },
-          });
-        }),
+      requirements.map(async (requirement) => {
+        await this.prisma.requirement.upsert({
+          where: { id: requirement.id },
+          create: {
+            id: requirement.id,
+            title: requirement.title,
+            levelId,
+          },
+          update: { title: requirement.title, levelId },
+        });
+      }),
     );
+  }
 
+  private async upsertResourcesForLevel(resources, levelId): Promise<void> {
     await Promise.all(
-      challenge.levels
-        .map((level) => {
-          return {
-            ...level,
-            resources: level.resources.map((hint) => ({
-              ...hint,
-              levelId: level.id,
-            })),
-          };
-        })
-        .flatMap((level) => level.resources)
-        .map(async (resource) => {
-          await this.prisma.resource.upsert({
-            where: { id: resource.id },
-            create: {
-              id: resource.id,
-              title: resource.title,
-              link: resource.link,
-              levelId: resource.levelId,
-            },
-            update: {
-              title: resource.title,
-              link: resource.link,
-              levelId: resource.levelId,
-            },
-          });
-        }),
+      resources.map(async (resource) => {
+        await this.prisma.resource.upsert({
+          where: { id: resource.id },
+          create: {
+            id: resource.id,
+            title: resource.title,
+            link: resource.link,
+            levelId,
+          },
+          update: {
+            title: resource.title,
+            link: resource.link,
+            levelId,
+          },
+        });
+      }),
     );
+  }
 
+  private async upsertHintsForLevel(hints, levelId: string): Promise<void> {
     await Promise.all(
-      challenge.levels
-        .map((level) => {
-          return {
-            ...level,
-            hints: level.hints.map((hint) => ({
-              ...hint,
-              levelId: level.id,
-            })),
-          };
-        })
-        .flatMap((level) => level.hints)
-        .map(async (hint) => {
-          await this.prisma.hint.upsert({
-            where: { id: hint.id },
-            create: {
-              id: hint.id,
-              content: hint.content,
-              levelId: hint.levelId,
-            },
-            update: { content: hint.content, levelId: hint.levelId },
-          });
-        }),
+      hints.map(async (hint) => {
+        await this.prisma.hint.upsert({
+          where: { id: hint.id },
+          create: {
+            id: hint.id,
+            content: hint.content,
+            levelId,
+          },
+          update: { content: hint.content, levelId },
+        });
+      }),
     );
   }
 }
