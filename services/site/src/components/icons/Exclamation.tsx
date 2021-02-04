3fbb05ab4e674edb92c3d43860f8443680a7bf8a
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2021-02-04 17:26:42 +0100

    fix(icons): import React

--- services/site/src/components/icons/Exclamation.tsx
+++ services/site/src/components/icons/Exclamation.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Exclamation = () => (
   <svg className="h-6 w-6 text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
     <path
