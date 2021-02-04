c066ec3e18d2313b9323501052d70cd51569fe8e
Thomas Dax <tdax.mmt-b2017@fh-salzburg.ac.at>
2021-02-04 17:16:45 +0100

    fix(sidebar): add keys to react lists

--- services/site/src/components/challenge/sidebar/Sections/ResourceSection.tsx
+++ services/site/src/components/challenge/sidebar/Sections/ResourceSection.tsx
@@ -14,7 +14,7 @@ const ResourceSection: React.FunctionComponent<ResourceSectionProps> = ({ resour
     <div className="flex-auto overflow-y-auto mt-10 px-8">
       <ul>
         {resources.map((resource) => (
-          <li className="text-primary mb-4">
+          <li key={resource.link} className="text-primary mb-4">
             {resource.label}
             <br />
             <a href={resource.link} target="_blank" className="text-black border-primary border-b-2 break-all">
