13af7c21b825a470f5abaa10c0bf06c6e27604d5
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-05 17:36:21 +0100

    chore(tf): change base_url output to url

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,6 @@ output "site_gateway_url" {
   value = aws_apigatewayv2_api.site_http_api.api_endpoint
 }
 
-output "site_base_url" {
+output "url" {
   value = "https://${aws_cloudfront_distribution.site.domain_name}"
 }
