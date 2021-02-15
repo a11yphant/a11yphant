af623f94af7d98796713e53760f72032123a1edf
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-15 20:16:24 +0100

    feat(setup): add env symlink for prisma package

--- install.sh
+++ install.sh
@@ -28,3 +28,7 @@ ln -s ./../../.env ./services/submission-checker/.env
 # create import-challenges symlinks
 rm -f ./services/import-challenges/.env
 ln -s ./../../.env ./services/import-challenges/.env
+
+# create prisma symlinks
+rm -f ./packages/prisma/.env
+ln -s ./../../.env ./packages/prisma/.env
