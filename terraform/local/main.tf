327fcfd6f390a63c41b096ca100dc7ec5ae00a52
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-02-24 13:38:05 +0100

    feat(tf): add sns topic

--- terraform/local/main.tf
+++ terraform/local/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     s3_force_path_style         = true
+    skip_requesting_account_id  = true
     access_key                  = "mock_access_key"
     secret_key                  = "mock_secret_key"
     region                      = "us-east-1"
@@ -10,4 +11,8 @@ provider "aws" {
         sns     = "http://localhost:4566"
         sqs     = "http://localhost:4566"
     }
+}
+
+module "messaging" {
+  source = "../modules/messaging"
 }
