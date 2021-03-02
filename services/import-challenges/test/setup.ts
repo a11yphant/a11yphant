d2caf0477bcef9d5c2dd2c274be074998b0c0cfe
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-02 15:25:57 +0100

    test(import-challenges): fix env file path

--- services/import-challenges/test/setup.ts
+++ services/import-challenges/test/setup.ts
@@ -2,7 +2,7 @@ import { setupDatabase } from '@a11y-challenges/prisma';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 
-config({ path: resolve(__dirname, '../../.env') });
+config({ path: resolve(__dirname, '../.env') });
 
 export default async function (): Promise<void> {
   await setupDatabase();
