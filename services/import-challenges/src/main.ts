b19793c274782ae3adb7ea3ea1962fd608caab7d
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-06 18:30:28 +0100

    feat(import-challenges): add lambda entry point

--- services/import-challenges/src/main.ts
+++ services/import-challenges/src/main.ts
@@ -1,14 +1,24 @@
-import { Logger } from '@nestjs/common';
+import { INestApplicationContext, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { S3Handler } from 'aws-lambda';
 
 import { AppModule } from './app.module';
 import { ImportService } from './import.service';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap(): Promise<INestApplicationContext | void> {
   const app = await NestFactory.createApplicationContext(AppModule);
   await app.init();
 
+  const config = app.get<ConfigService>(ConfigService);
+  if (config.get<boolean>('import-challenges.is-lambda')) {
+    return app;
+  }
+
+  await importChallenges(app);
+}
+
+async function importChallenges(app: INestApplicationContext): Promise<void> {
   const config = app.get<ConfigService>(ConfigService);
   const importer = app.get<ImportService>(ImportService);
   const logger = new Logger(AppModule.name);
@@ -26,4 +36,19 @@ async function bootstrap(): Promise<void> {
     await app.close();
   }
 }
-bootstrap();
+
+const appPromise = bootstrap();
+
+export const handle: S3Handler = async (event) => {
+  const app = await (appPromise as Promise<INestApplicationContext>);
+  const logger = new Logger(AppModule.name);
+
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, ' '),
+  );
+
+  logger.log(`Loading migrations from S3 (Bucket: ${bucket}, Key: ${key})`);
+
+  await app.close();
+};
