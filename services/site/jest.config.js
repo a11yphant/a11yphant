134a19a393778fe9baee6eb15317a30f2ad5008f
Thomas Dax <thomas.dax@vivid-planet.com>
2021-03-08 00:41:33 +0100

    chore(testing): add reporter and coverage config

--- services/site/jest.config.js
+++ services/site/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  testEnvironment: "jsdom",
+  rootDir: "./",
+  testRegex: ".*\\.test\\.tsx$",
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
@@ -6,4 +9,8 @@ module.exports = {
   moduleNameMapper: {
     "app/(.*)": "<rootDir>/src/$1",
   },
+  reporters: ["default", "jest-junit"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.(t|j)s(x)"],
+  coverageReporters: ["text", "text-summary", "cobertura"],
+  coverageDirectory: "<rootDir>",
 };
