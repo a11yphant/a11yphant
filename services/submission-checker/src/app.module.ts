1027356ba226e6de12f4ceb331de32441e28fd32
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-09 21:55:48 +0100

    feat(checker): remove controller

--- services/submission-checker/src/app.module.ts
+++ services/submission-checker/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { BrowserService } from './browser.service';
-import { CheckSubmissionController } from './check-submission.controller';
 import { CheckSubmissionService } from './check-submission.service';
 import submissionRenderer from './config/submission-checker';
 import { SubmissionService } from './submission.service';
@@ -14,7 +13,6 @@ import { WebdriverFactory } from './webdriver.factory';
       load: [submissionRenderer],
     }),
   ],
-  controllers: [CheckSubmissionController],
   providers: [
     BrowserService,
     CheckSubmissionService,
--- services/submission-checker/src/main.ts
+++ services/submission-checker/src/main.ts
@@ -1,11 +1,10 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const config = app.get<ConfigService>(ConfigService);
-  await app.listen(config.get<number>('app.port'));
+
+  await app.init();
 }
 bootstrap();
