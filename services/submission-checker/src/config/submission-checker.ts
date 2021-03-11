2cb9a3a704b9b5c707a9dfc67d820e20a5013748
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-11 15:43:47 +0100

    feat(checker): update config namespace name

--- services/submission-checker/src/config/submission-checker.ts
+++ services/submission-checker/src/config/submission-checker.ts
@@ -1,6 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
-export default registerAs("submissionRenderer", () => {
+export default registerAs("submission-checker", () => {
   return {
     "renderer-base-url": process.env.SUBMISSION_CHECKER_RENDERER_BASE_URL,
   };
