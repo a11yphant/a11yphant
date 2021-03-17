565d159b26f3324378d9214a196e3c47d60f4f52
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-17 14:55:20 +0100

    feat(api): add hint query

--- services/api/src/challenge/hint.service.ts
+++ services/api/src/challenge/hint.service.ts
@@ -7,6 +7,12 @@ import { Hint } from "./models/hint.model";
 export class HintService {
   constructor(private prisma: PrismaService) {}
 
+  async findOneById(id: string): Promise<Hint> {
+    const record = await this.prisma.hint.findUnique({ where: { id } });
+
+    return record ? HintService.createModelFromDatabaseRecord(record) : null;
+  }
+
   async findForLevel(levelId: string): Promise<Hint[]> {
     const hints = await this.prisma.hint.findMany({
       where: { levelId },
