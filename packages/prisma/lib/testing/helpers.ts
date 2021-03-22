488b023c1ab9e173ea335ad0907b3890e530be1d
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-22 21:50:25 +0100

    Revert "feat(prisma): rebuild indices after table reset"
    This reverts commit e7f211eb6d060f7c035e08ef975f5ed4b4bc2d1f.

--- packages/prisma/lib/testing/helpers.ts
+++ packages/prisma/lib/testing/helpers.ts
@@ -73,8 +73,6 @@ async function clearTableContents(client: PrismaClient): Promise<void> {
 
   for (const tableName of tableNames.map((row: any) => row.table_name)) {
     await client.$executeRaw(`DELETE FROM "${tableName}";`);
-
-    await client.$transaction([client.$executeRaw(`REINDEX TABLE "${tableName}";`)]);
   }
 }
 
