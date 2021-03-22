6ae2b4708a6e21e1f721a91e469dc7e79446d8f5
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-22 17:14:15 +0100

    fix(EvaluationHeader): remove unused class

--- services/site/src/components/evaluation/EvaluationHeader.tsx
+++ services/site/src/components/evaluation/EvaluationHeader.tsx
@@ -8,7 +8,7 @@ interface EvaluationHeaderProps {
 
 const EvaluationHeader: React.FunctionComponent<EvaluationHeaderProps> = ({ classes, level, challenge }) => {
   return (
-    <div className={`${classes} flex flex-row justify-between items-center box-border pb-6 h-fitContent w-full border-white border-b object-contain`}>
+    <div className={`${classes} flex flex-row justify-between items-center box-border pb-6 h-fitContent w-full border-white border-b`}>
       <h2 className="text-white leading-10">
         <strong>Evaluation</strong> <br /> {challenge} <br /> Level {level}
       </h2>
