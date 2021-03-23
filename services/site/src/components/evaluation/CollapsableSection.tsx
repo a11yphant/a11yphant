69696702d2394ac44dcbbdbef371bfb444dd4c73
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 15:29:21 +0100

    Add(CollapsableSection): section heading

--- services/site/src/components/evaluation/CollapsableSection.tsx
+++ services/site/src/components/evaluation/CollapsableSection.tsx
@@ -13,19 +13,20 @@ const CollapsibleSection: React.FunctionComponent<CollapsibleSectionProps> = ({ 
   const requirementPassed = false;
 
   return (
-    <div className={`${classes} flex flex-col items-center w-full box-border h-fitContent max-w-7xl m-auto`}>
+    <div className={`${classes} flex flex-col items-left w-full box-border h-fitContent max-w-7xl m-auto`}>
+      <h3 className="text-white font-bold h2">1.0 The link can be activated using the mouse</h3>
       <div className="flex flex-row items-center w-full box-border py-6">
         <div className="flex flex-col w-full">
-          <h3>
+          <h4>
             <Button
-              className="flex flex-row-reverse m-0 p-0 group text-white font-bold transition duration-300 hover:text-primaryDark focus:text-primaryDark"
+              className="h3 flex flex-row-reverse m-0 p-0 group text-white font-bold transition duration-300 hover:text-primaryDark focus:text-primaryDark"
               overrideClassname
               aria-expanded="false"
               icon={<Chevron className="text-white mr-8 group-hover:text-primaryDark group-focus:text-primaryDark" />}
             >
-              1.0 Global Requirements
+              1.1 Global Requirements
             </Button>
-          </h3>
+          </h4>
           <div>
             <p className="text-white ml-16 my-4">
               The HTML code must comply to all markup rules established by the W3C. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus
