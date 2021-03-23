983c7c6e3c54c13f01296abd28521b79550f92f5
Thomas Dax <thomas.dax@vivid-planet.com>
2021-03-23 16:40:37 +0100

    fix(level): add LevelResolver to generate-schema.ts

--- services/api/src/generate-schema.ts
+++ services/api/src/generate-schema.ts
@@ -5,9 +5,10 @@ import { printSchema } from "graphql";
 
 import { ChallengeResolver } from "./challenge/challenge.resolver";
 import { HintResolver } from "./challenge/hint.resolver";
+import { LevelResolver } from "./challenge/level.resolver";
 import { HelloWorldResolver } from "./hello-world/hello-world.resolver";
 
-const resolvers = [HelloWorldResolver, ChallengeResolver, HintResolver];
+const resolvers = [HelloWorldResolver, ChallengeResolver, HintResolver, LevelResolver];
 
 const scalars = [];
 
