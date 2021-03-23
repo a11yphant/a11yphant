b62af10517593d7aff4afa59b5166726893cc8e5
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 15:06:08 +0100

    Refactor: make it able to add classes to some svg icons

--- services/site/src/components/icons/X.tsx
+++ services/site/src/components/icons/X.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-const X: React.FunctionComponent = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    className="h-6 w-6"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-  </svg>
-);
+const X: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={`${className} h-6 w-6`}
+      aria-hidden="true"
+      focusable="false"
+      {...props}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  );
+};
 
 export default X;
