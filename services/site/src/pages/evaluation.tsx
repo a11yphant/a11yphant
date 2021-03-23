e2588b99e2ab866bb061eb044e5e1eb6ceffa2bb
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 11:13:57 +0100

    Fix(evaluation): remove graphQL query as it is not ready yet

--- services/site/src/pages/evaluation.tsx
+++ services/site/src/pages/evaluation.tsx
@@ -1,18 +1,9 @@
 import Button from "app/components/buttons/Button";
 import EvaluationHeader from "app/components/evaluation/EvaluationHeader";
 import Navigation from "app/components/Navigation";
-import { useChallengeQuery } from "app/generated/graphql";
 import React from "react";
 
-const challengeId = "242003d6-402e-49b7-9ec2-702445b37c8e";
-
 const Evaluation: React.FunctionComponent = () => {
-  const { loading } = useChallengeQuery({ variables: { id: challengeId } });
-
-  if (loading) {
-    return <div>Loading ...</div>;
-  }
-
   const levelCompleted = false;
 
   return (
