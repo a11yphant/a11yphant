b00e920f159ab328c0eee139d18a74e7c9d768d6
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 11:05:29 +0100

    Refactor(EvaluationHeader): rename classes to className

--- services/site/src/components/evaluation/EvaluationHeader.tsx
+++ services/site/src/components/evaluation/EvaluationHeader.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 interface EvaluationHeaderProps {
-  classes?: string;
+  className?: string;
   challenge: string;
   level: string;
 }
 
-const EvaluationHeader: React.FunctionComponent<EvaluationHeaderProps> = ({ classes, level, challenge }) => {
+const EvaluationHeader: React.FunctionComponent<EvaluationHeaderProps> = ({ className, level, challenge }) => {
   return (
-    <div className={`${classes} flex flex-row justify-between items-center box-border pb-6 h-fitContent w-full border-white border-b`}>
+    <div className={`${className} flex flex-row justify-between items-center box-border pb-6 h-fitContent w-full border-white border-b`}>
       <h2 className="text-white leading-10">
         <strong>Evaluation</strong> <br /> {challenge} <br /> Level {level}
       </h2>
