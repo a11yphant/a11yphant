60f67161a54e8ad5c06676f96364f50d04b60eba
Michael Brandstätter <mbrandstaetter.mmt-b2020@fh-salzburg.ac.at>
2021-03-23 12:17:27 +0100

    fix(submission): make nullable input fields optional

--- services/api/src/submission/SubmissionInput.ts
+++ services/api/src/submission/SubmissionInput.ts
@@ -5,9 +5,9 @@ export class SubmissionInput {
   @Field()
   levelId: string;
   @Field({ description: "HTML is formatted as a multi-line string with line breaks.", nullable: true })
-  html: string;
+  html?: string;
   @Field({ description: "CSS is formatted as a multi-line string with line breaks.", nullable: true })
-  css: string;
+  css?: string;
   @Field({ description: "JS is formatted as a multi-line string with line breaks.", nullable: true })
-  js: string;
+  js?: string;
 }
