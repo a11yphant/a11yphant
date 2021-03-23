f0503fc2831c2335acaac19bf90f0346131e4bb2
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 11:36:37 +0100

    Add: svg icon for check

---
+++ services/site/src/components/icons/Check.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Check: React.FunctionComponent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="h-24 w-24 text-white ml-10"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+  </svg>
+);
+
+export default Check;
