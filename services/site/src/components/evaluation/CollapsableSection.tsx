e45b432995be7828e8ae0d995b313cf7f7c20c04
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-24 11:04:49 +0100

    Add(CollapsableSection): animate toggle button

--- services/site/src/components/evaluation/CollapsableSection.tsx
+++ services/site/src/components/evaluation/CollapsableSection.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
+import { animated, useSpring } from "react-spring";
 
 import Button from "../buttons/Button";
 import Check from "../icons/Check";
 import Chevron from "../icons/Chevron";
 import X from "../icons/X";
-
 interface CollapsibleSectionProps {
   className?: string;
   passed: boolean;
@@ -14,6 +14,34 @@ interface CollapsibleSectionProps {
 
 const CollapsibleSection: React.FunctionComponent<CollapsibleSectionProps> = ({ className, passed, title, description }) => {
   const [showDescription, setShowDescription] = useState(false);
+
+  const AnimatedIcon = animated(Chevron);
+
+  const openSectionStyle = {
+    icon: {
+      transform: "rotate(0deg)",
+      tension: 0,
+      delay: 0,
+      easing: "d3-ease",
+    },
+  };
+
+  const closedSectionStyle = {
+    icon: {
+      transform: "rotate(180deg)",
+      tension: 0,
+      delay: 0,
+      easing: "d3-ease",
+    },
+  };
+
+  // any is necessary here because the types of react-spring are somehow messed up
+  const animation: any = useSpring({
+    iconTransform: showDescription ? closedSectionStyle.icon.transform : openSectionStyle.icon.transform,
+    config: {
+      duration: 300,
+    },
+  });
 
   return (
     <div className={`${className} flex flex-row items-start w-full box-border py-2`}>
@@ -26,7 +54,12 @@ const CollapsibleSection: React.FunctionComponent<CollapsibleSectionProps> = ({ 
             className="h3 flex flex-row-reverse m-0 py-4 pr-4 group text-white font-bold transition duration-300 hover:text-primaryDark focus:text-primaryDark"
             overrideClassname
             aria-expanded={showDescription}
-            icon={<Chevron className="text-white mr-8 group-hover:text-primaryDark group-focus:text-primaryDark" />}
+            icon={
+              <AnimatedIcon
+                style={{ transform: animation.iconTransform }}
+                className="text-white mr-8 group-hover:text-primaryDark group-focus:text-primaryDark"
+              />
+            }
           >
             {title}
           </Button>
