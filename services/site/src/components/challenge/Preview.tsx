9db2d5ac7ed976329d309cb041738a8723ad25dd
Thomas Dax <thomas.dax@vivid-planet.com>
2021-03-24 09:49:46 +0100

    fix(linting): fix a11y linting errors

--- services/site/src/components/challenge/Preview.tsx
+++ services/site/src/components/challenge/Preview.tsx
@@ -46,6 +46,7 @@ const Preview: React.FunctionComponent<PreviewProps> = ({ classes, cssCode, html
     <div className={`${classes} box-border relative border-2 rounded-lg border-primary py-2 px-4`}>
       <h3 className="text-primary mb-4">{heading}</h3>
       <iframe
+        title="Preview"
         className="w-full h-auto"
         srcDoc={`<style>${innerCssCode}</style><base target="_blank">${innerHtmlCode}<script>${innerJavascriptCode}</script>`}
       />
