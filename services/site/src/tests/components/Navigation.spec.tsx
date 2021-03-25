2c110c6d5535300b9b8cd1c96bb7bcedd6bb9f1e
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 18:49:13 +0100

    Add(Navigation): create test file

---
+++ services/site/src/tests/components/Navigation.spec.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import { cleanup } from "@testing-library/react";
+
+afterEach(cleanup);
+
+// TODO: add test when breadcrumb-test is done
+
+describe("Navigation", () => {
+  //   it("renders correctly", () => {
+  //     const { container } = render(<Navigation />);
+  //     expect(container.querySelectorAll("header")).toHaveProperty("length", 1);
+  //     expect(container.querySelectorAll(".logo")).toHaveProperty("length", 1);
+  //   });
+});
