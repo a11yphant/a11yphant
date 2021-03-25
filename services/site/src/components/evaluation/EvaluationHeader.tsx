7fc6083bf805b077f0185e639151054e9e6794cb
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 10:07:46 +0100

    Add: calculate total percentage score on evaluation page

--- services/site/src/components/evaluation/EvaluationHeader.tsx
+++ services/site/src/components/evaluation/EvaluationHeader.tsx
@@ -4,15 +4,16 @@ interface EvaluationHeaderProps {
   className?: string;
   challengeName: string;
   levelIdx: string;
+  score: number;
 }
 
-const EvaluationHeader: React.FunctionComponent<EvaluationHeaderProps> = ({ className, levelIdx, challengeName }) => {
+const EvaluationHeader: React.FunctionComponent<EvaluationHeaderProps> = ({ className, levelIdx, challengeName, score }) => {
   return (
     <div className={`${className} flex flex-row justify-between items-center box-border pb-6 h-fitContent w-full border-white border-b`}>
       <h2 className="text-white leading-10">
         <strong>Evaluation</strong> <br /> {challengeName} <br /> Level {levelIdx}
       </h2>
-      <p className="text-8xl text-white font-bold">81%</p>
+      <p className="text-8xl text-white font-bold">{isNaN(score) ? "" : `${score}%`}</p>
     </div>
   );
 };
