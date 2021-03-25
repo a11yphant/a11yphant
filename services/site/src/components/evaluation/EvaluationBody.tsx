53a2ab4a7009fb76439ff8f4f24087de94481a52
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 10:48:13 +0100

    Add(EvaluationBody): render check data from API

--- services/site/src/components/evaluation/EvaluationBody.tsx
+++ services/site/src/components/evaluation/EvaluationBody.tsx
@@ -1,3 +1,4 @@
+import { Check } from "app/generated/graphql";
 import React from "react";
 
 import CollapsableSection from "./CollapsableSection";
@@ -5,14 +6,23 @@ import CollapsableSection from "./CollapsableSection";
 interface EvaluationBodyProps {
   className?: string;
   requirementTitle: string;
-  checks: any; //TODO: replace when graphQL Endpoint is ready
+  checks: Check[];
+  requirementIdx: number;
 }
 
-const EvaluationBody: React.FunctionComponent<EvaluationBodyProps> = ({ className, requirementTitle, checks }) => {
+const EvaluationBody: React.FunctionComponent<EvaluationBodyProps> = ({ className, requirementTitle, checks, requirementIdx }) => {
+  // render checks
+  const getChecks = checks.map((check, idx) => {
+    const checkTitle = `${requirementIdx}.${idx + 1} ${check.title}`;
+    return (
+      <CollapsableSection key={check.id} passed={check.result === "SUCCESS" ? true : false} title={checkTitle} description={check.description} />
+    );
+  });
+
   return (
-    <div className={`${className} flex flex-col items-left w-full box-border h-full m-auto`}>
+    <div className={`${className} flex flex-col items-left w-full box-border h-full m-auto mb-8`}>
       <h3 className="text-white font-bold h2 mb-2"> {requirementTitle}</h3>
-      <CollapsableSection passed={true} title={checks.title} description={checks.description} />
+      {getChecks}
     </div>
   );
 };
