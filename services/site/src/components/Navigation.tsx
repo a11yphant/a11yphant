9ddad4793ed0f14eac66cea6328982dea023b682
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-25 16:34:59 +0100

    Add(Navigation): simple display conditions

--- services/site/src/components/Navigation.tsx
+++ services/site/src/components/Navigation.tsx
@@ -1,21 +1,49 @@
 import Breadcrumbs from "app/components/breadcrumbs/Breadcrumbs";
 import React from "react";
 
+import Button from "./buttons/Button";
 import Avatar from "./icons/Avatar";
 import Save from "./icons/Save";
 
 const Navigation: React.FunctionComponent = () => {
+  const displayBreadcrumbs = true;
+  const displaySave = true;
+  const displayUserProfile = true;
+  const displayRegistration = false;
+
   return (
     <header className="flex justify-between items-center p-6 h-1/20">
       <h1 className="logo">A11y Challenges</h1>
+      {displayBreadcrumbs ? (
+        <>
+          <div className="flex justify-center items-center">
+            <Breadcrumbs />
+          </div>
+        </>
+      ) : (
+        ""
+      )}
       <div className="flex justify-center items-center">
-        <Breadcrumbs />
-      </div>
-      <div className="flex justify-center items-center">
-        <Save />
-        <span className="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100 ml-8">
-          <Avatar />
-        </span>
+        {displaySave ? <Save /> : ""}
+        {displayUserProfile ? (
+          <>
+            <span className="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100 ml-8">
+              <Avatar />
+            </span>
+          </>
+        ) : (
+          ""
+        )}
+        {displayRegistration ? (
+          <>
+            <Button full className="mx-4">
+              Sign Up
+            </Button>{" "}
+            <Button>Login</Button>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </header>
   );
