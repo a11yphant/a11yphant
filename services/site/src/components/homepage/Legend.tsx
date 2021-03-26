fd759af90e329f2abde3372e5f56d00f8601011d
Daniela Kubesch <dkubesch.mmt-m2020@fh-salzburg.ac.at>
2021-03-26 11:30:25 +0100

    Refactor(Legend): adapt styling

--- services/site/src/components/homepage/Legend.tsx
+++ services/site/src/components/homepage/Legend.tsx
@@ -9,21 +9,21 @@ const Legend: React.FunctionComponent<LegendProps> = ({ className }) => {
     <div className={`${className} flex flex-row justify-between items-center box-border mt-2 mb-8 w-full `}>
       <ul className="flex flex-row">
         <li className="text-primary pr-1 mr-2 flex items-center">
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-white mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-white mr-2" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-white mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-white mr-2" />
           Easy
         </li>
         <li className="text-primary px-1 mx-2 flex items-center">
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-white mr-2" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-white mr-2" />
           Medium
         </li>
         <li className="text-primary px-1 mx-2 flex items-center">
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
-          <div className="w-3.5 h-4/5 border-2 rounded border-primary bg-primary mr-2" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-1" />
+          <div className="w-3 h-4/5 border-2 rounded border-primary bg-primary mr-2" />
           Hard
         </li>
       </ul>
