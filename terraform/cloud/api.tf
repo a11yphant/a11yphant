71e6784d0b0f906972b20153963a745e8b7e7dd9
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-31 23:19:21 +0200

    chore(tf): fix sns endpoint environment variable name

--- terraform/cloud/api.tf
+++ terraform/cloud/api.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "api" {
       DB_URL = "postgresql://${var.postgres_cluster_root_user}:${var.postgres_cluster_root_password}@${aws_rds_cluster.postgres.endpoint}:${aws_rds_cluster.postgres.port}/${var.postgres_cluster_database_name}?connect_timeout=30&pool_timeout=30"
       API_MESSAGING_TOPICS = "submission=${module.messaging.submission_topic_arn}"
       API_MESSAGING_REGION = "eu-central-1"
-      API_MESSAGING_ENDPOINT = "https://${aws_vpc_endpoint.sns.dns_entry[0]["dns_name"]}"
+      API_MESSAGING_SNS_ENDPOINT = "https://${aws_vpc_endpoint.sns.dns_entry[0]["dns_name"]}"
     }
   }
 
