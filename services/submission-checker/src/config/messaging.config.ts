277bb5f3af73176cd7d51059f0cd7d04c50725b2
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-03-31 14:48:29 +0200

    feat(submission-checker): use topic parser from package

--- services/submission-checker/src/config/messaging.config.ts
+++ services/submission-checker/src/config/messaging.config.ts
@@ -1,17 +1,10 @@
+import { parseTopics } from "@a11y-challenges/nestjs-aws-messaging";
 import { registerAs } from "@nestjs/config";
-
-function parseTopics(topics: string): Record<string, string> {
-  return topics.split(",").reduce((topics, current) => {
-    const [topic, arn] = current.split("=");
-
-    return { ...topics, [topic]: arn };
-  }, {});
-}
 
 export default registerAs("messaging", () => ({
   "poll-queue": process.env.SUBMISSION_CHECKER_MESSAGING_POLL_QUEUE || false,
   "queue-url": process.env.SUBMISSION_CHECKER_MESSAGING_QUEUE_URL,
   region: process.env.SUBMISSION_CHECKER_MESSAGING_REGION || "us-east-1",
   topics: parseTopics(process.env.SUBMISSION_CHECKER_MESSAGING_TOPICS || ""),
-  "delete-handled-messages": process.env.SUBMISSION_CHECKER_MESSAGING_DELETE_HANDLED_MESSAGES || true,
+  "delete-handled-messages": Boolean(+process.env.SUBMISSION_CHECKER_MESSAGING_DELETE_HANDLED_MESSAGES) || true,
 }));
