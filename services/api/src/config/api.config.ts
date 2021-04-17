0d0e581314f15c8c518019f223341ec9b7ddd70e
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-17 11:29:27 +0200

    feat(api): add support for heroku port environment variable

--- services/api/src/config/api.config.ts
+++ services/api/src/config/api.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("api", () => {
-  const port = Number(process.env.API_PORT) || 3000;
+  const port = Number(process.env.API_PORT) || Number(process.env.PORT) || 3000;
   const host = process.env.API_HOST || "localhost";
 
   return {
