e2efa0f80eb56aa1e214f2871e218b73edd28dc6
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-18 14:51:18 +0200

    chore(tf): add heroku release container

--- terraform/cloud/api.tf
+++ terraform/cloud/api.tf
@@ -1,6 +1,8 @@
 locals {
-  gitlab_ci_image = "gitlab.mediacube.at:5050/a11y-challenges/a11y-challenges/api:${terraform.workspace}"
-  heroku_image = "registry.heroku.com/${terraform.workspace}-a11yphant-api/web:latest"
+  gitlab_ci_app_image = "gitlab.mediacube.at:5050/a11y-challenges/a11y-challenges/api:${terraform.workspace}"
+  heroku_app_image = "registry.heroku.com/${terraform.workspace}-a11yphant-api/web:latest"
+  gitlab_ci_release_image = "gitlab.mediacube.at:5050/a11y-challenges/a11y-challenges/api-release:${terraform.workspace}"
+  heroku_release_image = "registry.heroku.com/${terraform.workspace}-a11yphant-api/release:latest"
 }
 
 resource "heroku_app" "api" {
@@ -33,22 +35,23 @@ resource "heroku_formation" "api" {
     size = "hobby"
 
     depends_on = [
-      herokux_app_container_release.api_release
+      herokux_app_container_release.api_app_container_release,
+      herokux_app_container_release.api_release_container_release,
     ]
 }
 
-data "docker_registry_image" "gitlab_ci_api_image" {
-  name = local.gitlab_ci_image
+data "docker_registry_image" "gitlab_ci_api_app_image" {
+  name = local.gitlab_ci_app_image
 }
 
-resource "docker_image" "gitlab_ci_api_image" {
-  name  = local.gitlab_ci_image
-  pull_triggers = [data.docker_registry_image.gitlab_ci_api_image.sha256_digest]
+resource "docker_image" "gitlab_ci_api_app_image" {
+  name  = local.gitlab_ci_app_image
+  pull_triggers = [data.docker_registry_image.gitlab_ci_api_app_image.sha256_digest]
 }
 
-resource "null_resource" "tag_api_image_for_heroku" {
+resource "null_resource" "tag_api_app_image_for_heroku" {
   provisioner "local-exec" {
-    command = "docker image tag ${local.gitlab_ci_image} ${local.heroku_image}"
+    command = "docker image tag ${local.gitlab_ci_app_image} ${local.heroku_app_image}"
   }
 
   triggers = {
@@ -56,12 +59,13 @@ resource "null_resource" "tag_api_image_for_heroku" {
   }
 
   depends_on = [
-    docker_image.gitlab_ci_api_image
+    docker_image.gitlab_ci_api_app_image
   ]
 }
-resource "null_resource" "push_api_image_to_heroku" {
+
+resource "null_resource" "push_api_app_image_to_heroku" {
   provisioner "local-exec" {
-    command = "docker push ${local.heroku_image}"
+    command = "docker push ${local.heroku_app_image}"
   }
 
   triggers = {
@@ -69,22 +73,83 @@ resource "null_resource" "push_api_image_to_heroku" {
   }
 
   depends_on = [
-    null_resource.tag_api_image_for_heroku
+    null_resource.tag_api_app_image_for_heroku
   ]
 }
 
-data "herokux_registry_image" "api" {
+data "docker_registry_image" "gitlab_ci_api_release_image" {
+  name = local.gitlab_ci_release_image
+}
+
+resource "docker_image" "gitlab_ci_api_release_image" {
+  name  = local.gitlab_ci_release_image
+  pull_triggers = [data.docker_registry_image.gitlab_ci_api_release_image.sha256_digest]
+}
+
+resource "null_resource" "tag_api_release_image_for_heroku" {
+  provisioner "local-exec" {
+    command = "docker image tag ${local.gitlab_ci_release_image} ${local.heroku_release_image}"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    docker_image.gitlab_ci_api_release_image
+  ]
+}
+
+resource "null_resource" "push_api_release_image_to_heroku" {
+  provisioner "local-exec" {
+    command = "docker push ${local.heroku_release_image}"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    null_resource.tag_api_release_image_for_heroku
+  ]
+}
+
+data "herokux_registry_image" "api_app" {
   app_id = heroku_app.api.uuid
   process_type = "web"
   docker_tag = "latest"
 
   depends_on = [
-    null_resource.push_api_image_to_heroku
+    null_resource.push_api_app_image_to_heroku
   ]
 }
 
-resource "herokux_app_container_release" "api_release" {
+resource "herokux_app_container_release" "api_app_container_release" {
     app_id = heroku_app.api.uuid
-    image_id = data.herokux_registry_image.api.digest
+    image_id = data.herokux_registry_image.api_app.digest
     process_type = "web"
+
+    depends_on = [
+    null_resource.push_api_release_image_to_heroku,
+  ]
+}
+
+data "herokux_registry_image" "api_release" {
+  app_id = heroku_app.api.uuid
+  process_type = "release"
+  docker_tag = "latest"
+
+  depends_on = [
+    null_resource.push_api_release_image_to_heroku
+  ]
+}
+
+resource "herokux_app_container_release" "api_release_container_release" {
+    app_id = heroku_app.api.uuid
+    image_id = data.herokux_registry_image.api_release.digest
+    process_type = "release"
+
+    depends_on = [
+      null_resource.push_api_release_image_to_heroku,
+  ]
 }
