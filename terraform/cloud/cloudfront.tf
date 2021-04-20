e55378203ae84ac6bcb2fb105b81924ff4788419
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-20 13:25:40 +0200

    chore(tf): add domain validation and certificate

--- terraform/cloud/cloudfront.tf
+++ terraform/cloud/cloudfront.tf
@@ -131,6 +131,15 @@ resource "aws_cloudfront_origin_access_identity" "site_access_identity" {
   comment = "${terraform.workspace} cloudfront site_access_identity"
 }
 
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_route53_record" "cdn_domain" {
   zone_id = var.route53_zone_id
   name = var.domain
@@ -141,4 +150,26 @@ resource "aws_route53_record" "cdn_domain" {
     zone_id = aws_cloudfront_distribution.site.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "validation_records" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.route53_zone_id
+}
+
+resource "aws_acm_certificate_validation" "domain" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
 }
