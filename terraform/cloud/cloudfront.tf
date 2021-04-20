7c25b4096000d02b8f7947e0123829fc48275a51
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-20 14:12:56 +0200

    chore(tf): create certificate in us-east-1

--- terraform/cloud/cloudfront.tf
+++ terraform/cloud/cloudfront.tf
@@ -133,6 +133,8 @@ resource "aws_cloudfront_origin_access_identity" "site_access_identity" {
 }
 
 resource "aws_acm_certificate" "certificate" {
+  provider = aws.us_east_1
+
   domain_name       = var.domain
   validation_method = "DNS"
 
