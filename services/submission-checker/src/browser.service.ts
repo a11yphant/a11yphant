d49023dae9a5fb142742d240570f00da5711792e
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-21 17:09:49 +0200

    feat(submission-checker): improve error handling while running browser tests

--- services/submission-checker/src/browser.service.ts
+++ services/submission-checker/src/browser.service.ts
@@ -1,21 +1,26 @@
 import AxeBuilder from "@axe-core/webdriverjs";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { AxeResults } from "axe-core";
 
 import { WebdriverFactory } from "./webdriver.factory";
 
 @Injectable()
 export class BrowserService {
-  constructor(private factory: WebdriverFactory) {}
+  constructor(private logger: Logger, private factory: WebdriverFactory) {}
 
   // TODO adapt return value type
   async runAxeChecks(url: string, options: unknown): Promise<AxeResults> {
     const driver = await this.factory.create();
-    await driver.get(url);
-    const axe = new AxeBuilder(driver).options(options);
-    const result = (await axe.analyze()) as AxeResults;
-    await driver.quit();
+    try {
+      await driver.get(url);
+      const axe = new AxeBuilder(driver).options(options);
+      const result = (await axe.analyze()) as AxeResults;
 
-    return result;
+      return result;
+    } catch (error) {
+      this.logger.error(error, error.trace, BrowserService.name);
+    } finally {
+      await driver.quit();
+    }
   }
 }
--- services/submission-checker/tests/browser.service.spec.ts
+++ services/submission-checker/tests/browser.service.spec.ts
@@ -1,4 +1,5 @@
 import { createMock } from "@golevelup/nestjs-testing";
+import { Logger } from "@nestjs/common";
 import { ThenableWebDriver } from "selenium-webdriver";
 
 import { BrowserService } from "../src/browser.service";
@@ -7,6 +8,7 @@ import { WebdriverFactory } from "../src/webdriver.factory";
 describe("browser service", () => {
   it("can run axe checks on a website", async () => {
     const browser = new BrowserService(
+      createMock<Logger>(),
       createMock<WebdriverFactory>({
         create: jest.fn().mockReturnValue(
           createMock<ThenableWebDriver>({
@@ -22,5 +24,47 @@ describe("browser service", () => {
     });
 
     expect(result).toBeTruthy();
+  });
+
+  it("quits the driver when the check did not fail", async () => {
+    const quit = jest.fn().mockResolvedValue(null);
+    const browser = new BrowserService(
+      createMock<Logger>(),
+      createMock<WebdriverFactory>({
+        create: jest.fn().mockReturnValue(
+          createMock<ThenableWebDriver>({
+            get: jest.fn().mockResolvedValue(null),
+            quit,
+          }),
+        ),
+      }),
+    );
+
+    await browser.runAxeChecks(`http://rendered-submission-url/1`, {
+      runOnly: ["landmark-one-main"],
+    });
+
+    expect(quit).toHaveBeenCalled();
+  });
+
+  it("quits the driver when loading the submission failed", async () => {
+    const quit = jest.fn().mockResolvedValue(null);
+    const browser = new BrowserService(
+      createMock<Logger>(),
+      createMock<WebdriverFactory>({
+        create: jest.fn().mockReturnValue(
+          createMock<ThenableWebDriver>({
+            get: jest.fn().mockRejectedValue({ error: "failed" }),
+            quit,
+          }),
+        ),
+      }),
+    );
+
+    await browser.runAxeChecks(`http://rendered-submission-url/1`, {
+      runOnly: ["landmark-one-main"],
+    });
+
+    expect(quit).toHaveBeenCalled();
   });
 });
