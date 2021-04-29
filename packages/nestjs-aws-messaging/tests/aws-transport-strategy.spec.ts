a448922b3345dba661925050adf1c0ad2605e8e9
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-04-29 22:39:32 +0200

    test(aws messaging): add test for error handling

--- packages/nestjs-aws-messaging/tests/aws-transport-strategy.spec.ts
+++ packages/nestjs-aws-messaging/tests/aws-transport-strategy.spec.ts
@@ -121,6 +121,41 @@ describe("AWS Transport Strategy", () => {
     expect(messageHandler).toHaveBeenCalledWith(expect.objectContaining(event));
   });
 
+  it("logs errors thrown by the message handler", () => {
+    const callback = jest.fn();
+    const messageHandler = jest.fn().mockRejectedValue(new Error("that did not work"));
+
+    const event = { content: "hi" };
+    const messageData: AWS.SQS.ReceiveMessageResult = {
+      Messages: [
+        {
+          MessageId: "id",
+          Body: JSON.stringify({
+            Message: JSON.stringify(event),
+            Timestamp: new Date().toISOString(),
+            MessageAttributes: {
+              type: { DataType: "String", Value: "test-message" },
+            },
+          }),
+          ReceiptHandle: "Hi",
+        },
+      ],
+    };
+
+    const receiveMessage = jest.fn((params, cb) => {
+      cb(null, messageData);
+    });
+
+    AWSMock.mock("SQS", "receiveMessage", receiveMessage);
+
+    const transport = new AwsTransportStrategy({ polling: true, queueUrl: "queue-url", region: "eu-central-1", deleteHandled: true });
+    transport.addHandler("test-message", messageHandler);
+    transport.listen(callback);
+
+    expect(callback).toHaveBeenCalled();
+    transport.close();
+  });
+
   it("can close a connection when polling is disabled", () => {
     const transport = new AwsTransportStrategy({ polling: false, queueUrl: "url", region: "eu-central-1", deleteHandled: true });
 
