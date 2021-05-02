87867c9800ec2ce5e19ad67337362375d646b99e
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-05-02 15:43:45 +0200

    Add(Card): images for gradients in card

--- services/site/src/components/homepage/Card.tsx
+++ services/site/src/components/homepage/Card.tsx
@@ -1,15 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 interface ICardProps {
   className?: string;
   heading: string;
+  easy?: boolean;
+  medium?: boolean;
+  hard?: boolean;
 }
 
-const Card: React.FunctionComponent<ICardProps> = ({ className, heading, children }) => {
+const Card: React.FunctionComponent<ICardProps> = ({ className, heading, children, easy, medium, hard }) => {
+  let gradient;
+
+  if (easy) {
+    gradient = "/images/01_easy.jpg";
+  } else if (medium) {
+    gradient = "/images/02_medium.jpg";
+  } else if (hard) {
+    gradient = "/images/03_hard.jpg";
+  }
+
   return (
-    <div className={`${className} w-64 h-64 border-2 border-primary rounded-lg flex flex-col justify-end p-4 my-4`}>
-      <h4 className="w-full text-primary font-bold">{heading}</h4>
-      <div className="w-full mt-2 text-primary">{children}</div>
+    <div className={`${className} w-64 h-64 border-2 border-primary rounded-xl flex flex-col justify-end overflow-hidden`}>
+      <Image src={gradient} alt="" width="500" height="500" />
+      <div className="p-4 bg-white">
+        <h4 className="w-full text-primary font-bold">{heading}</h4>
+        <div className="w-full mt-2 text-primary">{children}</div>
+      </div>
     </div>
   );
 };
