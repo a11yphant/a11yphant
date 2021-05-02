30860cc85a8b2d2eb7f514737f042f9b1dcdcbb5
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-05-02 17:25:47 +0200

    chore(setup): link npmrc into packages

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ cd $(dirname $0)
 # use correct npm and install dependencies
 nvm install
 nvm use
-npm install
 
 # create .env from .env.example
 if [ ! -f ./.env ]; then
@@ -16,20 +15,31 @@ fi
 # create api symlinks
 rm -f ./services/api/.env
 ln -s ./../../.env ./services/api/.env
+rm -f ./services/api/.npmrc
+ln -s ./../../.npmrc ./services/api/.npmrc
 
 # create site symlinks
 rm -f ./services/site/.env
 ln -s ./../../.env ./services/site/.env
+rm -f ./services/site/.npmrc
+ln -s ./../../.npmrc ./services/site/.npmrc
 
 # create submission-checker symlinks
 rm -f ./services/submission-checker/.env
 ln -s ./../../.env ./services/submission-checker/.env
+rm -f ./services/submission-checker/.npmrc
+ln -s ./../../.npmrc ./services/submission-checker/.npmrc
 
 # create submission-renderer symlinks
 rm -f ./services/submission-renderer/.env
 ln -s ./../../.env ./services/submission-renderer/.env
+rm -f ./services/submission-renderer/.npmrc
+ln -s ./../../.npmrc ./services/submission-renderer/.npmrc
 
 # create nestjs-aws-messaging symlinks
 rm -f ./packages/nestjs-aws-messaging/.env
 ln -s ./../../.env ./packages/nestjs-aws-messaging/.env
+rm -f ./packages/nestjs-aws-messaging/.npmrc
+ln -s ./../../.npmrc ./packages/nestjs-aws-messaging/.npmrc
 
+npm install
