7763ac61bdb290a003a019dd8ab2cc70e3087f95
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-05-04 14:58:35 +0200

    Refactor(ChallengeList): adapt styling

--- services/site/src/components/homepage/ChallengeList.tsx
+++ services/site/src/components/homepage/ChallengeList.tsx
@@ -34,9 +34,9 @@ const ChallengeList: React.FunctionComponent<ChallengeListProps> = ({ className,
 
   return (
     <div className={clsx("flex flex-col items-start mt-2 mb-6", className)}>
-      <div className={clsx("flex flex-row items-center items-baseline mx-4 mb-6", "h4")}>
-        <h3 className="flex items-center text-grey font-bold leading-10 mb-2">{heading}</h3>
-        <p className={clsx("text-grey ml-4 mb-0", "h3 font-normal")} aria-hidden="true">
+      <div className="flex flex-row items-center mx-4 mb-6">
+        <h3 className={clsx("flex items-center text-grey font-bold leading-10 mb-0", "h4")}>{heading}</h3>
+        <p className={clsx("text-grey ml-4 mb-0", "h4 font-normal")} aria-hidden="true">
           {`(${completedLevel}/${openLevel})`}
         </p>
         <p className="sr-only">{`${completedLevel} of ${openLevel} challenges completed`}</p>
