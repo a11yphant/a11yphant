a5580ef9845e580daedf1f996f4004be49a3b86f
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-05-05 20:23:04 +0200

    Refactor(reset-icon): adapt code

--- services/site/src/components/icons/Reset.tsx
+++ services/site/src/components/icons/Reset.tsx
@@ -5,7 +5,6 @@ const Reset: React.FunctionComponent = () => {
     <svg
       viewBox="-4 -3 24 24"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
       focusable="false"
       stroke="currentColor"
