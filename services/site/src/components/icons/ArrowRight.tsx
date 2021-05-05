a429826110dd851da796b1fc5815c468a3a2bde2
Thomas Dax <thomas.dax@vivid-planet.com>
2021-05-05 18:10:33 +0200

    refactor(icons): replace arrowRight icon

--- services/site/src/components/icons/ArrowRight.tsx
+++ services/site/src/components/icons/ArrowRight.tsx
@@ -2,16 +2,13 @@ import React from "react";
 
 const ArrowRight: React.FunctionComponent = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      className="text-white h-7 w-7 pl-2"
-      aria-hidden="true"
-      focusable="false"
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+    <svg viewBox="0 0 44 44" fill="currentColor" className="text-white h-7 w-7 pl-2" aria-hidden="true" focusable="false">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11 20.332h16.264L19.8 12.866 21.665 11 32.33 21.665 21.665 32.33l-1.866-1.866 7.465-7.466H11v-2.666z"
+        fill="#EDEDED"
+      />
     </svg>
   );
 };
