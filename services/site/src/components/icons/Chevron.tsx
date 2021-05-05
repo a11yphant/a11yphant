335d8362ce03e870e42dee4928cfc5ec371ec239
Thomas Dax <thomas.dax@vivid-planet.com>
2021-05-05 18:14:43 +0200

    refactor(icons): replace chevron icon

--- services/site/src/components/icons/Chevron.tsx
+++ services/site/src/components/icons/Chevron.tsx
@@ -3,16 +3,14 @@ import React from "react";
 const Chevron: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
+      viewBox="0 0 44 44"
+      fill="currentColor"
       className={`${className} h-8 w-8 transition duration-300 group`}
       aria-hidden="true"
       focusable="false"
       {...props}
     >
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+      <path d="M11 28l11-11 11 11" stroke="#B4B8B8" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
   );
 };
