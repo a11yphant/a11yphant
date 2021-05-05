05ff7d1bf4ea20515f0d76db181e9ced6f51d1ac
Thomas Dax <thomas.dax@vivid-planet.com>
2021-05-05 18:15:42 +0200

    refactor(icons): replace X icon

--- services/site/src/components/icons/X.tsx
+++ services/site/src/components/icons/X.tsx
@@ -2,17 +2,9 @@ import React from "react";
 
 const X: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      className={`${className} h-6 w-6`}
-      aria-hidden="true"
-      focusable="false"
-      {...props}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+    <svg viewBox="0 0 44 44" fill="currentColor" className={`${className} h-6 w-6`} aria-hidden="true" focusable="false" {...props}>
+      <path fill="#EDEDED" d="M13 14.895L14.895 13 31 29.105 29.105 31z" />
+      <path fill="#EDEDED" d="M29.105 13L31 14.895 14.895 31 13 29.105z" />
     </svg>
   );
 };
