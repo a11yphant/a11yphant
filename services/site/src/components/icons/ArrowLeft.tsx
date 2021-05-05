b211c3519d32af99865810a762af4af1f740b3d2
Thomas Dax <thomas.dax@vivid-planet.com>
2021-05-05 18:19:19 +0200

    feat(icons): add arrowLeft icon

---
+++ services/site/src/components/icons/ArrowLeft.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArrowLeft: React.FunctionComponent = () => {
+  return (
+    <svg viewBox="0 0 44 44" fill="currentColor" className="text-white h-7 w-7 pl-2" aria-hidden="true" focusable="false">
+      <path d="M13 14.895L14.895 13 31 29.105 29.105 31z" />
+      <path d="M29.105 13L31 14.895 14.895 31 13 29.105z" />
+    </svg>
+  );
+};
+
+export default ArrowLeft;
