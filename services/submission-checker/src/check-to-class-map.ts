40443390d99a69d84cb1988832832c8eb5f6991a
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-05-05 17:20:39 +0200

    refactor(submission-checker): extract selenium check

--- services/submission-checker/src/check-to-class-map.ts
+++ services/submission-checker/src/check-to-class-map.ts
@@ -1,9 +1,13 @@
 import { AxeLinkNameCheck } from "./checks/axe-link-name.check";
+import { ElementExists } from "./checks/element-exists.check";
+import { ElementNotExists } from "./checks/element-not-exists.check";
 import { HtmlIsValidCheck } from "./checks/html-is-valid.check";
 
 export const checkToClassMap = {
   "axe-link-name": AxeLinkNameCheck,
   "html-is-valid": HtmlIsValidCheck,
+  "element-exists": ElementExists,
+  "element-not-exists": ElementNotExists,
 };
 
 export const CHECK_TO_CLASS_MAP = "CHECK_TO_CLASS_MAP";
--- services/submission-checker/src/checks/element-exists.check.ts
+++ services/submission-checker/src/checks/element-exists.check.ts
@@ -1,18 +1,14 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { By } from "selenium-webdriver";
+import { Injectable } from "@nestjs/common";
+import { By, WebDriver } from "selenium-webdriver";
 
-import { BrowserService } from "../browser.service";
 import { Rule } from "../rule.interface";
 import { RuleCheckResult } from "../rule-check-result.interface";
 import { Submission } from "../submission.interface";
-import { Check } from "./check.interface";
+import { SeleniumCheck } from "./selenium-check";
 
 @Injectable()
-export class ElementExists implements Check {
-  constructor(private logger: Logger, private config: ConfigService, private browser: BrowserService) {}
-
-  public async run(submission: Submission, rule: Rule): Promise<RuleCheckResult> {
+export class ElementExists extends SeleniumCheck {
+  public async evaluateRule(driver: WebDriver, submission: Submission, rule: Rule): Promise<RuleCheckResult> {
     if (!rule.options?.selector) {
       this.logger.error(
         `Executing check ${rule.key} on submission ${submission.id} due too missing selector configuration`,
@@ -26,26 +22,11 @@ export class ElementExists implements Check {
       };
     }
 
-    const url = `${this.config.get<string>("submission-checker.renderer-base-url")}${submission.id}`;
+    const matchingElements = await driver.findElements(By.css(rule.options.selector));
 
-    const driver = await this.browser.startSession();
-    try {
-      await driver.get(url);
-      const matchingElements = await driver.findElements(By.css(rule.options.selector));
-
-      return {
-        id: rule.id,
-        status: matchingElements.length > 0 ? "success" : "failed",
-      };
-    } catch (error) {
-      this.logger.error(`Executing check ${rule.key} on submission ${submission.id} failed: ${error.message}`, error.stack, ElementExists.name);
-
-      return {
-        id: rule.id,
-        status: "error",
-      };
-    } finally {
-      await driver.close();
-    }
+    return {
+      id: rule.id,
+      status: matchingElements.length > 0 ? "success" : "failed",
+    };
   }
 }
