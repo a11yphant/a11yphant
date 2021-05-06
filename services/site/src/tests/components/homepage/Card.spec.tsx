92cb1c8e18d22b9bbc75109b37659c35d435ef1c
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-05-06 11:53:34 +0200

    test(api): ensure the correct gradients for different difficulties

--- services/site/src/tests/components/homepage/Card.spec.tsx
+++ services/site/src/tests/components/homepage/Card.spec.tsx
@@ -28,4 +28,52 @@ describe("Card", () => {
     // image is temporarily replaced with an background image
     // expect(container.querySelectorAll("img").length).toBeGreaterThan(0);
   });
+
+  it("renders the correct gradient for easy challenges", () => {
+    const { container } = render(
+      <Card
+        key={1}
+        className="mr-24"
+        challengeSlug={"semantic-html"}
+        heading={headingText}
+        levels={levelAmount}
+        difficulty={ChallengeDifficulty.Easy}
+        challengeNumber={1}
+      />,
+    );
+
+    expect(container.querySelector(".bg-gradient-easy")).toBeTruthy();
+  });
+
+  it("renders the correct gradient for medium challenges", () => {
+    const { container } = render(
+      <Card
+        key={1}
+        className="mr-24"
+        challengeSlug={"semantic-html"}
+        heading={headingText}
+        levels={levelAmount}
+        difficulty={ChallengeDifficulty.Medium}
+        challengeNumber={1}
+      />,
+    );
+
+    expect(container.querySelector(".bg-gradient-medium")).toBeTruthy();
+  });
+
+  it("renders the correct gradient for hard challenges", () => {
+    const { container } = render(
+      <Card
+        key={1}
+        className="mr-24"
+        challengeSlug={"semantic-html"}
+        heading={headingText}
+        levels={levelAmount}
+        difficulty={ChallengeDifficulty.Hard}
+        challengeNumber={1}
+      />,
+    );
+
+    expect(container.querySelector(".bg-gradient-hard")).toBeTruthy();
+  });
 });
