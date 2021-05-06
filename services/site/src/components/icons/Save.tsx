6e2da4bafe91dcde8365d0a21892952d55baa300
Thomas Dax <thomas.dax@vivid-planet.com>
2021-05-06 17:55:26 +0200

    fix(icons): fix color and size of Save icon

--- services/site/src/components/icons/Save.tsx
+++ services/site/src/components/icons/Save.tsx
@@ -1,12 +1,12 @@
 import clsx from "clsx";
 import React from "react";
 
-const Save: React.FunctionComponent = () => {
+const Save: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className }) => {
   return (
-    <svg viewBox="0 0 44 44" fill="none" stroke="currentColor" className={clsx("text-primary w-5 h-5")} aria-hidden="true" focusable="false">
-      <path d="M25 26a3 3 0 11-6 0 3 3 0 016 0z" strokeWidth="2" strokeMiterlimit="10" />
-      <path d="M32 33H12a1 1 0 01-1-1V12a1 1 0 011-1h16.172a1 1 0 01.707.293l3.828 3.828a1 1 0 01.293.707V32a1 1 0 01-1 1z" strokeWidth="2" />
-      <path strokeWidth="2" d="M17 11h10v7H17z" />
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" className={clsx("w-6 h-6", className)} aria-hidden="true" focusable="false">
+      <path d="M15 16a3 3 0 11-6 0 3 3 0 016 0z" stroke="#EDEDED" strokeWidth="2" strokeMiterlimit="10" />
+      <path d="M22 23H2a1 1 0 01-1-1V2a1 1 0 011-1h16.172a1 1 0 01.707.293l3.828 3.828a1 1 0 01.293.707V22a1 1 0 01-1 1z" strokeWidth="2" />
+      <path strokeWidth="2" d="M7 1h10v7H7z" />
     </svg>
   );
 };
