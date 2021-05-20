e7ac327be238c3df50268c3fac09656d6d519e6d
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-05-20 13:16:01 +0200

    chore(tf): remove unused aws iam policy

--- terraform/cloud/global_iam_policies.tf
+++ terraform/cloud/global_iam_policies.tf
@@ -24,29 +24,6 @@ resource "aws_iam_policy" "lambda_logging" {
 EOF
 }
 
-resource "aws_iam_policy" "vpc_access" {
-  name        = "${terraform.workspace}-vpc-access"
-  path        = "/"
-  description = "IAM policy for allowing the lambda to gaining access to a vpc by creating network interfaces"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:CreateNetworkInterface",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DeleteNetworkInterface"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_policy" "submission_topic_publishing" {
   name        = "${terraform.workspace}-submission-topic-publishing"
   path        = "/"
