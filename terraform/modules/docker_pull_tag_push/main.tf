3ae5987ca80a23dde62bb7637b625f89455934d3
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-05-20 12:34:54 +0200

    chore(tf): extract docker push pull to module

---
+++ terraform/modules/docker_pull_tag_push/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+data "docker_registry_image" "source_image" {
+  name = var.source_image
+}
+
+resource "docker_image" "source_image" {
+  name          = var.source_image
+  pull_triggers = [data.docker_registry_image.source_image.sha256_digest]
+}
+
+resource "null_resource" "tag_image" {
+  provisioner "local-exec" {
+    command = "docker image tag ${var.source_image} ${var.target_image}"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    docker_image.source_image
+  ]
+}
+
+resource "null_resource" "push_target_image" {
+  provisioner "local-exec" {
+    command = "docker push ${var.target_image}"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    null_resource.tag_image,
+  ]
+}
