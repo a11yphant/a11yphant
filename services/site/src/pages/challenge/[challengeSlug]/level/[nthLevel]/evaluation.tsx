e60868becc6feb0bce1606a630069ea1d4cd7a9c
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-05-25 14:22:34 +0200

    Fix: scroll bars

--- services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel]/evaluation.tsx
+++ services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel]/evaluation.tsx
@@ -102,7 +102,7 @@ const Evaluation: React.FunctionComponent = () => {
           <LoadingScreen />
         ) : (
           <>
-            <div className="flex flex-col items-left w-full box-border h-full max-w-7xl m-auto pt-24 mt-0 mb-4 overflow-scroll">
+            <div className="flex flex-col items-left w-full box-border h-full max-w-7xl m-auto pt-24 mt-0 mb-4 overflow-auto overscroll-none">
               <ul className="h-full">{getRequirements}</ul>
             </div>
             <div className="absolute bottom-8 right-8">
