af414c958918ff8b4b7720fbac7b5c0c4bb7d5ab
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-06-01 11:23:53 +0200

    tests(site): update link tests

--- services/site/src/tests/lib/apollo-client/create-forward-cookies-to-server-link.spec.ts
+++ services/site/src/tests/lib/apollo-client/create-forward-cookies-to-server-link.spec.ts
@@ -2,6 +2,8 @@ import { ApolloLink, execute, gql } from "@apollo/client";
 import { createForwardCookiesToServerLink } from "app/lib/apollo-client/create-forward-cookies-to-server-link";
 import { GetServerSidePropsContext } from "next";
 
+import { createFakeObservable, createTerminatingLink } from "./helpers";
+
 const query = gql`
   query test {
     user {
@@ -11,48 +13,40 @@ const query = gql`
 `;
 
 describe("forward cookies to server", () => {
-  it("returns a link if no context is provided", () => {
-    expect.assertions(1);
+  it("returns a link if no context is provided", (done) => {
+    const link = createForwardCookiesToServerLink().concat(createTerminatingLink());
 
-    const finalLink = jest.fn();
-    const link = createForwardCookiesToServerLink().concat(
-      new ApolloLink(() => {
-        finalLink();
-        return null;
-      }),
-    );
-
-    execute(link, { query }).subscribe(jest.fn());
-    expect(finalLink).toHaveBeenCalled();
+    execute(link, { query }).subscribe({
+      complete: done,
+      error: done.fail,
+    });
   });
 
-  it("returns a link if a context is provided", () => {
-    expect.assertions(1);
-
-    const finalLink = jest.fn();
+  it("returns a link if a context is provided", (done) => {
     const context = { res: {}, req: {} } as unknown as GetServerSidePropsContext;
-    const link = createForwardCookiesToServerLink(context).concat(
-      new ApolloLink(() => {
-        finalLink();
-        return null;
-      }),
-    );
+    const link = createForwardCookiesToServerLink(context).concat(createTerminatingLink());
 
-    execute(link, { query }).subscribe(jest.fn());
-    expect(finalLink).toHaveBeenCalled();
+    execute(link, { query }).subscribe({
+      complete: done,
+      error: done.fail,
+    });
   });
 
-  it("sets the cookie header in the operation context", () => {
+  it("sets the cookie header in the operation context", (done) => {
     expect.assertions(1);
 
     const context = { res: {}, req: { headers: { cookie: "cookie" } } } as unknown as GetServerSidePropsContext;
     const link = createForwardCookiesToServerLink(context).concat(
       new ApolloLink((operation) => {
         expect(operation.getContext()?.headers?.cookie).toEqual("cookie");
-        return null;
+
+        return createFakeObservable();
       }),
     );
 
-    execute(link, { query }).subscribe(jest.fn());
+    execute(link, { query }).subscribe({
+      complete: done,
+      error: done.fail,
+    });
   });
 });
