932dea34ffcc20ce1fc2a03f56c1c16827403f0b
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-06-05 21:06:04 +0200

    Fix: make spinning animation more smooth

--- services/site/src/components/icons/LoadingIndicator.tsx
+++ services/site/src/components/icons/LoadingIndicator.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const LoadingIndicator: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => {
   return (
     <svg
-      width="1em"
-      height="1em"
+      width="0.94em"
+      height="0.94em"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
