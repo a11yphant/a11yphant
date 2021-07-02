4fa3e743f59a95008580dc4ada9b25f989ea09a4
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-07-02 22:37:45 +0200

    Refactor(Button): use clsx for classes

--- services/site/src/components/buttons/Button.tsx
+++ services/site/src/components/buttons/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
@@ -10,13 +11,12 @@ interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes
 const Button: React.FunctionComponent<ButtonProps> = ({ full = false, icon, srText, className, overrideClassname = false, children, ...props }) => {
   return (
     <button
-      className={`
-      ${className}
-      ${full ? "bg-primary text-white" : ""} 
-      ${
+      className={clsx(
+        className,
+        full && "bg-primary text-white",
         !overrideClassname &&
-        "border-primary border-2 rounded px-4 py-2 tracking-wider inline-flex items-center transition duration-300 hover:text-white hover:bg-primaryDark hover:border-primaryDark focus:text-white focus:bg-primaryDark focus:border-primaryDark"
-      }`}
+          "inline-flex items-center px-4 py-2 border-primary border-2 rounded tracking-wider transition duration-300 hover:text-white hover:bg-primaryDark hover:border-primaryDark focus:text-white focus:bg-primaryDark focus:border-primaryDark",
+      )}
       {...props}
     >
       {children}
