fce7a0ddd36e38820b1a13e7c22ecf13f6f8b352
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-08-31 18:15:29 +0200

    refactor: give icons the same props

--- services/site/src/components/icons/Check.tsx
+++ services/site/src/components/icons/Check.tsx
@@ -3,15 +3,7 @@ import React from "react";
 
 const Check: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 60 46"
-      className={clsx(className)}
-      aria-hidden="true"
-      focusable="false"
-      {...props}
-    >
+    <svg fill="none" viewBox="0 0 60 46" className={clsx(className)} aria-hidden="true" focusable="false" {...props}>
       <path d="M4 21.622l17.297 16.756L56 4" stroke="currentColor" strokeWidth="10.16" />
     </svg>
   );
--- services/site/src/components/icons/Reset.tsx
+++ services/site/src/components/icons/Reset.tsx
@@ -10,7 +10,10 @@ const Reset: React.FunctionComponent<React.SVGProps<SVGSVGElement>> = ({ classNa
       aria-hidden="true"
       focusable="false"
       className={clsx(
-        "mr-2 ml-1 h-5 w-5 text-grey transition duration-300 group group-hover:text-primary-light group-focus:text-primary-light",
+        "mr-2 ml-1 h-5 w-5 text-grey",
+        "group transition duration-300",
+        "group-hover:text-primary-light",
+        "group-focus:text-primary-light",
         className,
       )}
       {...props}
