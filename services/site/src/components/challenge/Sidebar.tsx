65b738563892c5ec8023baf3120765f239687549
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-01 11:12:46 +0200

    add: padding to sidebar component

--- services/site/src/components/challenge/Sidebar.tsx
+++ services/site/src/components/challenge/Sidebar.tsx
@@ -13,8 +13,8 @@ interface SidebarProps {
 
 const Sidebar: React.FunctionComponent<SidebarProps> = ({ className, challengeName, level }) => {
   return (
-    <aside className={clsx("w-sidebar py-4 px-7", "container-dark overflow-auto", className)}>
-      <div className="w-full min-h-full flex flex-col">
+    <aside className={clsx("w-sidebar py-4 px-7", "container-dark", className)}>
+      <div className="w-full h-full flex flex-col overflow-auto">
         <h2 className={clsx("text-grey-middle", "h6")}>{challengeName}</h2>
         <h3 className={clsx("my-8", "h4")}>Instructions</h3>
         <p className={clsx("whitespace-pre-wrap", "prose")} dangerouslySetInnerHTML={{ __html: sanitizeHtml(level.instructions) }} />
