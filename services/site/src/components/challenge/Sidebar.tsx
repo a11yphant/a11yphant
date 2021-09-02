c57380178c7511f319010dc18386ad248b622f93
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-02 12:13:53 +0200

    refactor: extract overlay into component

--- services/site/src/components/challenge/Sidebar.tsx
+++ services/site/src/components/challenge/Sidebar.tsx
@@ -1,7 +1,7 @@
+import ScrollOverlayWrapper from "app/components/common/scrollOverlayWrapper";
 import { Level } from "app/generated/graphql";
 import clsx from "clsx";
-import React, { useCallback, useState } from "react";
-import { useResizeDetector } from "react-resize-detector";
+import React from "react";
 import sanitizeHtml from "sanitize-html";
 
 import HintList from "./sidebar/HintList";
@@ -13,50 +13,18 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FunctionComponent<SidebarProps> = ({ className, challengeName, level }) => {
-  const [isVisibleBottom, setIsVisibleBottom] = useState(false);
-  const [isVisibleTop, setIsVisibleTop] = useState(false);
-
-  const wrapperRef = React.useRef<HTMLDivElement>();
-
-  // show overlay when content is scroll-able
-  React.useEffect(() => {
-    if (wrapperRef.current?.scrollHeight > wrapperRef.current?.clientHeight) {
-      setIsVisibleBottom(true);
-    }
-  }, []);
-
-  const listenToScroll = useCallback(() => {
-    // remove gradient when scrolled to bottom
-    if (wrapperRef.current?.clientHeight === wrapperRef.current?.scrollHeight - Math.abs(wrapperRef.current?.scrollTop)) {
-      isVisibleBottom && // to limit setting state only the first time
-        setIsVisibleBottom(false);
-    } else {
-      setIsVisibleBottom(true);
-    }
-
-    // add gradient on top when scrolled to bottom
-    if (wrapperRef.current?.scrollTop > 0) {
-      setIsVisibleTop(true);
-    } else {
-      setIsVisibleTop(false);
-    }
-  }, [wrapperRef, isVisibleBottom]);
-
-  useResizeDetector({
-    targetRef: wrapperRef,
-    onResize: listenToScroll,
-  });
-
   return (
     <aside className={clsx("w-sidebar py-4", "container-dark", className)}>
-      <div onScroll={listenToScroll} ref={wrapperRef} className="w-full h-full px-7 pb-1 flex flex-col overflow-auto">
-        {isVisibleTop && <div className={clsx("w-sidebar h-28 top-20 rotate-180", "custom-overlay")} />}
+      <ScrollOverlayWrapper
+        className={"w-full h-full px-7 pb-1 flex flex-col overflow-auto"}
+        classNameTop={"w-sidebar h-28 top-20 rotate-180"}
+        classNameBottom={"w-sidebar h-44"}
+      >
         <h2 className={clsx("text-grey-middle", "h6")}>{challengeName}</h2>
         <h3 className={clsx("my-8", "h4")}>Instructions</h3>
         <p className={clsx("whitespace-pre-wrap", "prose")} dangerouslySetInnerHTML={{ __html: sanitizeHtml(level.instructions) }} />
         <HintList tasks={level.tasks} />
-        {isVisibleBottom && <div className={clsx("w-sidebar h-44", "custom-overlay")} />}
-      </div>
+      </ScrollOverlayWrapper>
     </aside>
   );
 };
