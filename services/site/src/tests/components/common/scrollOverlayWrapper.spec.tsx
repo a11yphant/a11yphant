40f6d9292a75faff4c3f36eb061c674eac831422
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-02 15:49:42 +0200

    add: test for srollOverlayWrapper

---
+++ services/site/src/tests/components/common/scrollOverlayWrapper.spec.tsx
@@ -0,0 +1,49 @@
+import { cleanup } from "@testing-library/react";
+import ScrollOverlayWrapper from "app/components/common/scrollOverlayWrapper";
+import { shallow } from "enzyme";
+import React from "react";
+
+afterEach(cleanup);
+
+jest.mock("react-resize-detector", () => ({
+  useResizeDetector: () => {
+    return;
+  },
+}));
+
+describe("Scroll Overlay Wrapper", () => {
+  it("renders Wrapper div", () => {
+    const wrapper = shallow(<ScrollOverlayWrapper />);
+
+    expect(wrapper.find("div")).toBeTruthy();
+  });
+
+  //   TODO: write more tests when you know how to
+  //   it("renders both overlays", () => {
+  //     const wrapper = mount(<ScrollOverlayWrapper />);
+  //     expect(wrapper.find("div.custom-overlay").length).toBe(2);
+  //   });
+
+  //   it("renders only top overlay", () => {
+  //     const wrapper = shallow(<ScrollOverlayWrapper displayBottom={false} />);
+  //     expect(wrapper.find("div.custom-overlay").length).toBe(1);
+  //   });
+
+  //   it("renders only bottom overlay", async () => {
+  //     const wrapper = mount(<ScrollOverlayWrapper displayTop={false} />);
+
+  //     jest.spyOn(wrapper.getDOMNode(), "scrollHeight", "get").mockImplementation(() => 100);
+
+  //     await act(async () => {
+  //       wrapper.update();
+  //     });
+  //     // wrapper.find("div").first().getDOMNode().scrollHeight = 100;
+
+  //     expect(wrapper.find(".custom-overlay").length).toBe(1);
+  //   });
+
+  it("renders no overlay", () => {
+    const wrapper = shallow(<ScrollOverlayWrapper displayTop={false} displayBottom={false} />);
+    expect(wrapper.find("div.custom-overlay").length).toBe(0);
+  });
+});
