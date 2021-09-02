078dc89a742320b38605d1e5ccdc3cbe11e8044a
Michael Brandstätter <mbrandstaetter.mmt-m2020@fh-salzburg.ac.at>
2021-09-02 18:18:06 +0200

    feat(conf): add defaults for github client cred

--- services/api/src/config/oauth.config.ts
+++ services/api/src/config/oauth.config.ts
@@ -2,8 +2,8 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("oauth", () => ({
   github: {
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    clientID: process.env.GITHUB_CLIENT_ID || "demo_id", // clientID needs to be set to something or class will not be instantiated
+    clientSecret: process.env.GITHUB_CLIENT_SECRET || "demo_secret",
     callbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback",
   },
 }));
