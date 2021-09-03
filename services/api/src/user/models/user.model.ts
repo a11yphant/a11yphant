5ffc9f71938262a4aa1110058fd9141a668f274e
Michael Brandstätter <mbrandstaetter.mmt-m2020@fh-salzburg.ac.at>
2021-09-03 15:42:04 +0200

    feat(user): adapt user model for oauth

--- services/api/src/user/models/user.model.ts
+++ services/api/src/user/models/user.model.ts
@@ -1,6 +1,12 @@
 export class User {
-  constructor(properties: { id: string }) {
+  constructor(properties: { id: string; authId?: string; authProvider?: string }) {
     this.id = properties.id;
+    this.authProvider = properties.authProvider || "anonymous";
+    this.authId = properties.authId;
   }
   id: string;
+
+  authProvider: string;
+
+  authId?: string;
 }
