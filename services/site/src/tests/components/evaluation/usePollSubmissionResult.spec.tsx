cbe8d9cf2d35865767c7e356483b7a8392745db0
Thomas Dax <thomas.dax@vivid-planet.com>
2021-09-03 12:27:41 +0200

    test(usePollSubmissionResult): add test for interval call of query in usePollSubmissionResult

--- services/site/src/tests/components/evaluation/usePollSubmissionResult.spec.tsx
+++ services/site/src/tests/components/evaluation/usePollSubmissionResult.spec.tsx
@@ -9,7 +9,15 @@ import React from "react";
 
 afterEach(cleanup);
 
-const submissionId = "6e19e948-e022-4167-a82c-423feaf0d03a";
+// jest.mock("app/generated/graphql", () => {
+//   return {
+//     ...jest.requireActual<NodeJS.Dict<unknown>>("app/generated/graphql"),
+//     useResultForSubmissionLazyQuery: jest.fn(),
+//   };
+// });
+
+const mockFailSubmissionId = "6e19e948-e022-4167-a82c-423feaf0d03a";
+const mockPendingSubmissionId = "51fc32e4-12a0-4dde-abc4-d3fcf5611351";
 const mockRequirements = [
   {
     __typename: "RequirementResult",
@@ -37,19 +45,32 @@ const mockRequirements = [
   },
 ];
 
+const mockedPendingDataProvider = jest.fn(() => ({
+  data: {
+    resultForSubmission: {
+      id: mockPendingSubmissionId,
+      status: ResultStatus.Pending,
+      numberOfCheckedRequirements: 0,
+      numberOfFailedRequirementChecks: 0,
+      requirements: [],
+      __typename: "Result",
+    },
+  },
+}));
+
 const mocks: MockedResponse[] = [
   {
     request: {
       query: ResultForSubmissionDocument,
       variables: {
-        id: submissionId,
+        id: mockFailSubmissionId,
       },
     },
     result: {
       data: {
         resultForSubmission: {
           __typename: "Result",
-          id: submissionId,
+          id: mockFailSubmissionId,
           status: ResultStatus.Fail,
           numberOfCheckedRequirements: 2,
           numberOfFailedRequirementChecks: 1,
@@ -57,6 +78,15 @@ const mocks: MockedResponse[] = [
         },
       },
     },
+  },
+  {
+    request: {
+      query: ResultForSubmissionDocument,
+      variables: {
+        id: mockPendingSubmissionId,
+      },
+    },
+    newData: mockedPendingDataProvider,
   },
 ];
 
@@ -68,7 +98,7 @@ describe("usePollSubmissionResult", () => {
   it("returns correct status, requirements and totalScore", async () => {
     jest.useFakeTimers();
 
-    const { result, waitForNextUpdate } = renderHook(() => usePollSubmissionResult(submissionId), { wrapper });
+    const { result, waitForNextUpdate } = renderHook(() => usePollSubmissionResult(mockFailSubmissionId), { wrapper });
     await act(async () => {
       await waitForNextUpdate();
     });
@@ -82,5 +112,26 @@ describe("usePollSubmissionResult", () => {
     expect(result.current.status).toBe(ResultStatus.Fail);
     expect(result.current.requirements).toEqual(mockRequirements);
     expect(result.current.totalScore).toBe(50);
+  });
+
+  it("queries every 3 seconds", async () => {
+    jest.useFakeTimers();
+
+    const { waitForNextUpdate } = renderHook(() => usePollSubmissionResult(mockPendingSubmissionId), { wrapper });
+    await act(async () => {
+      await waitForNextUpdate();
+    });
+    // initial call in useEffect
+    expect(mockedPendingDataProvider).toHaveBeenCalledTimes(1);
+
+    // call in interval after 3 seconds
+    jest.advanceTimersByTime(3000);
+    await waitForNextUpdate();
+    expect(mockedPendingDataProvider).toHaveBeenCalledTimes(2);
+
+    // call again in interval after another 3 seconds
+    jest.advanceTimersByTime(3000);
+    await waitForNextUpdate();
+    expect(mockedPendingDataProvider).toHaveBeenCalledTimes(3);
   });
 });
