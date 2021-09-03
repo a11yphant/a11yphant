fd735bf2799963e02d3698f04e44a9489cc27c2b
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-03 12:28:40 +0200

    add: small screen notification

--- services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel].tsx
+++ services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel].tsx
@@ -2,6 +2,7 @@ import ButtonLoading from "app/components/buttons/ButtonLoading";
 import Editors, { EditorLanguage } from "app/components/challenge/Editors";
 import Preview from "app/components/challenge/Preview";
 import Sidebar from "app/components/challenge/Sidebar";
+import IllustrationCoding from "app/components/icons/IllustrationCoding";
 import {
   ChallengeBySlugDocument,
   ChallengeBySlugQuery,
@@ -14,8 +15,10 @@ import {
   useLevelByChallengeSlugQuery,
 } from "app/generated/graphql";
 import { initializeApollo } from "app/lib/apollo-client";
+import clsx from "clsx";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 
@@ -118,6 +121,28 @@ const Level: React.FunctionComponent = () => {
     });
   }
 
+  const smallScreenNotification = (
+    <>
+      <section className={clsx("mx-8 py-10 h-full flex flex-col justify-center items-left", "md:hidden")}>
+        <IllustrationCoding className={clsx("max-w-xs mb-12 self-start", "sm:mb-12 sm:max-w-sm")} />
+        <h2 className={clsx("mb-8", "h3", "sm:h2")}>Your device is too small</h2>
+        <p>Please use a tablet or desktop device with a larger screen.</p>
+        <Link href="/">
+          <a
+            className={clsx(
+              "w-max mt-4 px-4 py-2 font-normal bg-primary text-white border-primary border-2 rounded tracking-wider inline-flex items-center",
+              "transition duration-300",
+              "hover:text-white hover:bg-primary-dark hover:border-primary-dark",
+              "focus:text-white focus:bg-primary-dark focus:border-primary-dark",
+            )}
+          >
+            Go to homepage
+          </a>
+        </Link>
+      </section>
+    </>
+  );
+
   return (
     <>
       <Head>
@@ -125,9 +150,10 @@ const Level: React.FunctionComponent = () => {
           {challenge.name} - Level {nthLevel}
         </title>
       </Head>
-      <main className="h-main p-4 flex justify-between box-border">
-        <Sidebar className="h-full" challengeName={challenge.name} level={level} />
-        <div className="h-full pl-4 relative box-border flex justify-between flex-col flex-auto">
+      <main className={clsx("h-main md:p-4 md:flex md:justify-between md:box-border")}>
+        {smallScreenNotification}
+        <Sidebar className={clsx("h-full", "hidden md:block")} challengeName={challenge.name} level={level} />
+        <div className={clsx("h-full pl-4 relative box-border flex justify-between flex-col flex-auto", "hidden md:block")}>
           <Editors
             onReset={resetToInitialCode}
             className="w-full h-3/5"
