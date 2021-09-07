c7609c50b45cc61ca44437dcb965b4033e4048d6
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-09-07 10:56:38 +0200

    test(site): update test names

--- services/site/src/tests/components/homepage/Hero.spec.tsx
+++ services/site/src/tests/components/homepage/Hero.spec.tsx
@@ -6,21 +6,21 @@ import Hero from "app/components/homepage/Hero";
 afterEach(cleanup);
 
 describe("Hero", () => {
-  it("has two level two headings", async () => {
+  it("renders two headings", async () => {
     const root = render(<Hero />);
 
     expect(await root.findAllByRole("heading", { level: 2 })).toHaveLength(2);
   });
 
-  it("has a link to challenges", async () => {
+  it("renders a link to the challenges section", async () => {
     const root = render(<Hero />);
 
     expect(await root.findByText("Start Coding")).toHaveAttribute("href", "/#challenges");
   });
 
-  it("has a link to challenges", async () => {
+  it("renders a link for the github sign up", async () => {
     const root = render(<Hero />);
 
-    expect(await root.findByText("Sign Up via Github")).toHaveProperty("href", "http://localhost/auth/github");
+    expect(await root.findByText("Sign up via Github")).toHaveProperty("href", "http://localhost/auth/github");
   });
 });
