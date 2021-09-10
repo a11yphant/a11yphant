4034edabefd13ff303fcba9419bf2f8992ebd4c2
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-09-10 10:52:45 +0200

    feat(site): fix hero in xl breakpoint

--- services/site/src/components/homepage/Hero.tsx
+++ services/site/src/components/homepage/Hero.tsx
@@ -6,9 +6,9 @@ import Github from "../icons/Github";
 
 const Hero: React.FC = () => (
   <div
-    className={clsx("max-w-screen-3xl mx-8 mt-12 mb-24 flex flex-col", "sm:m-12", "md:mx-24", "xl:flex-row xl:items-center xl:h-main", "2xl:mx-auto")}
+    className={clsx("max-w-screen-3xl mx-8 mt-12 mb-24 flex flex-col", "sm:m-12", "md:mx-24", "xl:h-main xl:flex-row xl:items-center", "2xl:mx-auto")}
   >
-    <section className={clsx("flex flex-col justify-center mb-24", "xl:flex-shrink xl:mb-0", "2xl:mx-24")}>
+    <section className={clsx("flex flex-col justify-center mb-24", "xl:flex-shrink xl:flex-grow xl:mb-0", "2xl:ml-24")}>
       <h2 className={clsx("mb-6 max-w-2xl font-bold font-sans text-3xl leading-tight", "sm:text-4xl sm:leading-snug", "xl:text-5xl xl:leading-snug")}>
         Learning web accessibility made easy
       </h2>
@@ -36,6 +36,7 @@ const Hero: React.FC = () => (
         "md:px-12 md:py-10",
         "lg:items-center lg:px-16 lg:py-14",
         "xl:block xl:px-20 xl:py-16 xl:max-w-xl xl:ml-12 xl:self-center xl:flex-shrink-0",
+        "2xl:mr-24",
       )}
     >
       <h2
