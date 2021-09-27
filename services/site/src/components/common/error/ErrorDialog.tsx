54bf1707316ef054322cf36f5d000825e84892f5
Thomas Dax <thomas.dax@vivid-planet.com>
2021-09-27 17:41:07 +0200

    refactor(ErrorDialog): rebuild error dialog using headless ui

--- services/site/src/components/common/error/ErrorDialog.tsx
+++ services/site/src/components/common/error/ErrorDialog.tsx
@@ -1,3 +1,4 @@
+import { Dialog, Transition } from "@headlessui/react";
 import Button from "app/components/buttons/Button";
 import { ApolloErrorResponse } from "app/components/common/error/useErrorDialog";
 import Exclamation from "app/components/icons/Exclamation";
@@ -29,63 +30,72 @@ const ErrorDialog: React.FunctionComponent<ErrorDialogProps> = ({ open, onClose,
   }, []);
 
   return (
-    <div
-      onClick={onClose}
-      className={clsx("z-10 inset-0 overflow-y-auto bg-grey-dark bg-opacity-50 flex justify-center items-center", open ? "fixed" : "hidden")}
-      aria-hidden
+    <Transition
+      show={open}
+      enter="transition duration-100 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-75 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0"
     >
-      <div
-        className={clsx(
-          "min-w-[25%] max-w-[50%] min-h-[16rem] max-h-screen overflow-auto px-12 py-14 relative bg-background-light rounded-lg",
-          "shadow-modal",
-          "border border-4 border-error-dark",
-        )}
-      >
-        <Button
-          onClick={onClose}
-          overrideClassName
-          className={clsx("absolute top-6 right-6", "transition-colors duration-300", "hover:text-error", "focus:text-error")}
+      <Dialog open={open} onClose={() => onClose()} className={clsx("fixed z-10 inset-0 overflow-y-auto h-screen flex items-center justify-center")}>
+        <div
+          className={clsx(
+            "p-8 bg-background-light rounded-lg min-w-max max-w-xl flex flex-col relative",
+            "md:px-12 md:py-10 md:min-w-[32rem] md:mx-auto",
+            "lg:px-16 lg:py-14",
+            "xl:px-20 xl:py-16",
+            "border border-4 border-error-dark",
+          )}
         >
-          <span className="sr-only">Close</span>
-          <X className="w-3.5 h-3.5" />
-        </Button>
-
-        <div className="flex">
-          <Exclamation className={clsx("w-8 h-8 mr-2", "text-error-dark")} />
-          <h2 className={clsx("text-2xl")}>{title}</h2>
-        </div>
-
-        <div className="p-4">
-          <ul>
-            {messages.map((message, idx) => (
-              <li key={idx}>{message}</li>
-            ))}
-          </ul>
-        </div>
-
-        {process.env.NODE_ENV === "development" && (
-          <div className="p-8 border border-error">
-            <strong className="text-error">Original Error (Only shown in Development Mode): </strong>
-            <p>{JSON.stringify(errorResponse)}</p>
-          </div>
-        )}
-
-        <div className="absolute bottom-7 right-7">
+          <Dialog.Overlay className="fixed inset-0 bg-background opacity-25 z-[-1]" />
           <Button
-            onClick={onClose}
+            onClick={() => onClose()}
             overrideClassName
-            className={clsx(
-              "px-6 py-4 leading-4 inline-flex items-center rounded tracking-wider transition duration-300",
-              "hover:text-white hover:bg-error hover:border-error",
-              "focus:text-white focus:bg-error focus:border-error",
-              "bg-error-dark",
-            )}
+            className={clsx("w-11 h-11 p-3.5 absolute top-4 right-4", "transition-colors duration-300", "hover:text-error", "focus:text-error")}
           >
-            Okay
+            <span className="sr-only">Close</span>
+            <X />
           </Button>
+
+          <div className="flex">
+            <Exclamation className={clsx("w-8 h-8 mr-2", "text-error-dark")} />
+            <Dialog.Title className={clsx("mb-8", "h4")}>{title}</Dialog.Title>
+          </div>
+
+          <div className="p-4">
+            <ul>
+              {messages.map((message, idx) => (
+                <li key={idx}>{message}</li>
+              ))}
+            </ul>
+          </div>
+
+          {process.env.NODE_ENV === "development" && (
+            <div className="p-8 border border-error">
+              <strong className="text-error">Original Error (Only shown in Development Mode): </strong>
+              <p>{JSON.stringify(errorResponse)}</p>
+            </div>
+          )}
+
+          <div className="absolute bottom-7 right-7">
+            <Button
+              onClick={onClose}
+              overrideClassName
+              className={clsx(
+                "px-6 py-4 leading-4 inline-flex items-center rounded tracking-wider transition duration-300",
+                "hover:text-white hover:bg-error hover:border-error",
+                "focus:text-white focus:bg-error focus:border-error",
+                "bg-error-dark",
+              )}
+            >
+              Okay
+            </Button>
+          </div>
         </div>
-      </div>
-    </div>
+      </Dialog>
+    </Transition>
   );
 };
 
