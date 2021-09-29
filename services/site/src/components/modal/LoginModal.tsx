9038042ea5a5bf0cadc2188fbcc8659d347c7009
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-29 09:48:17 +0200

    fix: focus styling issue after opening the modal

--- services/site/src/components/modal/LoginModal.tsx
+++ services/site/src/components/modal/LoginModal.tsx
@@ -54,7 +54,7 @@ const LoginModal: React.FunctionComponent<LoginModalProps> = ({
               "w-11 h-11 p-3.5 absolute top-4 right-4",
               "transition-colors duration-300",
               "hover:text-primary-light",
-              "focus:text-primary-light",
+              "focus-visible:text-primary-light",
             )}
           >
             <span className="sr-only">Close</span>
