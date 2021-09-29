d6927beefef3d7c6d3688cb1a83c97a4ce01697a
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-29 13:58:59 +0200

    add: prepare for inner html

--- services/site/src/pages/quiz.tsx
+++ services/site/src/pages/quiz.tsx
@@ -77,7 +77,8 @@ const Quiz: React.FunctionComponent = () => {
         >
           <h2 className={clsx("mb-2", "h5")}>Quiz</h2>
           <div className={clsx("grid grid-cols-7")}>
-            <h3 className={clsx("h2 leading-tight tracking-wider font-mono col-span-4 mr-8")}>What is the purpose of the head tag?</h3>
+            {/* TODO:: dangerouslySetInnerHTML={{ __html: sanitizeHtml(level.instructions) }} */}
+            <h3 className={clsx("h2 leading-tight tracking-wider font-mono col-span-4 mr-8", "prose")}>What is the purpose of the head tag?</h3>
             <div className={clsx("col-span-3")}>
               <SingleAnswer srTitle={"Possible answers to the quiz"} answers={mockAnswers} />
             </div>
