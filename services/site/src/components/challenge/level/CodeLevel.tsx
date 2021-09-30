cee0163408d813caf7ab8daeff4fee72f51a387a
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-09-30 14:46:19 +0200

    refactor: extract coding level into component

---
+++ services/site/src/components/challenge/level/CodeLevel.tsx
@@ -0,0 +1,170 @@
+import ButtonLoading from "app/components/buttons/ButtonLoading";
+import Editors, { EditorLanguage } from "app/components/challenge/Editors";
+import Preview from "app/components/challenge/Preview";
+import Sidebar from "app/components/challenge/Sidebar";
+import { CodeLevelDetailsFragment, useRequestCheckMutation } from "app/generated/graphql";
+import { useSubmissionAutoSave } from "app/hooks/useSubmissionAutoSave";
+import clsx from "clsx";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+interface CodeLevelProps {
+  challengeName: string;
+  level: CodeLevelDetailsFragment;
+  onAutoSaveLoadingChange: (autoSaveLoading: boolean) => void;
+}
+
+const CodeLevel = ({ challengeName, level, onAutoSaveLoadingChange }: CodeLevelProps): React.ReactElement => {
+  const router = useRouter();
+
+  const {
+    setLevelId,
+    submissionId,
+    setSubmissionId,
+    setSubmissionCode,
+    submissionCode,
+    updateSubmission,
+    loading: autoSaveLoading,
+  } = useSubmissionAutoSave();
+
+  React.useEffect(() => {
+    onAutoSaveLoadingChange(autoSaveLoading);
+  }, [autoSaveLoading, onAutoSaveLoadingChange]);
+
+  React.useEffect(() => {
+    setLevelId(level.id);
+    if (level?.lastSubmission) {
+      setSubmissionCode({
+        html: level.lastSubmission.html,
+        js: level.lastSubmission.js,
+        css: level.lastSubmission.css,
+      });
+
+      if (level.lastSubmission.result) {
+        return;
+      }
+
+      setSubmissionId(level.lastSubmission.id);
+    } else {
+      setSubmissionCode({
+        html: level.code?.html,
+        js: level.code?.js,
+        css: level.code?.css,
+      });
+    }
+  }, [level, setLevelId, setSubmissionCode, setSubmissionId]);
+
+  const [requestCheckMutation] = useRequestCheckMutation();
+
+  const [showSubmitLoadingAnimation, setShowSubmitLoadingAnimation] = useState(false);
+
+  const resetToInitialCode = (language?: EditorLanguage): void => {
+    if (language) {
+      setSubmissionCode({
+        ...submissionCode,
+        [language]: level.code?.[language],
+      });
+    } else {
+      setSubmissionCode({
+        html: level.code?.html,
+        js: level.code?.js,
+        css: level.code?.css,
+      });
+    }
+  };
+
+  const submitLevel = async (): Promise<void> => {
+    setShowSubmitLoadingAnimation(true);
+
+    await updateSubmission();
+
+    await requestCheckMutation({
+      variables: { requestCheckInput: { submissionId } },
+    });
+
+    router.push(`${router.asPath}/evaluation/${submissionId}`);
+  };
+
+  const editorConfiguration = [];
+
+  if (level.hasHtmlEditor) {
+    editorConfiguration.push({
+      languageLabel: "HTML",
+      language: EditorLanguage.html,
+      code: submissionCode?.html,
+      updateCode: (html) => {
+        setSubmissionCode({ ...submissionCode, html });
+      },
+      heading: "index.html",
+    });
+  }
+
+  if (level.hasCssEditor) {
+    editorConfiguration.push({
+      languageLabel: "CSS",
+      language: EditorLanguage.css,
+      code: submissionCode?.css,
+      updateCode: (css) => {
+        setSubmissionCode({ ...submissionCode, css });
+      },
+      heading: "index.css",
+    });
+  }
+
+  if (level.hasJsEditor) {
+    editorConfiguration.push({
+      languageLabel: "JavaScript",
+      language: EditorLanguage.javascript,
+      code: submissionCode?.js,
+      updateCode: (js) => {
+        setSubmissionCode({ ...submissionCode, js });
+      },
+      heading: "index.js",
+    });
+  }
+
+  return (
+    <>
+      <Sidebar className={clsx("h-full hidden", "lg:block")} challengeName={challengeName} level={level} />
+      <section className={clsx("w-full hidden", "lg:flex")}>
+        <div className="h-full pl-4 relative box-border justify-between flex-col flex-auto">
+          <Editors
+            onReset={resetToInitialCode}
+            className="w-full h-3/5"
+            editors={editorConfiguration}
+            theme="light"
+            options={{
+              lineHeight: 24,
+              fontSize: 12,
+              wordWrap: "on",
+              minimap: {
+                enabled: false,
+              },
+            }}
+          />
+          <Preview
+            className="w-full h-2/5"
+            heading="Preview"
+            htmlCode={submissionCode?.html}
+            cssCode={submissionCode?.css}
+            javascriptCode={submissionCode?.js}
+          />
+          <div className="absolute right-0 bottom-0 pt-2 pl-2 pr-0 pb-0 bg-background border-light border-t-2 border-l-2 rounded-tl-xl">
+            <ButtonLoading
+              primary
+              onClick={submitLevel}
+              className="px-10"
+              loading={showSubmitLoadingAnimation}
+              submitButton
+              srTextLoading="The submission is being processed."
+            >
+              Submit
+            </ButtonLoading>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CodeLevel;
--- services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel].tsx
+++ services/site/src/pages/challenge/[challengeSlug]/level/[nthLevel].tsx
@@ -1,8 +1,7 @@
 import { Transition } from "@headlessui/react";
-import ButtonLoading from "app/components/buttons/ButtonLoading";
-import Editors, { EditorLanguage } from "app/components/challenge/Editors";
-import Preview from "app/components/challenge/Preview";
-import Sidebar from "app/components/challenge/Sidebar";
+import { isCodeLevel, isQuizLevel } from "app/components/challenge/helpers";
+import CodeLevel from "app/components/challenge/level/CodeLevel";
+import QuizLevel from "app/components/challenge/level/QuizLevel";
 import SmallScreenNotification from "app/components/common/SmallScreenNotification";
 import LoadingIndicator from "app/components/icons/LoadingIndicator";
 import Navigation from "app/components/Navigation";
@@ -15,146 +14,56 @@ import {
   LevelByChallengeSlugQueryVariables,
   useChallengeBySlugQuery,
   useLevelByChallengeSlugQuery,
-  useRequestCheckMutation,
 } from "app/generated/graphql";
-import { useSubmissionAutoSave } from "app/hooks/useSubmissionAutoSave";
 import { initializeApollo } from "app/lib/apollo-client";
 import { getServerSideCurrentUser } from "app/lib/server-side-props/get-current-user";
 import clsx from "clsx";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Level: React.FunctionComponent = () => {
   const router = useRouter();
   const { challengeSlug, nthLevel } = router.query;
-
-  const {
-    setLevelId,
-    submissionId,
-    setSubmissionId,
-    setSubmissionCode,
-    submissionCode,
-    updateSubmission,
-    loading: autoSaveLoading,
-  } = useSubmissionAutoSave();
-
-  const {
-    loading,
-    data: { level },
-  } = useLevelByChallengeSlugQuery({
-    variables: { challengeSlug: challengeSlug as string, nth: Number(nthLevel) },
-    onCompleted: ({ level }) => {
-      if (level.__typename !== "CodeLevel") return;
-
-      setLevelId(level.id);
-      if (level?.lastSubmission) {
-        setSubmissionCode({
-          html: level.lastSubmission.html,
-          js: level.lastSubmission.js,
-          css: level.lastSubmission.css,
-        });
-
-        if (level.lastSubmission.result) {
-          return;
-        }
-
-        setSubmissionId(level.lastSubmission.id);
-      } else {
-        setSubmissionCode({
-          html: level.code?.html,
-          js: level.code?.js,
-          css: level.code?.css,
-        });
-      }
-    },
-  });
+  const [autoSaveLoading, setAutoSaveLoading] = React.useState<boolean>(false);
 
   const {
     loading: loadingChallenge,
     data: { challenge },
   } = useChallengeBySlugQuery({ variables: { slug: challengeSlug as string } });
 
-  const [requestCheckMutation] = useRequestCheckMutation();
-
-  const [showSubmitLoadingAnimation, setShowSubmitLoadingAnimation] = useState(false);
-
-  useEffect(() => {
-    if (level.__typename === "QuizLevel") {
-      router.push(`/challenge/${challengeSlug as string}/level/${parseInt(nthLevel as string, 10) + 1}`);
-    }
-  }, [level, nthLevel, challengeSlug, router]);
-  if (level.__typename !== "CodeLevel") return null;
-
-  const resetToInitialCode = (language?: EditorLanguage): void => {
-    if (language) {
-      setSubmissionCode({
-        ...submissionCode,
-        [language]: level.code?.[language],
-      });
-    } else {
-      setSubmissionCode({
-        html: level.code?.html,
-        js: level.code?.js,
-        css: level.code?.css,
-      });
-    }
-  };
-
-  const submitLevel = async (): Promise<void> => {
-    setShowSubmitLoadingAnimation(true);
-
-    await updateSubmission();
-
-    await requestCheckMutation({
-      variables: { requestCheckInput: { submissionId } },
-    });
-
-    router.push(`${router.asPath}/evaluation/${submissionId}`);
-  };
+  const {
+    loading,
+    data: { level },
+  } = useLevelByChallengeSlugQuery({
+    variables: { challengeSlug: challengeSlug as string, nth: Number(nthLevel) },
+  });
 
   if (loading || loadingChallenge) {
     return <div>Loading ...</div>;
   }
 
-  const editorConfiguration = [];
-
-  if (level.hasHtmlEditor) {
-    editorConfiguration.push({
-      languageLabel: "HTML",
-      language: EditorLanguage.html,
-      code: submissionCode?.html,
-      updateCode: (html) => {
-        setSubmissionCode({ ...submissionCode, html });
-      },
-      heading: "index.html",
-    });
-  }
-
-  if (level.hasCssEditor) {
-    editorConfiguration.push({
-      languageLabel: "CSS",
-      language: EditorLanguage.css,
-      code: submissionCode?.css,
-      updateCode: (css) => {
-        setSubmissionCode({ ...submissionCode, css });
-      },
-      heading: "index.css",
-    });
-  }
-
-  if (level.hasJsEditor) {
-    editorConfiguration.push({
-      languageLabel: "JavaScript",
-      language: EditorLanguage.javascript,
-      code: submissionCode?.js,
-      updateCode: (js) => {
-        setSubmissionCode({ ...submissionCode, js });
-      },
-      heading: "index.js",
-    });
-  }
+  // --------------------- QUIZ
+  const mockQuestion = "What is the purpose of the head tag?";
+  const mockAnswers = [
+    {
+      id: "1",
+      text: "This tag does not exist in the HTML specification.",
+    },
+    {
+      id: "2",
+      text: "It contains meta information regarding the page, for example the title.",
+    },
+    {
+      id: "3",
+      text: "It contains the content of the page.",
+    },
+    {
+      id: "4",
+      text: "This tag should contain the website's logo.",
+    },
+  ];
 
   return (
     <>
@@ -178,44 +87,8 @@ const Level: React.FunctionComponent = () => {
       </Navigation>
       <main className={clsx("h-main", "md:p-4 md:flex md:justify-between md:box-border")}>
         <SmallScreenNotification />
-        <Sidebar className={clsx("h-full hidden", "lg:block")} challengeName={challenge.name} level={level} />
-        <section className={clsx("w-full hidden", "lg:flex")}>
-          <div className="h-full pl-4 relative box-border justify-between flex-col flex-auto">
-            <Editors
-              onReset={resetToInitialCode}
-              className="w-full h-3/5"
-              editors={editorConfiguration}
-              theme="light"
-              options={{
-                lineHeight: 24,
-                fontSize: 12,
-                wordWrap: "on",
-                minimap: {
-                  enabled: false,
-                },
-              }}
-            />
-            <Preview
-              className="w-full h-2/5"
-              heading="Preview"
-              htmlCode={submissionCode?.html}
-              cssCode={submissionCode?.css}
-              javascriptCode={submissionCode?.js}
-            />
-            <div className="absolute right-0 bottom-0 pt-2 pl-2 pr-0 pb-0 bg-background border-light border-t-2 border-l-2 rounded-tl-xl">
-              <ButtonLoading
-                primary
-                onClick={submitLevel}
-                className="px-10"
-                loading={showSubmitLoadingAnimation}
-                submitButton
-                srTextLoading="The submission is being processed."
-              >
-                Submit
-              </ButtonLoading>
-            </div>
-          </div>
-        </section>
+        {isCodeLevel(level) && <CodeLevel challengeName={challenge.name} level={level} onAutoSaveLoadingChange={setAutoSaveLoading} />}
+        {isQuizLevel(level) && <QuizLevel question={mockQuestion} answers={mockAnswers} />}
       </main>
     </>
   );
