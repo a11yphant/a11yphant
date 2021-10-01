b6a5025d686bdf304e537d213006ca67cd6d92f0
Michael Brandstätter <mbrandstaetter.mmt-m2020@fh-salzburg.ac.at>
2021-10-01 14:10:08 +0200

    feat(auth): add authenticationService

--- services/api/src/authentication/authentication.module.ts
+++ services/api/src/authentication/authentication.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from "@nestjs/config";
 import { UserModule } from "@/user/user.module";
 
 import { AuthenticationController } from "./authentication.controller";
+import { AuthenticationService } from "./authentication.service";
 import { HashService } from "./hash.service";
 import { JwtService } from "./jwt.service";
 import { SessionInterceptor } from "./session.interceptor";
@@ -13,7 +14,7 @@ import { TwitterStrategy } from "./strategies/twitter.strategy";
 
 @Module({
   imports: [ConfigModule, UserModule],
-  providers: [HashService, SessionInterceptor, JwtService, GithubStrategy, TwitterStrategy, StoreService, Logger],
+  providers: [AuthenticationService, GithubStrategy, HashService, JwtService, Logger, SessionInterceptor, StoreService, TwitterStrategy],
   controllers: [AuthenticationController],
   exports: [SessionInterceptor, HashService],
 })
