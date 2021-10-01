690bdf4c202cdf2b632469ddb9a11ede231ccd8b
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-10-01 15:04:22 +0200

    add(QuizLevel): success and error animations

--- services/site/src/components/challenge/level/QuizLevel.tsx
+++ services/site/src/components/challenge/level/QuizLevel.tsx
@@ -2,10 +2,12 @@ import ButtonLoading from "app/components/buttons/ButtonLoading";
 import SingleAnswer from "app/components/challenge/quiz/SingleAnswer";
 import { CompleteEvaluationButton } from "app/components/evaluation/CompleteEvaluationButton";
 import { ResultStatus, useSubmitQuizLevelAnswerMutation } from "app/generated/graphql";
+import correctAnimation from "app/lotties/correct_lottie.json";
+import failAnimation from "app/lotties/fail_lottie.json";
 import clsx from "clsx";
 import React from "react";
+import Lottie from "react-lottie";
 import sanitizeHtml from "sanitize-html";
-
 interface QuizLevelProps {
   question: string;
   answers: Array<{ id: string; text: string }>;
@@ -38,11 +40,55 @@ const QuizLevel: React.FunctionComponent<QuizLevelProps> = ({ levelId, question,
         <h2 className={clsx("mb-2", "h4")}>Quiz</h2>
         <div className={clsx("grid grid-cols-7")}>
           <h3
-            className={clsx("mr-8 leading-tight tracking-wider font-mono col-span-4 text-5xl", "h2 prose")}
+            className={clsx("mr-8 leading-tight tracking-wider font-mono col-span-4 text-5xl", "h2 prose", quizResult && "opacity-50")}
             dangerouslySetInnerHTML={{ __html: sanitizeHtml(question) }}
           />
           <div className={clsx("col-span-3")}>
-            <SingleAnswer srTitle={"Possible answers to the quiz"} answers={answers} chosenId={chosenId} onChooseId={setChosenId} />
+            {quizResult === undefined && (
+              <SingleAnswer srTitle={"Possible answers to the quiz"} answers={answers} chosenId={chosenId} onChooseId={setChosenId} />
+            )}
+            {quizResult?.status === ResultStatus.Fail && (
+              <div>
+                <p className="h2 leading-tight">
+                  Really?<br></br> Try Again!
+                </p>
+                <Lottie
+                  options={{
+                    loop: false,
+                    autoplay: true,
+                    animationData: failAnimation,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                  height={230}
+                  width={160}
+                  ariaRole={"img"}
+                  title={"A cross"}
+                  style={{ margin: "2rem 0 0" }}
+                />
+              </div>
+            )}
+            {quizResult?.status === ResultStatus.Success && (
+              <div>
+                <p className="h2 leading-tight">Correct!</p>
+                <Lottie
+                  options={{
+                    loop: false,
+                    autoplay: true,
+                    animationData: correctAnimation,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                  height={240}
+                  width={200}
+                  ariaRole={"img"}
+                  title={"A check mark"}
+                  style={{ margin: "2rem 0 0" }}
+                />
+              </div>
+            )}
           </div>
         </div>
         <div className="flex justify-end mr-[-3rem]">
