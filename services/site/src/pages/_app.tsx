681f4e5c857e5bf4c3f78ab92fa0bc3ecfd1ad80
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-10-05 14:52:09 +0200

    refactor: move scroll overlay to each individual page

--- services/site/src/pages/_app.tsx
+++ services/site/src/pages/_app.tsx
@@ -5,10 +5,8 @@ import "app/styles/custom.scss";
 
 import { ApolloProvider } from "@apollo/client";
 import { ErrorDialogProvider, useErrorDialog } from "app/components/common/error/useErrorDialog";
-import ScrollOverlayWrapper from "app/components/common/ScrollOverlayWrapper";
 import { UserAccountModalProvider } from "app/components/user/UserAccountModalProvider";
 import { useApollo } from "app/lib/apollo-client";
-import clsx from "clsx";
 import Router from "next/router";
 import NProgress from "nprogress";
 import React from "react";
@@ -33,13 +31,7 @@ const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
       <ApolloProvider client={apolloClient}>
         <UserAccountModalProvider>
           <div className="w-full h-screen">
-            <ScrollOverlayWrapper
-              className={clsx("w-full h-full overflow-auto", "scroll-wrapper")}
-              enableTopOverlay={false}
-              classNameBottomOverlay={"w-full h-52"}
-            >
-              <Component {...pageProps} />
-            </ScrollOverlayWrapper>
+            <Component {...pageProps} />
           </div>
         </UserAccountModalProvider>
       </ApolloProvider>
