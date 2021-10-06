ee1729953de1cf2aa29de41ef90879467cd18c8a
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-10-06 19:15:26 +0200

    test(api): check if the graphql api gets written to the filesystem

--- services/api/tests/generate-schema.spec.ts
+++ services/api/tests/generate-schema.spec.ts
@@ -1,7 +1,17 @@
+import fs from "fs";
+
 import { generateSchema } from "@/generate-schema";
+
+jest.spyOn(fs, "writeFileSync");
 
 describe("generate schema", () => {
   it("can generate the schema", async () => {
     await expect(generateSchema()).resolves.toBeUndefined();
+  });
+
+  it("writes the schema to schema.gql", async () => {
+    await generateSchema();
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith("schema.gql", expect.any(String));
   });
 });
