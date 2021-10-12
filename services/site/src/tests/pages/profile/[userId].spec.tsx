c4700b4431af5a41765e852f822ab65acdb05d79
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-10-12 10:38:09 +0200

    test(site): add tests for profile page

--- services/site/src/tests/pages/profile/[userId].spec.tsx
+++ services/site/src/tests/pages/profile/[userId].spec.tsx
@@ -1,5 +1,7 @@
 import { cleanup, render } from "@testing-library/react";
-import UserProfile from "app/pages/profile/[userId]";
+import { initializeApollo } from "app/lib/apollo-client";
+import UserProfile, { getServerSideProps } from "app/pages/profile/[userId]";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
 
 jest.mock("next/router", () => ({
@@ -37,36 +39,94 @@ jest.mock("app/components/navigation", () => ({
   default: () => <></>,
 }));
 
+jest.mock("app/lib/apollo-client", () => ({
+  __esModule: true,
+  initializeApollo: jest.fn().mockReturnValue({
+    query: jest.fn(),
+  }),
+}));
+
 afterEach(cleanup);
 
 describe("user profile page", () => {
-  it("can render", () => {
-    const { container } = render(<UserProfile />);
+  describe("component", () => {
+    it("can render", () => {
+      const { container } = render(<UserProfile />);
 
-    expect(container.firstChild).toBeTruthy();
+      expect(container.firstChild).toBeTruthy();
+    });
+
+    it("shows the name of the user", () => {
+      const { findByText } = render(<UserProfile />);
+
+      expect(findByText("Hans Schröder")).toBeTruthy();
+    });
+
+    it("shows the name of the user", () => {
+      const { findByText } = render(<UserProfile />);
+
+      expect(findByText("Hans Schröder")).toBeTruthy();
+    });
+
+    it("shows the name of the challenge", () => {
+      const { findByText } = render(<UserProfile />);
+
+      expect(findByText("Dummy Challenge")).toBeTruthy();
+    });
+
+    it("shows the status for the challenge", () => {
+      const { findByText } = render(<UserProfile />);
+
+      expect(findByText("Done")).toBeTruthy();
+    });
   });
 
-  it("shows the name of the user", () => {
-    const { findByText } = render(<UserProfile />);
+  describe("getServerSideProps", () => {
+    it("returns an initial apollo state", async () => {
+      (initializeApollo as jest.Mock).mockReturnValue({
+        query: jest.fn().mockResolvedValue({
+          data: {
+            user: {
+              id: "0f00f41d-d782-4bba-9aac-fd5a929ab318",
+              displayName: "Hans Schröder",
+            },
+          },
+        }),
+        cache: {
+          extract: jest.fn().mockReturnValue({}),
+        },
+      });
+      const context: Partial<GetServerSidePropsContext> = {
+        params: {
+          userId: "92136047-3a39-4c22-bc09-ead41b8e2e08",
+        },
+      };
 
-    expect(findByText("Hans Schröder")).toBeTruthy();
-  });
+      // typescript assumes that the return type is { redirect: Redirect } and then complains about accessing props
+      const serverSideProps = (await getServerSideProps(context as GetServerSidePropsContext)) as any;
+      expect(serverSideProps).toBeTruthy();
+      expect(serverSideProps.props).toBeTruthy();
+      expect(serverSideProps.props.initialApolloState).toBeTruthy();
+    });
 
-  it("shows the name of the user", () => {
-    const { findByText } = render(<UserProfile />);
+    it("returns not found if the user was not found", async () => {
+      (initializeApollo as jest.Mock).mockReturnValue({
+        query: jest.fn().mockResolvedValue({
+          data: {
+            user: null,
+          },
+        }),
+      });
+      const context: Partial<GetServerSidePropsContext> = {
+        params: {
+          userId: "92136047-3a39-4c22-bc09-ead41b8e2e08",
+        },
+      };
 
-    expect(findByText("Hans Schröder")).toBeTruthy();
-  });
-
-  it("shows the name of the challenge", () => {
-    const { findByText } = render(<UserProfile />);
-
-    expect(findByText("Dummy Challenge")).toBeTruthy();
-  });
-
-  it("shows the status for the challenge", () => {
-    const { findByText } = render(<UserProfile />);
-
-    expect(findByText("Done")).toBeTruthy();
+      // typescript assumes that the return type is { redirect: Redirect } and then complains about accessing not found
+      const serverSideProps = (await getServerSideProps(context as GetServerSidePropsContext)) as any;
+      expect(serverSideProps).toBeTruthy();
+      expect(serverSideProps.notFound).toBeTruthy();
+    });
   });
 });
