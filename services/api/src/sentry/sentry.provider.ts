c848125a43baee9ffbdcb02cd5f268452ba0666a
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-10-12 23:26:39 +0200

    feat(api): add sentry transaction

--- services/api/src/sentry/sentry.provider.ts
+++ services/api/src/sentry/sentry.provider.ts
@@ -1,8 +1,9 @@
 import { Provider } from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
-import { GraphqlInterceptor } from "@ntegral/nestjs-sentry";
+
+import { SentryInterceptor } from "./sentry.interceptor";
 
 export const SentryProvider: Provider = {
   provide: APP_INTERCEPTOR,
-  useFactory: () => new GraphqlInterceptor(),
+  useFactory: () => new SentryInterceptor(),
 };
