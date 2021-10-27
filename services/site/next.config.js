9d8d273be86f11b384c5ac96f15315a6523afa25
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-10-27 17:09:05 +0200

    feat(site): use default api URLs for splitbee

--- services/site/next.config.js
+++ services/site/next.config.js
@@ -7,19 +7,8 @@ if (assetBaseUrl) {
 
 module.exports = {
   async rewrites() {
-    const rewrites = [
-      {
-        source: "/bee.js",
-        destination: "https://cdn.splitbee.io/sb.js",
-      },
-      {
-        source: "/_hive/:slug",
-        destination: "https://hive.splitbee.io/:slug",
-      },
-    ];
-
     if (!process.env.NEXT_PUBLIC_SITE_GRAPHQL_PROXY_TARGET) {
-      return rewrites;
+      return [];
     }
 
     return [
@@ -27,7 +16,6 @@ module.exports = {
         source: "/graphql",
         destination: process.env.NEXT_PUBLIC_SITE_GRAPHQL_PROXY_TARGET,
       },
-      ...rewrites,
     ];
   },
 
--- services/site/src/lib/tracking/index.tsx
+++ services/site/src/lib/tracking/index.tsx
@@ -5,8 +5,6 @@ export function initializeTracking(): void {
   if (!process.env.NEXT_PUBLIC_SITE_SPLITBEE_TOKEN) return;
 
   splitbee.init({
-    scriptUrl: "/bee.js",
-    apiUrl: "/_hive",
     disableCookie: true,
     token: process.env.NEXT_PUBLIC_SITE_SPLITBEE_TOKEN,
   });
