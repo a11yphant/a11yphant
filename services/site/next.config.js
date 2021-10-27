5d1dc4e25e22243c816e52766e87a96c034fdb45
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-10-27 15:26:03 +0200

    feat(site): rewrite to next config

--- services/site/next.config.js
+++ services/site/next.config.js
@@ -7,8 +7,19 @@ if (assetBaseUrl) {
 
 module.exports = {
   async rewrites() {
+    const rewrites = [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+
     if (!process.env.NEXT_PUBLIC_SITE_GRAPHQL_PROXY_TARGET) {
-      return [];
+      return rewrites;
     }
 
     return [
@@ -16,6 +27,7 @@ module.exports = {
         source: "/graphql",
         destination: process.env.NEXT_PUBLIC_SITE_GRAPHQL_PROXY_TARGET,
       },
+      ...rewrites,
     ];
   },
 
