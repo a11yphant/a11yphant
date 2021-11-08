fdd6ebb08d5793a07a0344237cd970f2ca8e9d8b
Michael Hinterhofer <mhinterhofer.mmt-m2020@fh-salzburg.ac.at>
2021-11-08 16:03:44 +0100

    feat(user): add interceptor to update lastSeen time

--- services/api/src/user/user.module.ts
+++ services/api/src/user/user.module.ts
@@ -3,12 +3,13 @@ import { forwardRef, Logger, Module } from "@nestjs/common";
 import { AuthenticationModule } from "@/authentication/authentication.module";
 import { PrismaModule } from "@/prisma/prisma.module";
 
+import { LastSeenInterceptor } from "./last-seen.interceptor";
 import { UserResolver } from "./user.resolver";
 import { UserService } from "./user.service";
 
 @Module({
   imports: [PrismaModule, forwardRef(() => AuthenticationModule)],
-  providers: [UserService, UserResolver, Logger],
-  exports: [UserService],
+  providers: [UserService, UserResolver, Logger, LastSeenInterceptor],
+  exports: [UserService, LastSeenInterceptor],
 })
 export class UserModule {}
