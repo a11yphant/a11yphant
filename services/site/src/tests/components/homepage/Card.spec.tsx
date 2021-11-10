6ba91a19d46c9bcde5c4f8e5af934d134e28bb60
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-11-10 15:55:00 +0100

    tests(site): await findbytext result

--- services/site/src/tests/components/homepage/Card.spec.tsx
+++ services/site/src/tests/components/homepage/Card.spec.tsx
@@ -27,8 +27,6 @@ describe("Card", () => {
 
     expect(screen.getByText(headingText, { selector: "a" })).toBeTruthy();
     expect(screen.getByText("12 Levels", { selector: "p" })).toBeTruthy();
-    // image is temporarily replaced with an background image
-    // expect(container.querySelectorAll("img").length).toBeGreaterThan(0);
   });
 
   it("renders the correct gradient for easy challenges", () => {
@@ -82,7 +80,7 @@ describe("Card", () => {
     expect(container.querySelector(".bg-gradient-hard")).toBeTruthy();
   });
 
-  it("renders the progress for started challenges", () => {
+  it("renders the progress for started challenges", async () => {
     const { findByText } = render(
       <Card
         key={1}
@@ -90,13 +88,13 @@ describe("Card", () => {
         challengeSlug={"semantic-html"}
         heading={headingText}
         levels={levelAmount}
-        finishedLevels={2}
+        finishedLevels={7}
         difficulty={ChallengeDifficulty.Hard}
         challengeNumber={1}
       />,
     );
 
-    expect(findByText(`${finishedLevels}`)).toBeTruthy();
+    expect(await findByText(/7/i)).toBeTruthy();
   });
   it("renders a checkmark for finished challenges", () => {
     const { container } = render(
