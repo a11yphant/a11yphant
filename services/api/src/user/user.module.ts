0c8924d7e5ab8ab87d2aa1731b112b8794c6ec5e
Michael Hinterhofer <mhinterhofer.mmt-m2020@fh-salzburg.ac.at>
2021-11-10 16:03:17 +0100

    feat(user): add console command for stale user deletion

--- services/api/src/user/user.module.ts
+++ services/api/src/user/user.module.ts
@@ -3,13 +3,14 @@ import { forwardRef, Logger, Module } from "@nestjs/common";
 import { AuthenticationModule } from "@/authentication/authentication.module";
 import { PrismaModule } from "@/prisma/prisma.module";
 
+import { DeleteStaleUsers } from "./delete-stale-users.console";
 import { LastSeenInterceptor } from "./last-seen.interceptor";
 import { UserResolver } from "./user.resolver";
 import { UserService } from "./user.service";
 
 @Module({
   imports: [PrismaModule, forwardRef(() => AuthenticationModule)],
-  providers: [UserService, UserResolver, Logger, LastSeenInterceptor],
+  providers: [UserService, UserResolver, Logger, LastSeenInterceptor, DeleteStaleUsers],
   exports: [UserService, LastSeenInterceptor],
 })
 export class UserModule {}
