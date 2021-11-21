d866ba41afdf5eb0b0e1284fabb7ec3e987c17da
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-11-21 17:26:06 +0100

    tests(api): refacotr quiz level resolver test

--- services/api/tests/unit/challenge/quiz-level.resovler.spec.ts
+++ services/api/tests/unit/challenge/quiz-level.resovler.spec.ts
@@ -1,33 +1,42 @@
 import { createMock } from "@golevelup/ts-jest";
-import { AnswerOptionFactory } from "@tests/support/factories/models/answer-option.factory";
 import { QuizLevelFactory } from "@tests/support/factories/models/quiz-level.factory";
 
+import { SessionToken } from "@/authentication/interfaces/session-token.interface";
 import { AnswerOptionService } from "@/challenge/answer-option.service";
 import { LevelStatus } from "@/challenge/enums/level-status.enum";
 import { LevelService } from "@/challenge/level.service";
 import { QuizLevelResolver } from "@/challenge/quiz-level.resolver";
 
-describe("quiz level resolver", () => {
-  it("returns answer options for the level", async () => {
-    const resolver = new QuizLevelResolver(
-      createMock<AnswerOptionService>({
-        findForQuizLevel: jest.fn().mockResolvedValue(AnswerOptionFactory.buildList(2)),
-      }),
-      createMock<LevelService>(),
-    );
-    const level = QuizLevelFactory.build();
+function createQuizLevelResolver(
+  partials: {
+    levelService?: Partial<LevelService>;
+    answerOptionService?: Partial<AnswerOptionService>;
+  } = {},
+): QuizLevelResolver {
+  const levelService = createMock<LevelService>(partials.levelService);
+  const answerOptionService = createMock<AnswerOptionService>(partials.answerOptionService);
 
-    expect(await resolver.answerOptions(level)).toHaveLength(2);
+  return new QuizLevelResolver(answerOptionService, levelService);
+}
+
+describe("quiz level resolver", () => {
+  const level = QuizLevelFactory.build();
+  const sessionToken: SessionToken = { userId: "userid" };
+  it("returns answer options for the level", async () => {
+    const findForQuizLevel = jest.fn().mockResolvedValue([]);
+
+    const resolver = createQuizLevelResolver({ answerOptionService: { findForQuizLevel } });
+    const resolvedAnswerOptions = await resolver.answerOptions(level);
+
+    expect(resolvedAnswerOptions).toBeDefined();
+    expect(findForQuizLevel).toHaveBeenCalledWith(level.id);
   });
 
   it("resolves out the status of a level", async () => {
-    const resolver = new QuizLevelResolver(
-      createMock<AnswerOptionService>(),
-      createMock<LevelService>({
-        findStatusForQuizLevel: jest.fn().mockResolvedValue(LevelStatus.OPEN),
-      }),
-    );
-    const status = await resolver.status(QuizLevelFactory.build(), { userId: "userid" });
+    const findStatusForQuizLevel = jest.fn().mockResolvedValue(LevelStatus.OPEN);
+
+    const resolver = createQuizLevelResolver({ levelService: { findStatusForQuizLevel } });
+    const status = await resolver.status(level, sessionToken);
 
     expect(status).toBe(LevelStatus.OPEN);
   });
