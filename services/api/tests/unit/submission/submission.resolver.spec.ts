2cca0b3522df9c6c181a3473f15afd0020e2c1a8
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-11-21 18:08:31 +0100

    tests(api): refactor submission resolvers

--- services/api/tests/unit/submission/submission.resolver.spec.ts
+++ services/api/tests/unit/submission/submission.resolver.spec.ts
@@ -7,47 +7,49 @@ import { CodeLevelSubmission } from "@/submission/graphql/models/code-level-subm
 import { SubmissionResolver } from "@/submission/graphql/resolvers/submission.resolver";
 import { CodeLevelResultService } from "@/submission/services/code-level-result.service";
 
+function createSubmissionResolver(
+  partials: {
+    codeLevelResultService?: Partial<CodeLevelResultService>;
+    levelService?: Partial<LevelService>;
+  } = {},
+): SubmissionResolver {
+  const codeLevelResultService = createMock<CodeLevelResultService>(partials.codeLevelResultService);
+  const levelService = createMock<LevelService>(partials.levelService);
+
+  return new SubmissionResolver(levelService, codeLevelResultService);
+}
+
 describe("submission resolver", () => {
   it("can resolve a level", async () => {
-    const mockLevel = CodeLevelFactory.build();
+    const level = CodeLevelFactory.build();
+    const submission = new CodeLevelSubmission({ id: "bla", levelId: "blu", createdAt: new Date(), updatedAt: new Date() });
+    const findOne = jest.fn().mockResolvedValue(level);
 
-    const resolver = new SubmissionResolver(
-      createMock<LevelService>({
-        findOne: jest.fn().mockResolvedValue(mockLevel),
-      }),
-      createMock<CodeLevelResultService>(),
-    );
+    const resolver = createSubmissionResolver({ levelService: { findOne } });
+    const resolvedLevel = await resolver.level(submission);
 
-    const level = await resolver.level(new CodeLevelSubmission({ id: "bla", levelId: "blu", createdAt: new Date(), updatedAt: new Date() }));
-
-    expect(level).toBeTruthy();
-    expect(level.id).toBe(mockLevel.id);
+    expect(resolvedLevel).toBeTruthy();
+    expect(findOne).toHaveBeenCalledWith(submission.levelId);
   });
 
   it("can find the result for a submission", async () => {
-    const resolver = new SubmissionResolver(
-      createMock<LevelService>(),
-      createMock<CodeLevelResultService>({
-        findOneForSubmission: jest.fn().mockResolvedValue({ id: "uuid" }),
-      }),
-    );
+    const submission = CodeLevelSubmissionFactory.build({ id: "uuid" });
+    const findOneForSubmission = jest.fn().mockResolvedValue({ id: "uuid" });
 
-    const submission = await resolver.result(CodeLevelSubmissionFactory.build({ id: "uuid" }));
+    const resolver = createSubmissionResolver({ codeLevelResultService: { findOneForSubmission } });
+    const resolvedResult = await resolver.result(submission);
 
-    expect(submission).toBeTruthy();
-    expect(submission).toHaveProperty("id", "uuid");
+    expect(resolvedResult).toBeTruthy();
+    expect(findOneForSubmission).toHaveBeenCalledWith(submission.id);
   });
 
   it("returns null if the submission does not have a result", async () => {
-    const resolver = new SubmissionResolver(
-      createMock<LevelService>(),
-      createMock<CodeLevelResultService>({
-        findOneForSubmission: jest.fn().mockResolvedValue(null),
-      }),
-    );
+    const findOneForSubmission = jest.fn().mockResolvedValue(null);
+    const submission = CodeLevelSubmissionFactory.build();
 
-    const submission = await resolver.result(CodeLevelSubmissionFactory.build({ id: "uuid" }));
+    const resolver = createSubmissionResolver({ codeLevelResultService: { findOneForSubmission } });
+    const resolvedSubmission = await resolver.result(submission);
 
-    expect(submission).toBeNull();
+    expect(resolvedSubmission).toBeNull();
   });
 });
