44da32c514ed66c8192b716899aac74389607faa
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-11-21 17:20:45 +0100

    tests(api): refactor code level resolver test

--- services/api/tests/unit/challenge/code-level.resolver.spec.ts
+++ services/api/tests/unit/challenge/code-level.resolver.spec.ts
@@ -1,111 +1,94 @@
 import { createMock } from "@golevelup/ts-jest";
 import { CodeLevelFactory } from "@tests/support/factories/models/code-level.factory";
 import { CodeLevelSubmissionFactory } from "@tests/support/factories/models/code-level-submission.factory";
-import { RequirementFactory } from "@tests/support/factories/models/requirement.factory";
-import { TaskFactory } from "@tests/support/factories/models/task.factory";
 
+import { SessionToken } from "@/authentication/interfaces/session-token.interface";
 import { CodeLevelResolver } from "@/challenge/code-level.resolver";
 import { LevelStatus } from "@/challenge/enums/level-status.enum";
 import { LevelService } from "@/challenge/level.service";
-import { Requirement } from "@/challenge/models/requirement.model";
 import { RequirementService } from "@/challenge/requirement.service";
 import { TaskService } from "@/challenge/task.service";
 import { CodeLevelSubmissionService } from "@/submission/services/code-level-submission.service";
 
-describe("code level resolver", () => {
-  it("resolves the requirements for a level", async () => {
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>({
-        findForLevel: jest
-          .fn()
-          .mockResolvedValue([
-            new Requirement({ id: "uuid-1", title: "Requirement 1", description: "A helpful message" }),
-            new Requirement({ id: "uuid-2", title: "Requirement 2", description: "A helpful message" }),
-          ]),
-      }),
-      createMock<TaskService>(),
-      createMock<CodeLevelSubmissionService>(),
-      createMock<LevelService>(),
-    );
+function createCodeLevelResolver(
+  partials: {
+    levelService?: Partial<LevelService>;
+    requirementService?: Partial<RequirementService>;
+    taskService?: Partial<TaskService>;
+    codeLevelSubmissionService?: Partial<CodeLevelSubmissionService>;
+  } = {},
+): CodeLevelResolver {
+  const levelService = createMock<LevelService>(partials.levelService);
+  const requirementService = createMock<RequirementService>(partials.requirementService);
+  const taskService = createMock<TaskService>(partials.taskService);
+  const codeLevelSubmissionService = createMock<CodeLevelSubmissionService>(partials.codeLevelSubmissionService);
 
-    expect((await resolver.requirements(CodeLevelFactory.build())).length).toEqual(2);
+  return new CodeLevelResolver(requirementService, taskService, codeLevelSubmissionService, levelService);
+}
+
+describe("code level resolver", () => {
+  const level = CodeLevelFactory.build();
+  const sessionToken: SessionToken = { userId: "userid" };
+
+  it("resolves the requirements for a level", async () => {
+    const findForLevel = jest.fn().mockResolvedValue([]);
+
+    const resolver = createCodeLevelResolver({ requirementService: { findForLevel } });
+    const resolvedRequirements = await resolver.requirements(level);
+
+    expect(resolvedRequirements).toBeTruthy();
+    expect(findForLevel).toHaveBeenCalledWith(level.id);
   });
 
   it("resolves the tasks for a level", async () => {
-    const tasks = [TaskFactory.build(), TaskFactory.build()];
+    const findForLevel = jest.fn().mockResolvedValue([]);
 
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>(),
-      createMock<TaskService>({
-        findForLevel: jest.fn().mockResolvedValue(tasks),
-      }),
-      createMock<CodeLevelSubmissionService>(),
-      createMock<LevelService>(),
-    );
+    const resolver = createCodeLevelResolver({ taskService: { findForLevel } });
+    const resolvedTasks = await resolver.tasks(level);
 
-    expect((await resolver.tasks(CodeLevelFactory.build())).length).toEqual(tasks.length);
+    expect(resolvedTasks).toBeTruthy();
+    expect(findForLevel).toHaveBeenCalledWith(level.id);
   });
 
   it("resolves the requirements for a level", async () => {
-    const requirements = [RequirementFactory.build(), RequirementFactory.build()];
+    const findForLevel = jest.fn().mockResolvedValue([]);
 
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>({
-        findForLevel: jest.fn().mockResolvedValue(requirements),
-      }),
-      createMock<TaskService>(),
-      createMock<CodeLevelSubmissionService>(),
-      createMock<LevelService>(),
-    );
+    const resolver = createCodeLevelResolver({ requirementService: { findForLevel } });
+    const resolvedLevels = await resolver.requirements(level);
 
-    expect((await resolver.requirements(CodeLevelFactory.build())).length).toEqual(requirements.length);
+    expect(resolvedLevels).toBeTruthy();
+    expect(findForLevel).toHaveBeenCalledWith(level.id);
   });
 
   it("resolves the last submission for a level", async () => {
     const submission = CodeLevelSubmissionFactory.build();
+    const findLastForUserAndLevel = jest.fn().mockResolvedValue(submission);
 
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>(),
-      createMock<TaskService>(),
-      createMock<CodeLevelSubmissionService>({
-        findLastForUserAndLevel: jest.fn().mockResolvedValue(submission),
-      }),
-      createMock<LevelService>(),
-    );
+    const resolver = createCodeLevelResolver({ codeLevelSubmissionService: { findLastForUserAndLevel } });
+    const resolvedSubmission = await resolver.lastSubmission(level, sessionToken);
 
-    const fetchedSubmission = await resolver.lastSubmission(CodeLevelFactory.build(), { userId: "userid" });
-
-    expect(fetchedSubmission.id).toBe(submission.id);
+    expect(resolvedSubmission).toBeTruthy();
+    expect(findLastForUserAndLevel).toHaveBeenCalledWith(sessionToken.userId, level.id);
   });
 
   it("resolves the last submission for a level", async () => {
     const submission = CodeLevelSubmissionFactory.build();
+    const findLastForUserAndLevel = jest.fn().mockResolvedValue(submission);
 
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>(),
-      createMock<TaskService>(),
-      createMock<CodeLevelSubmissionService>({
-        findLastForUserAndLevel: jest.fn().mockResolvedValue(submission),
-      }),
-      createMock<LevelService>(),
-    );
+    const resolver = createCodeLevelResolver({ codeLevelSubmissionService: { findLastForUserAndLevel } });
+    const resolvedSubmission = await resolver.lastSubmission(level, sessionToken);
 
-    const fetchedSubmission = await resolver.lastSubmission(CodeLevelFactory.build(), { userId: "userid" });
-
-    expect(fetchedSubmission.id).toBe(submission.id);
+    expect(resolvedSubmission).toBeTruthy();
+    expect(findLastForUserAndLevel).toHaveBeenCalledWith(sessionToken.userId, level.id);
   });
 
   it("resolves out the status of a level", async () => {
-    const resolver = new CodeLevelResolver(
-      createMock<RequirementService>(),
-      createMock<TaskService>(),
-      createMock<CodeLevelSubmissionService>(),
-      createMock<LevelService>({
-        findStatusForCodeLevel: jest.fn().mockResolvedValue(LevelStatus.OPEN),
-      }),
-    );
-    const status = await resolver.status(CodeLevelFactory.build(), { userId: "userid" });
+    const findStatusForCodeLevel = jest.fn().mockResolvedValue(LevelStatus.OPEN);
 
-    expect(status).toBe(LevelStatus.OPEN);
+    const resolver = createCodeLevelResolver({ levelService: { findStatusForCodeLevel } });
+    const status = await resolver.status(level, sessionToken);
+
+    expect(status).toBeDefined();
+    expect(findStatusForCodeLevel).toHaveBeenCalledWith(level.id, sessionToken.userId);
   });
 });
