ff79605a377bec758257c20ec425023e826baae5
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-11-26 10:13:08 +0100

    test(e2e): allow configuration of base url

--- tests/cypress/integration/code-level.spec.js
+++ tests/cypress/integration/code-level.spec.js
@@ -1,6 +1,6 @@
 describe("code level", () => {
     it("can submit a level", () => {
-        cy.visit("http://localhost:3001/challenge/a-valid-html-document/level/01");
+        cy.visit("/challenge/a-valid-html-document/level/01");
 
         cy.get("button").contains("Submit").parent("button").should("be.enabled").click();
         cy.contains("loading");
--- tests/cypress/integration/landing-page.spec.js
+++ tests/cypress/integration/landing-page.spec.js
@@ -1,6 +1,6 @@
 describe("landing page", () => {
     it("contains a list of challenges", () => {
-        cy.visit("http://localhost:3001/");
+        cy.visit("/");
         cy.contains("A valid HTML document");
         cy.contains("Section Heading Elements");
         cy.contains("Content Elements");
