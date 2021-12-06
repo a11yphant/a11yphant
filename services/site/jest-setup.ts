abfd51d349a2270fa3ad3aa2c4db08ab4c1c7171
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-12-06 19:02:33 +0100

    refactor: change path of mock file for testing

--- services/site/jest-setup.ts
+++ services/site/jest-setup.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { mockUsePrefersReducedMotion } from "app/tests/components/helper/mockUsePrefersReducedMotion";
+import { mockUsePrefersReducedMotion } from "app/tests/helper/mockUsePrefersReducedMotion";
 import Enzyme from "enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
