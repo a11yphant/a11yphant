1c87dd54bbb08b579f2cba371294cd15d9e3749c
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-12-06 18:54:07 +0100

    add: mock for prefers reducd motion hook for all tests

--- services/site/jest-setup.ts
+++ services/site/jest-setup.ts
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { mockUsePrefersReducedMotion } from "app/tests/components/helper/mockUsePrefersReducedMotion";
 import Enzyme from "enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+mockUsePrefersReducedMotion();
