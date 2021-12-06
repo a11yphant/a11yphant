158048be7375e304d37b8db90184ccf617bff684
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-12-06 12:35:25 +0100

    refactor: remove difficulty sections

--- services/site/src/pages/index.tsx
+++ services/site/src/pages/index.tsx
@@ -2,7 +2,6 @@ import Footer from "app/components/Footer";
 import ChallengeHeader from "app/components/homepage/ChallengeHeader";
 import ChallengeList from "app/components/homepage/ChallengeList";
 import { ChallengeModal } from "app/components/homepage/challengeModal/ChallengeModal";
-import { DifficultyEasy, DifficultyHard, DifficultyMedium } from "app/components/homepage/difficulties/Difficulties";
 import Hero from "app/components/homepage/Hero";
 import Legend from "app/components/homepage/Legend";
 import Navigation from "app/components/Navigation";
@@ -21,7 +20,7 @@ const Home: React.FunctionComponent = () => {
   const { currentUser } = useCurrentUser();
 
   const {
-    data: { easyChallenges, mediumChallenges, hardChallenges },
+    data: { easyChallenges },
   } = useChallengesQuery();
 
   const onCloseModal = (): void => {
@@ -45,7 +44,11 @@ const Home: React.FunctionComponent = () => {
             className={clsx("max-w-screen-3xl mx-8 mt-32 mb-4", "sm:mx-12 sm:mt-28 sm:mb-12", "md:mx-24 md:mb-24", "2xl:mx-auto")}
           >
             <ChallengeHeader className={clsx("2xl:mx-24")} userLoggedIn={currentUser?.isRegistered} />
+
             <Legend className={clsx("2xl:mx-24")} />
+            {easyChallenges.length !== 0 && <ChallengeList className={clsx("2xl:mx-24")} heading={<>All challenges</>} challenges={easyChallenges} />}
+
+            {/* TODO: add when more difficult challenge content exists
             {easyChallenges.length !== 0 && (
               <ChallengeList
                 className={clsx("2xl:mx-24")}
@@ -83,7 +86,7 @@ const Home: React.FunctionComponent = () => {
                 }
                 challenges={hardChallenges}
               />
-            )}
+            )} */}
           </section>
         </div>
         <ChallengeModal open={!!router.query.challenge} onClose={onCloseModal} challengeSlug={router.query.challenge as string} />
