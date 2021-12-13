b382e720e0b9644ddb07771bbed4060cc3d371f2
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-13 21:53:34 +0100

    tests(site): add login form tests

--- services/site/src/tests/components/user/LoginBox.spec.tsx
+++ services/site/src/tests/components/user/LoginBox.spec.tsx
@@ -1,13 +1,21 @@
 import { MockedProvider } from "@apollo/client/testing";
 import { render, screen } from "@testing-library/react";
 import LoginBox from "app/components/user/LoginBox";
+import LoginForm from "app/components/user/LoginForm";
 import React from "react";
 
 const mockShow = jest.fn();
+const mockHide = jest.fn();
+
+jest.mock("app/components/user/LoginForm", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 jest.mock("app/components/user/useUserAccountModalApi", () => ({
   useUserAccountModalApi: () => ({
     show: mockShow,
+    hide: mockHide,
   }),
 }));
 
@@ -22,12 +30,13 @@ function renderLoginBox(): ReturnType<typeof render> {
 describe("login box", () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    (LoginForm as jest.Mock).mockImplementation(() => <div>LoginForm</div>);
   });
 
   it("renders the login form", () => {
     renderLoginBox();
 
-    expect(screen.getByRole("form", { name: /Login/ })).toBeInTheDocument();
+    expect(screen.getByText("LoginForm")).toBeInTheDocument();
   });
 
   it("renders the login via github link", () => {
@@ -54,5 +63,15 @@ describe("login box", () => {
     screen.getByRole("button", { name: /Create a free account/ }).click();
 
     expect(mockShow).toHaveBeenCalledWith("signup");
+  });
+
+  it("closes the modal after a successful login", () => {
+    (LoginForm as jest.Mock).mockImplementation((props: Parameters<typeof LoginForm>[0]) => {
+      props.onSuccessfulLogin();
+      return <div>LoginForm</div>;
+    });
+    render(<LoginBox />);
+
+    expect(mockHide).toHaveBeenCalled();
   });
 });
--- services/site/src/tests/components/user/LoginForm.spec.tsx
+++ services/site/src/tests/components/user/LoginForm.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import LoginForm from "app/components/user/LoginForm";
 import { useLoginMutation } from "app/generated/graphql";
 import React from "react";
@@ -26,5 +26,65 @@ describe("login form", () => {
   it("renders a submit button", () => {
     render(<LoginForm />);
     expect(screen.getByRole("button", { name: /Log in/ })).toBeInTheDocument();
+  });
+
+  it("calls the onLogin callback if the form is sent successfully", async () => {
+    const email = "test@a11yphant.com";
+    const password = "verysecret";
+
+    const onLogin = jest.fn();
+
+    render(<LoginForm onSuccessfulLogin={onLogin} />);
+
+    const emailInput = screen.getByRole("textbox", { name: /Email/ });
+    fireEvent.change(emailInput, { target: { value: email } });
+
+    const passwordInput = screen.getByLabelText(/Password/);
+    fireEvent.change(passwordInput, { target: { value: password } });
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    await waitFor(() => expect(emailInput).toHaveValue(email));
+    await waitFor(() => expect(passwordInput).toHaveValue(password));
+
+    expect(onLogin).toHaveBeenCalled();
+  });
+
+  it("does not call onLogin if the form does not filled out", async () => {
+    const onLogin = jest.fn();
+
+    render(<LoginForm onSuccessfulLogin={onLogin} />);
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    await screen.findByText("The password is required");
+
+    expect(onLogin).not.toHaveBeenCalled();
+  });
+
+  it("calls the login mutation on submit", async () => {
+    const email = "test@a11yphant.com";
+    const password = "verysecret";
+
+    const login = jest.fn();
+    (useLoginMutation as jest.Mock).mockReturnValue([login, { loading: false }]);
+
+    render(<LoginForm />);
+
+    const emailInput = screen.getByRole("textbox", { name: /Email/ });
+    fireEvent.change(emailInput, { target: { value: email } });
+
+    const passwordInput = screen.getByLabelText(/Password/);
+    fireEvent.change(passwordInput, { target: { value: password } });
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    await waitFor(() => expect(emailInput).toHaveValue(email));
+    await waitFor(() => expect(passwordInput).toHaveValue(password));
+
+    expect(login).toHaveBeenCalledWith({ variables: { email, password } });
   });
 });
