a376336dbca5f39f1477b37d5a63ab53f6096150
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-14 17:07:44 +0100

    tests(site): add tests for text input

---
+++ services/site/src/tests/components/common/inputs/TextInput.spec.tsx
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import TextInput from "app/components/common/inputs/TextInput";
+import React from "react";
+
+describe("text input", () => {
+  it("renders the label", () => {
+    render(<TextInput label="label" />);
+
+    expect(screen.getByLabelText("label")).toBeInTheDocument();
+  });
+
+  it("renders the value", () => {
+    render(<TextInput label="label" value="value" />);
+
+    expect(screen.getByLabelText("label")).toHaveValue("value");
+  });
+
+  it("renders a input of the passed type", () => {
+    render(<TextInput label="label" type="text" />);
+
+    expect(screen.getByLabelText("label")).toHaveAttribute("type", "text");
+  });
+
+  it("renders the placeholder", () => {
+    render(<TextInput label="label" placeholder="placeholder" />);
+
+    expect(screen.getByLabelText("label")).toHaveAttribute("placeholder", "placeholder");
+  });
+
+  it("calls the onChange handler when the input changes", async () => {
+    const onChange = jest.fn();
+    render(<TextInput label="label" onChange={onChange} />);
+
+    const input = screen.getByLabelText("label");
+    fireEvent.change(input, { target: { value: "value" } });
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it("renders the helper text", () => {
+    render(<TextInput label="label" helperText="helper text" />);
+
+    expect(screen.getByText("helper text")).toBeInTheDocument();
+  });
+
+  it("renders the input in the error state", () => {
+    const { container } = render(<TextInput label="label" helperText="helper text" error />);
+
+    expect(container.querySelector(".Mui-error")).toBeTruthy();
+  });
+
+  it("passes the input ref to the input", () => {
+    const ref = React.createRef<HTMLInputElement>();
+    render(<TextInput label="label" inputRef={ref} />);
+
+    expect(ref.current).toBeInTheDocument();
+  });
+});
