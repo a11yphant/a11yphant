04fe4cbac804333fcde804c083de03f250e16757
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-14 18:12:33 +0100

    tests(site): remove unnecessary mocks

--- services/site/src/tests/components/user/UserAccountBox.spec.tsx
+++ services/site/src/tests/components/user/UserAccountBox.spec.tsx
@@ -1,26 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
 
-import { cleanup } from "@testing-library/react";
 import LoginBox from "app/components/user/LoginBox";
 import SignUpBox from "app/components/user/SignUpBox";
 import { UserAccountBox } from "app/components/user/UserAccountBox";
 import { shallow } from "enzyme";
 import React from "react";
-
-const mockShow = jest.fn();
-const mockHide = jest.fn();
-
-jest.mock("app/components/user/useUserAccountModalApi", () => ({
-  useUserAccountModalApi: () => ({
-    show: mockShow,
-    hide: mockHide,
-  }),
-}));
-
-afterEach(() => {
-  cleanup();
-  jest.clearAllMocks();
-});
 
 describe("UserAccountBox", () => {
   it("renders the sign up options in sign up mode", () => {
