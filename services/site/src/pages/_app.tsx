5c4844e0d2c4537cb84708b4cfe8ac3c426c4ba4
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-16 13:32:23 +0100

    feat(site): add emotion cache

--- services/site/src/pages/_app.tsx
+++ services/site/src/pages/_app.tsx
@@ -4,18 +4,24 @@ import "app/styles/fonts.scss";
 import "app/styles/custom.scss";
 
 import { ApolloProvider } from "@apollo/client";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 import { ErrorDialogProvider, useErrorDialog } from "app/components/common/error/useErrorDialog";
 import ScrollOverlayWrapper, { ScrollOverlayWrapperProps } from "app/components/common/ScrollOverlayWrapper";
 import { UserAccountModalProvider } from "app/components/user/UserAccountModalProvider";
 import { useApollo } from "app/lib/apollo-client";
+import createEmotionCache from "app/lib/emotion/createEmotionCache";
 import { initializeTracking } from "app/lib/tracking";
 import Router from "next/router";
 import NProgress from "nprogress";
 import React from "react";
+
 interface AppProps {
   Component: React.JSXElementConstructor<any>;
+  emotionCache: EmotionCache;
   pageProps: any;
 }
+
+const clientSideEmotionCache = createEmotionCache();
 
 NProgress.configure({
   showSpinner: false,
@@ -37,27 +43,29 @@ const ConditionalWrapper = <T,>({ condition, Wrapper, children, ...otherProps }:
 
 initializeTracking();
 
-const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+const App: React.FunctionComponent<AppProps> = ({ Component, pageProps, emotionCache = clientSideEmotionCache }) => {
   const { errorDialog, errorDialogApi } = useErrorDialog();
   const apolloClient = useApollo(pageProps.initialApolloState, errorDialogApi);
 
   return (
-    <ErrorDialogProvider errorDialog={errorDialog} errorDialogApi={errorDialogApi}>
-      <ApolloProvider client={apolloClient}>
-        <UserAccountModalProvider>
-          <ConditionalWrapper<ScrollOverlayWrapperProps>
-            condition={pageProps.showScrollOverlay ?? true}
-            Wrapper={ScrollOverlayWrapper}
-            enableTopOverlay={false}
-            enableBottomOverlay={true}
-            classNameBottomOverlay={"h-52 -mt-52"}
-            attachScrollListenerToDocument
-          >
-            <Component {...pageProps} />
-          </ConditionalWrapper>
-        </UserAccountModalProvider>
-      </ApolloProvider>
-    </ErrorDialogProvider>
+    <CacheProvider value={emotionCache}>
+      <ErrorDialogProvider errorDialog={errorDialog} errorDialogApi={errorDialogApi}>
+        <ApolloProvider client={apolloClient}>
+          <UserAccountModalProvider>
+            <ConditionalWrapper<ScrollOverlayWrapperProps>
+              condition={pageProps.showScrollOverlay ?? true}
+              Wrapper={ScrollOverlayWrapper}
+              enableTopOverlay={false}
+              enableBottomOverlay={true}
+              classNameBottomOverlay={"h-52 -mt-52"}
+              attachScrollListenerToDocument
+            >
+              <Component {...pageProps} />
+            </ConditionalWrapper>
+          </UserAccountModalProvider>
+        </ApolloProvider>
+      </ErrorDialogProvider>
+    </CacheProvider>
   );
 };
 
--- services/site/src/pages/_document.tsx
+++ services/site/src/pages/_document.tsx
@@ -1,9 +1,34 @@
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "app/lib/emotion/createEmotionCache";
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) =>
+          function EnhanceApp(props) {
+            return <App emotionCache={cache} {...props} />;
+          },
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style data-emotion={`${style.key} ${style.ids.join(" ")}`} key={style.key} dangerouslySetInnerHTML={{ __html: style.css }} />
+    ));
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
   }
 
   render(): JSX.Element {
