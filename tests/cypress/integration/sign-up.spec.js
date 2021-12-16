a96060416befdc025fbdb2d9e217453e2d9c9f15
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-16 23:04:20 +0100

    chore(ci): debug docker networks

---
+++ tests/cypress/integration/sign-up.spec.js
@@ -0,0 +1,16 @@
+function getInputByLabel(label) {
+    return cy.contains('label', label).parent().find('input')
+}
+
+describe("sign up", () => {
+    it("can sign up", () => {
+        Cypress.Cookies.debug(true);
+        cy.visit("/");
+        getInputByLabel('Name').type("Sign up user");
+        getInputByLabel('Email').type("sign-up@a11yphant.com");
+        getInputByLabel('Password').type("very-secret");
+        cy.contains('button', 'Sign up').click();
+
+        cy.contains("button", "Sign up").should("not.exist");
+    });
+});
