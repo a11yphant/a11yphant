338feec194d6dd30ab5a5fabd19d7f1e6b454775
Thomas Dax <thomas.dax@vivid-planet.com>
2021-12-16 16:23:07 +0100

    test(prefersReducedMotion): add comment to explain default value of usePrefersReducedMotion

--- services/site/src/tests/hooks/prefersReducedMotion.spec.tsx
+++ services/site/src/tests/hooks/prefersReducedMotion.spec.tsx
@@ -28,6 +28,8 @@ describe("prefersReducedMotion", () => {
     jest.resetAllMocks();
   });
 
+  // Todo: If the site is rendered on the server, the default value "true" should be returned
+  // That's because before we know if the user prefers reduced motion, we don't want to render any animations per default
   it("returns true during SSR", () => {
     mockMatchMedia(true);
     const { result } = renderHookServer(() => usePrefersReducedMotion());
