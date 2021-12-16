0cfbded1babd8c245888700b0ee1fcd26a663491
Thomas Dax <thomas.dax@vivid-planet.com>
2021-12-16 17:35:32 +0100

    test(UserAccountBox): rewrite tests with react testing library

--- services/site/src/tests/components/user/UserAccountBox.spec.tsx
+++ services/site/src/tests/components/user/UserAccountBox.spec.tsx
@@ -1,8 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 
-import Button from "app/components/buttons/Button";
-import { UserAccountBox } from "app/components/user/UserAccountBox";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import { UserAccountBox, UserAccountBoxProps } from "app/components/user/UserAccountBox";
 import React from "react";
 
 const mockShow = jest.fn();
@@ -15,52 +14,42 @@ jest.mock("app/components/user/useUserAccountModalApi", () => ({
   }),
 }));
 
+const renderUserAccountBox = ({ mode, ...props }: Partial<UserAccountBoxProps>): void => {
+  render(<UserAccountBox mode={mode} {...props} />);
+};
+
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe("UserAccountBox", () => {
   it("renders correctly in signup mode", () => {
-    const wrapper = shallow(<UserAccountBox mode="signup" />);
+    renderUserAccountBox({ mode: "signup" });
 
-    // Github button has sign up text
-    expect(wrapper.findWhere((n) => n.text() === "Sign up via Github").length).toBeGreaterThanOrEqual(1);
-
-    // Twitter button has sign up text
-    expect(wrapper.findWhere((n) => n.text() === "Sign up via Twitter").length).toBeGreaterThanOrEqual(1);
-
-    // "link" to login modal exists
-    expect(wrapper.find(Button).findWhere((n) => n.children().length === 1 && n.children().text() === "Already have an account? Log in."));
+    expect(screen.queryAllByRole("link")).toHaveLength(2);
+    expect(screen.getByRole("button", { name: /Log in/ })).toBeInTheDocument();
   });
 
   it("show login modal on 'link' to login click", () => {
-    const wrapper = shallow(<UserAccountBox mode="signup" />);
+    renderUserAccountBox({ mode: "signup" });
 
-    wrapper.find(Button).simulate("click");
-    wrapper.update();
+    screen.getByRole("button", { name: /Log in/ }).click();
 
     expect(mockShow).toHaveBeenCalledTimes(1);
     expect(mockShow).toHaveBeenCalledWith("login");
   });
 
   it("renders correctly in login mode", () => {
-    const wrapper = shallow(<UserAccountBox mode="login" />);
+    renderUserAccountBox({ mode: "login" });
 
-    // Github button has login text
-    expect(wrapper.findWhere((n) => n.text() === "Login with Github").length).toBeGreaterThanOrEqual(1);
-
-    // Twitter button has login text
-    expect(wrapper.findWhere((n) => n.text() === "Login with Twitter").length).toBeGreaterThanOrEqual(1);
-
-    // "link" to signup modal exists
-    expect(wrapper.find(Button).findWhere((n) => n.children().length === 1 && n.children().text() === "New? Create a free account."));
+    expect(screen.queryAllByRole("link")).toHaveLength(2);
+    expect(screen.getByRole("button", { name: /Create a free account/ })).toBeInTheDocument();
   });
 
   it("show signup modal on 'link' to signup click", () => {
-    const wrapper = shallow(<UserAccountBox mode="login" />);
+    renderUserAccountBox({ mode: "login" });
 
-    wrapper.find(Button).simulate("click");
-    wrapper.update();
+    screen.getByRole("button", { name: /Create a free account/ }).click();
 
     expect(mockShow).toHaveBeenCalledTimes(1);
     expect(mockShow).toHaveBeenCalledWith("signup");
