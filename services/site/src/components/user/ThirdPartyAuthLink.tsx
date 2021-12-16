a674e1308580dfb3055190ecdb677cf8be14d05c
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-16 17:59:21 +0100

    feat(site): adjust max width for third party auth buttons

--- services/site/src/components/user/ThirdPartyAuthLink.tsx
+++ services/site/src/components/user/ThirdPartyAuthLink.tsx
@@ -9,7 +9,7 @@ const ThirdPartyAuthLink: React.FC<ThirdPartyAuthLinkProps> = ({ href, children 
   <a
     href={href}
     className={clsx(
-      "px-8 py-4 mb-4 block w-full text-center align-middle text-light font-normal leading-none rounded border border-white",
+      "px-8 py-4 mb-4 block w-full max-w-full text-center align-middle text-light font-normal leading-none rounded border border-white",
       "transition duration-300 group",
       "hover:bg-white hover:text-primary",
     )}
