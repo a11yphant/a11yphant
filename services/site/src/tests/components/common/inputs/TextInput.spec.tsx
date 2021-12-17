d7fa720c17bead3a24e10cd49905afab28775dec
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-17 11:00:09 +0100

    tests(site): extract label to variable

--- services/site/src/tests/components/common/inputs/TextInput.spec.tsx
+++ services/site/src/tests/components/common/inputs/TextInput.spec.tsx
@@ -3,55 +3,57 @@ import TextInput from "app/components/common/inputs/TextInput";
 import React from "react";
 
 describe("text input", () => {
+  const labelText = "label";
   it("renders the label", () => {
-    render(<TextInput label="label" />);
+    render(<TextInput label={labelText} />);
 
-    expect(screen.getByLabelText("label")).toBeInTheDocument();
+    expect(screen.getByLabelText(labelText)).toBeInTheDocument();
   });
 
   it("renders the value", () => {
-    render(<TextInput label="label" value="value" />);
+    render(<TextInput label={labelText} value="value" />);
 
-    expect(screen.getByLabelText("label")).toHaveValue("value");
+    expect(screen.getByLabelText(labelText)).toHaveValue("value");
   });
 
   it("renders a input of the passed type", () => {
-    render(<TextInput label="label" type="text" />);
+    const type = "text";
+    render(<TextInput label={labelText} type={type} />);
 
-    expect(screen.getByLabelText("label")).toHaveAttribute("type", "text");
+    expect(screen.getByLabelText(labelText)).toHaveAttribute("type", type);
   });
 
   it("renders the placeholder", () => {
-    render(<TextInput label="label" placeholder="placeholder" />);
+    render(<TextInput label={labelText} placeholder="placeholder" />);
 
-    expect(screen.getByLabelText("label")).toHaveAttribute("placeholder", "placeholder");
+    expect(screen.getByLabelText(labelText)).toHaveAttribute("placeholder", "placeholder");
   });
 
   it("calls the onChange handler when the input changes", async () => {
     const onChange = jest.fn();
-    render(<TextInput label="label" onChange={onChange} />);
+    render(<TextInput label={labelText} onChange={onChange} />);
 
-    const input = screen.getByLabelText("label");
+    const input = screen.getByLabelText(labelText);
     fireEvent.change(input, { target: { value: "value" } });
 
     expect(onChange).toHaveBeenCalled();
   });
 
   it("renders the helper text", () => {
-    render(<TextInput label="label" helperText="helper text" />);
+    render(<TextInput label={labelText} helperText="helper text" />);
 
     expect(screen.getByText("helper text")).toBeInTheDocument();
   });
 
   it("renders the input in the error state", () => {
-    render(<TextInput label="label" helperText="helper text" error />);
+    render(<TextInput label={labelText} helperText="helper text" error />);
 
     expect(screen.getByRole("textbox")).toHaveAttribute("aria-invalid", "true");
   });
 
   it("passes the input ref to the input", () => {
     const ref = React.createRef<HTMLInputElement>();
-    render(<TextInput label="label" inputRef={ref} />);
+    render(<TextInput label={labelText} inputRef={ref} />);
 
     expect(ref.current).toBeInTheDocument();
   });
