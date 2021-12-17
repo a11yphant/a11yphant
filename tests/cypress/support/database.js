8d9aaa231573620fd0fecfed91f0950113404004
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2021-12-17 09:05:24 +0100

    feat(cypress): read environment variable correctly

--- tests/cypress/support/database.js
+++ tests/cypress/support/database.js
@@ -1,6 +1,5 @@
 export function resetDatabase() {
-    cy.log(process.env)
-    if (process.env.CI_PROJECT_ID) {
+    if (Cypress.env('CI_PROJECT_ID')) {
         cy.exec('npm run db:reset:ci');
     } else {
         cy.exec('npm run db:reset');
@@ -8,7 +7,7 @@ export function resetDatabase() {
 }
 
 export function createDatabaseDump() {
-    if (process.env.CI_PROJECT_ID) {
+    if (Cypress.env('CI_PROJECT_ID')) {
         cy.exec('npm run db-dump:update:ci');
     } else {
         cy.exec('npm run db-dump:update');
@@ -16,7 +15,7 @@ export function createDatabaseDump() {
 }
 
 export function importDatabaseDump() {
-    if (process.env.CI_PROJECT_ID) {
+    if (Cypress.env('CI_PROJECT_ID')) {
         cy.exec('npm run db-dump:import:ci');
     } else {
         cy.exec('npm run db-dump:import');
