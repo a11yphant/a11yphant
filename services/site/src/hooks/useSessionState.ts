74827bc8e2b781acb8850e868a19378034f67dac
Thomas Dax <thomas.dax@vivid-planet.com>
2021-12-21 14:35:14 +0100

    feat(useSessionState): add useSessionState hook

---
+++ services/site/src/hooks/useSessionState.ts
@@ -0,0 +1,43 @@
+import React from "react";
+
+// https://stackoverflow.com/questions/24613955/is-there-a-type-in-typescript-for-anything-except-functions#answer-48045023
+type NoFunctionValue = boolean | string | number | null | undefined | NoFunctionObject | NoFunctionArray;
+type NoFunctionObject = {
+  [key: string]: NoFunctionValue;
+};
+type NoFunctionArray = Array<NoFunctionValue>;
+
+const useSessionState = <S extends NoFunctionValue = undefined>(
+  key: string,
+  initialValue?: S | (() => S),
+): [S | undefined, React.Dispatch<React.SetStateAction<S | undefined>>] => {
+  const [state, setState] = React.useState<S>();
+
+  React.useEffect(() => {
+    const item = sessionStorage.getItem(key);
+
+    if (item) {
+      console.log("parsed ", JSON.parse(item));
+      setState(JSON.parse(item) as S);
+      return;
+    }
+
+    if (initialValue) {
+      setState(initialValue instanceof Function ? initialValue() : initialValue);
+      return;
+    }
+
+    setState(undefined);
+  }, []);
+
+  React.useEffect(() => {
+    if (state) {
+      const serializedState = JSON.stringify(state);
+      sessionStorage.setItem(key, serializedState);
+    }
+  }, [key, state]);
+
+  return [state, setState];
+};
+
+export default useSessionState;
