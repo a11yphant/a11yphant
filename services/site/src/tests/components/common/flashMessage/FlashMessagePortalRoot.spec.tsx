34379a7c733bd9ce5c9e018a4fbb726c2ff013e6
Thomas Dax <thomas.dax@vivid-planet.com>
2021-12-21 16:11:57 +0100

    test(FlashMessagePortalRoot): add FlashMessageContextProvider wrapper

--- services/site/src/tests/components/common/flashMessage/FlashMessagePortalRoot.spec.tsx
+++ services/site/src/tests/components/common/flashMessage/FlashMessagePortalRoot.spec.tsx
@@ -1,9 +1,10 @@
 import { queryByAttribute, render } from "@testing-library/react";
+import { FlashMessageContextProvider } from "app/components/common/flashMessage/FlashMessageContext";
 import { FLASH_MESSAGE_PORTAL_ROOT_ID, FlashMessagePortalRoot } from "app/components/common/flashMessage/FlashMessagePortalRoot";
 
 describe("FlashMessagePortalRoot", () => {
   it("renders div with id = FLASH_MESSAGE_PORTAL_ROOT_ID", () => {
-    const { container } = render(<FlashMessagePortalRoot />);
+    const { container } = render(<FlashMessagePortalRoot />, { wrapper: FlashMessageContextProvider });
 
     expect(queryByAttribute("id", container, FLASH_MESSAGE_PORTAL_ROOT_ID));
   });
