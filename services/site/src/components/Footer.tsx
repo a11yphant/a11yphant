9d9ab950f024c2c73e6f30b34673648f6418eee1
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-12-22 00:34:43 +0100

    fix(footer): focus style

--- services/site/src/components/Footer.tsx
+++ services/site/src/components/Footer.tsx
@@ -7,11 +7,11 @@ const Footer: React.FunctionComponent = () => (
     <div className={clsx("md:flex md:flex-row md:justify-between", "2xl:mx-24")}>
       <nav className={clsx("mb-4", "md:mb-0")} aria-label="Footer Navigation">
         <ul className={clsx("mb-0")}>
-          <li className={clsx("mb-0 pt-4 pb-4", "md:pt-0 md:pb-0 md:inline-block md:mr-16")}>
+          <li className={clsx("mb-0 -ml-3 pt-4 pb-4", "md:pt-0 md:pb-0 md:inline-block md:mr-16")}>
             <Link href="/imprint">
               <a
                 className={clsx(
-                  "py-3 pr-3 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
+                  "py-3 px-3 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
                   "transition-colors duration-300",
                   "hover:text-primary-light hover:decoration-primary-light",
                   "focus-rounded-instead-of-underline",
