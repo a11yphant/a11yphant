991850ea7e020c73f782dd266138ae5c6aa06b6c
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-12-22 18:05:19 +0100

    add(FullScreenLayout): possibility to add a footer

--- services/site/src/components/layouts/FullScreenLayout.tsx
+++ services/site/src/components/layouts/FullScreenLayout.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 interface FullScreenLayoutProps {
   header: JSX.Element;
+  footer?: JSX.Element;
 }
 
-const FullScreenLayout: React.FC<FullScreenLayoutProps> = ({ children, header }) => (
+const FullScreenLayout: React.FC<FullScreenLayoutProps> = ({ children, header, footer }) => (
   <div className={clsx("h-screen max-h-screen flex flex-col")}>
     <div className={clsx("shrink-0")}>{header}</div>
     <div className={clsx("flex-auto overflow-hidden")}>{children}</div>
+    <div className={clsx("shrink-0")}>{footer}</div>
   </div>
 );
 
