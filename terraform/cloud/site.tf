b8f14a36d998c84bfbeb1143f43bb08eb90487f5
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2022-01-31 13:39:23 +0100

    chore(tf): match example

--- terraform/cloud/site.tf
+++ terraform/cloud/site.tf
@@ -100,7 +100,7 @@ resource "aws_apigatewayv2_route" "api_default_route" {
 resource "aws_apigatewayv2_integration" "site_lambda_integration" {
   api_id                 = aws_apigatewayv2_api.site_http_api.id
   integration_type       = "AWS"
-  integration_method     = "ANY"
+  integration_method     = "POST"
   integration_uri        = aws_lambda_alias.site_latest.invoke_arn
   payload_format_version = "2.0"
 }
