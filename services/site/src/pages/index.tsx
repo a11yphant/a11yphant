ca6d9d72e08837328046fd75c2281f23f066841c
Daniela Kubesch <daniela.kubesch@outlook.com>
2022-04-21 14:10:52 +0200

    refactor: twitter meta tags

--- services/site/src/pages/index.tsx
+++ services/site/src/pages/index.tsx
@@ -70,14 +70,15 @@ const Home: React.VoidFunctionComponent<HomeProps> = ({ fmType }) => {
         />
         <meta property="og:locale" content="en" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.a11yphant.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/images/mockups-social-media.jpg" />
+        <meta property="og:url" content="https://a11yphant.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@a11yphant" />
         <meta name="twitter:title" content="a11yphant" />
         <meta
-          name="twitter:description"
+          property="twitter:description"
           content="a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes."
         />
+        <meta name="twitter:image" content="/images/mockups-social-media.jpg" />
         <meta name="theme-color" content="#121212" media="(prefers-color-scheme: dark)" />
         <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
       </Head>
