038ebed25187e14fbe8134f13688bd56066e8596
Daniela Kubesch <daniela.kubesch@outlook.com>
2022-04-21 17:39:12 +0200

    add(footer): product hunt badge

--- services/site/src/components/Footer.tsx
+++ services/site/src/components/Footer.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 const Footer: React.FunctionComponent = () => (
   <footer className={clsx("max-w-screen-3xl mx-8 mt-10 mb-20", "sm:mx-12 sm:mt-20 sm:mb-12", "md:mt-28", "lg:mx-24", "2xl:mx-auto")}>
-    <div className={clsx("lg:flex lg:flex-row lg:justify-between", "2xl:mx-24")}>
-      <nav className={clsx("mb-2 flex items-center", "lg:mb-0")} aria-label="Footer">
-        <ul className={clsx("mb-0")}>
-          <li className={clsx("mb-0 -ml-3 pt-4 pb-4", "lg:pt-0 lg:pb-0 lg:inline-block lg:mr-12")}>
+    <div className={clsx("xl:flex xl:flex-row xl:justify-between", "2xl:mx-24")}>
+      <nav className={clsx("mb-2 flex items-center justify-start", "sm:justify-center", "xl:justify-start xl:mb-0")} aria-label="Footer">
+        <ul className={clsx("mb-0 flex")}>
+          <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:inline-block xl:mr-4")}>
             <Link href="/imprint">
               <a
                 className={clsx(
-                  "py-3 px-3 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
+                  "py-3 px-4 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
                   "transition-colors duration-300",
                   "hover:text-primary-light hover:decoration-primary-light",
                   "focus-rounded-instead-of-underline",
@@ -21,11 +21,11 @@ const Footer: React.FunctionComponent = () => (
               </a>
             </Link>
           </li>
-          <li className={clsx("mb-0 -ml-3 pt-4 pb-4 inline-block", "lg:ml-0 lg:pt-0 lg:pb-0 lg:mr-12")}>
+          <li className={clsx("mb-0 -ml-3 pt-4 pb-4 inline-block", "xl:ml-0 xl:pt-0 xl:pb-0 xl:mr-4")}>
             <Link href="/privacy-policy">
               <a
                 className={clsx(
-                  "py-3 px-6 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
+                  "py-3 px-8 text-light font-sans font-normal border-none underline decoration-transparent underline-offset-4 decoration-2",
                   "transition-colors duration-300",
                   "hover:text-primary-light hover:decoration-primary-light",
                   "focus-rounded-instead-of-underline",
@@ -37,9 +37,9 @@ const Footer: React.FunctionComponent = () => (
           </li>
         </ul>
       </nav>
-      <nav className={clsx("mb-6", "lg:mb-0")} aria-label="Social Links and Sponsors">
-        <ul className={clsx("mb-0 flex items-center")}>
-          <li className={clsx("mb-0 -ml-3 inline-block", "lg:ml-0 lg:mr-16")}>
+      <nav className={clsx("mb-6", "xl:mb-0")} aria-label="Social Links and Sponsors">
+        <ul className={clsx("mb-0 flex items-center justify-start", "sm:justify-center", "xl:justify-start")}>
+          <li className={clsx("mb-0 -ml-3 mr-8 inline-block", "xl:ml-0 xl:mr-8")}>
             <Link href="https://splitbee.io/?ref=badge">
               <a className={clsx("group")} target={"_blank"} rel="noopener noreferrer nofollow">
                 <span className="sr-only">Splitbee Analytics (opens in a new tab)</span>
@@ -56,9 +56,29 @@ const Footer: React.FunctionComponent = () => (
               </a>
             </Link>
           </li>
+          <li className={clsx("mb-0 -ml-3 inline-block", "xl:ml-0 xl:mr-8")}>
+            <Link href="https://www.producthunt.com/posts/a11yphant?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-a11yphant">
+              <a className={clsx("group")} target={"_blank"} rel="noopener noreferrer nofollow">
+                <span className="sr-only">Product Hunt campaign of a11yphant (opens in a new tab)</span>
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=328460&theme=dark"
+                  alt="Featured on Product Hunt badge"
+                  // style="width: 250px; height: 54px;"
+                  width="250"
+                  height="54"
+                  className={clsx(
+                    "border-transparent border-[3px] rounded-lg",
+                    "transition-colors duration-300",
+                    "group-hover:border-light",
+                    "group-focus:border-light",
+                  )}
+                />
+              </a>
+            </Link>
+          </li>
         </ul>
       </nav>
-      <p className={clsx("mb-0 flex items-center")}>
+      <p className={clsx("mb-0 flex items-center max-w-none justify-start", "sm:justify-center", "xl:justify-start")}>
         Made with
         <span aria-label="love" role="img" className="px-2">
           💜
