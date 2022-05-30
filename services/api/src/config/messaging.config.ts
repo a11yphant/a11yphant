bbd878414cb8a4ce8d06dc75e86e845448929c91
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2022-05-30 18:46:35 +0200

    feat(api): update messaging env variables

--- services/api/src/config/messaging.config.ts
+++ services/api/src/config/messaging.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("messaging", () => ({
-  "rabbitmq-url": process.env.SUBMISSION_CHECKER_MESSAGING_RABBITMQ_URL,
-  "consume-queue-name": process.env.SUBMISSION_CHECKER_MESSAGING_PUBLISH_QUEUE_NAME,
-  "publish-queue-name": process.env.SUBMISSION_CHECKER_MESSAGING_CONSUME_QUEUE_NAME,
+  "rabbitmq-url": process.env.API_MESSAGING_RABBITMQ_URL,
+  "consume-queue-name": process.env.API_MESSAGING_CONSUME_QUEUE_NAME,
+  "publish-queue-name": process.env.API_MESSAGING_PUBLISH_QUEUE_NAME,
 }));
