7e68e62319f22b61a0efd7588ff6ed6182da42e3
Thomas Dax <thomas.dax@vivid-planet.com>
2023-03-10 14:43:10 +0100

    feat(OAuthLoginFailedMessage): add error message for when the OAuth login fails

--- services/site/src/components/common/flashMessage/messages/getFlashMessage.tsx
+++ services/site/src/components/common/flashMessage/messages/getFlashMessage.tsx
@@ -1,12 +1,14 @@
 import { EmailConfirmationFailedMessage } from "app/components/common/flashMessage/messages/EmailConfirmationFailedMessage";
 import { EmailConfirmationSuccessfulMessage } from "app/components/common/flashMessage/messages/EmailConfirmationSuccessfulMessage";
 import { HintReminderMessage } from "app/components/common/flashMessage/messages/HintReminderMessage";
+import { OAuthLoginFailedMessage } from "app/components/common/flashMessage/messages/OAuthLoginFailedMessage";
 import React from "react";
 
 export enum FlashMessageEnum {
   HINT_REMINDER = "hint-reminder",
   EMAIL_CONFIRMATION_FAILED = "email-confirmation-failed",
   EMAIL_CONFIRMATION_SUCCESSFUL = "email-confirmation-successful",
+  OAUTH_LOGIN_FAILED = "oauth-login-failed",
 }
 
 type FlashMessages = {
@@ -17,6 +19,7 @@ const flashMessages: FlashMessages = {
   [FlashMessageEnum.HINT_REMINDER]: <HintReminderMessage />,
   [FlashMessageEnum.EMAIL_CONFIRMATION_FAILED]: <EmailConfirmationFailedMessage />,
   [FlashMessageEnum.EMAIL_CONFIRMATION_SUCCESSFUL]: <EmailConfirmationSuccessfulMessage />,
+  [FlashMessageEnum.OAUTH_LOGIN_FAILED]: <OAuthLoginFailedMessage />,
 };
 
 export const getFlashMessage = (flashMessageEnum: FlashMessageEnum): React.ReactElement => {
