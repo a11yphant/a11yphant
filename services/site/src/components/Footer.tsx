62a60c3fc9bbfee7bab769131b41561cf9c1811d
Daniela Kubesch <daniela.kubesch@outlook.com>
2023-03-10 12:37:30 +0100

    add(Footer): responsiveness

--- services/site/src/components/Footer.tsx
+++ services/site/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 const Footer: React.FunctionComponent = () => (
-  <footer className={clsx("max-w-screen-3xl mx-8 mt-10 mb-20", "sm:mx-12 sm:mt-20 sm:mb-7", "md:mt-28", "lg:mx-24", "2xl:mx-auto")}>
+  <footer className={clsx("max-w-screen-3xl mx-8 mt-10 mb-16", "sm:mx-12 sm:mt-20 sm:mb-7", "md:mt-28", "lg:mx-24", "2xl:mx-auto")}>
     <div className={clsx("xl:flex xl:flex-row xl:justify-between", "2xl:mx-24")}>
       <div className="flex flex-col justify-start">
         <p className={clsx("h5 mb-2 max-w-none")}>The easy way to learn web accessibility</p>
@@ -17,11 +17,11 @@ const Footer: React.FunctionComponent = () => (
         <p className={clsx("mb-0 mt-5 max-w-none")}>© 2022 - {new Date().getFullYear()}, a11yphant.</p>
       </div>
 
-      <nav className={clsx("mb-2 flex items-start justify-start", "sm:justify-center", "xl:justify-start xl:mb-0")} aria-label="Footer">
-        <div className="flex flex-col">
+      <nav className={clsx("mt-10 mb-6 flex flex-col items-start justify-start", "sm:flex-row", "xl:my-0")} aria-label="Footer">
+        <div className={clsx("flex flex-col mb-5", "xl:mb-0")}>
           <h2 className="h6 mb-3 uppercase">Project</h2>
           <ul className={clsx("mb-0 flex flex-col")}>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="/about">
                 <a
                   className={clsx(
@@ -35,7 +35,7 @@ const Footer: React.FunctionComponent = () => (
                 </a>
               </Link>
             </li>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="/press-kit">
                 <a
                   className={clsx(
@@ -49,7 +49,7 @@ const Footer: React.FunctionComponent = () => (
                 </a>
               </Link>
             </li>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="https://github.com/a11yphant/a11yphant">
                 <a
                   target={"_blank"}
@@ -68,10 +68,10 @@ const Footer: React.FunctionComponent = () => (
           </ul>
         </div>
 
-        <div className="flex flex-col mx-9">
+        <div className={clsx("flex flex-col my-5", "sm:my-0 sm:mx-9")}>
           <h2 className="h6 mb-3 uppercase">Social</h2>
           <ul className={clsx("mb-0 flex flex-col")}>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="mailto:info@a11yphant.com">
                 <a
                   className={clsx(
@@ -85,7 +85,7 @@ const Footer: React.FunctionComponent = () => (
                 </a>
               </Link>
             </li>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="https://twitter.com/a11yphant">
                 <a
                   target={"_blank"}
@@ -101,7 +101,7 @@ const Footer: React.FunctionComponent = () => (
                 </a>
               </Link>
             </li>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="https://github.com/a11yphant/a11yphant">
                 <a
                   target={"_blank"}
@@ -120,10 +120,10 @@ const Footer: React.FunctionComponent = () => (
           </ul>
         </div>
 
-        <div className="flex flex-col">
+        <div className={clsx("flex flex-col mt-5", "sm:mt-0")}>
           <h2 className="h6 mb-3 uppercase">Site</h2>
           <ul className={clsx("mb-0 flex flex-col")}>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="/imprint">
                 <a
                   className={clsx(
@@ -137,7 +137,7 @@ const Footer: React.FunctionComponent = () => (
                 </a>
               </Link>
             </li>
-            <li className={clsx("mb-0 -ml-4 pt-4 pb-4", "xl:pt-0 xl:pb-0 xl:flex xl:mr-4")}>
+            <li className={clsx("mb-0 -ml-4 flex")}>
               <Link href="/privacy-policy">
                 <a
                   className={clsx(
@@ -155,7 +155,7 @@ const Footer: React.FunctionComponent = () => (
         </div>
       </nav>
 
-      <div className={clsx("mb-6 flex items-start justify-end", "sm:justify-end", "xl:mb-0 xl:justify-end")}>
+      <div className={clsx("flex items-start justify-start", "xl:justify-end")}>
         <div className={clsx("mb-0 -ml-3 inline-block", "xl:ml-0")}>
           <Link href="https://www.producthunt.com/posts/a11yphant?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-a11yphant">
             <a className={clsx("group")} target={"_blank"} rel="noopener noreferrer nofollow">
