65e5cdc7306f161844c376d6c2e6b367bc20151f
Michael Hinterhofer <michael@hntrh.fr>
2023-03-11 18:13:46 +0100

    chore(api): adapt imports and changes for new faker package

--- services/api/tests/support/factories/models/hint.factory.ts
+++ services/api/tests/support/factories/models/hint.factory.ts
@@ -1,4 +1,4 @@
-import faker from "faker";
+import { faker } from "@faker-js/faker";
 import { Factory } from "rosie";
 
 import { Hint } from "@/challenge/models/hint.model";
--- services/api/tests/unit/challenge/hint.service.spec.ts
+++ services/api/tests/unit/challenge/hint.service.spec.ts
@@ -1,8 +1,8 @@
+import { faker } from "@faker-js/faker";
 import { createMock } from "@golevelup/ts-jest";
 import { Logger } from "@nestjs/common";
 import { Factory, HINT, HintData, TASK, TaskData } from "@tests/support/factories/database";
 import { useDatabase } from "@tests/support/helpers";
-import faker from "faker";
 
 import { HintService } from "@/challenge/hint.service";
 
