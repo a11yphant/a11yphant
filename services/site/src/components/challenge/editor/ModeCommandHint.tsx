cd0237c24a589a7e9e79802d3f53a48f1a515a9b
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-05-18 20:22:14 +0200

    feat(site): fix hydration error in tab mode hint

---
+++ services/site/src/components/challenge/editor/ModeCommandHint.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const CTRL_SHIFT_PLATFORMS = ["Macintosh", "MacIntel", "MacPPC", "Mac68K", "iPhone", "iPad", "iPod"];
+
+function getCommandForTabModeSwitch(): "ctrl + shift" | "ctrl" {
+  // there is no sign of deprecation of navigator.platform in the spec
+  // https://html.spec.whatwg.org/multipage/system-state.html#dom-navigator-platform
+  if (typeof navigator === "undefined" || !navigator.platform) {
+    return "ctrl";
+  }
+
+  if (CTRL_SHIFT_PLATFORMS.includes(navigator.platform)) {
+    return "ctrl + shift";
+  }
+
+  return "ctrl";
+}
+
+const ModeCommandHint: React.FunctionComponent = () => {
+  const [commandForTabModeSwitch, setCommandForTabModeSwitch] = React.useState("");
+
+  React.useEffect(() => {
+    setCommandForTabModeSwitch(getCommandForTabModeSwitch());
+  }, []);
+
+  return (
+    <p className="text-grey-middle">
+      Press <span className="italic text-grey-middle">{commandForTabModeSwitch} + m</span> to use the tab key for site navigation.
+    </p>
+  );
+};
+
+export default ModeCommandHint;
