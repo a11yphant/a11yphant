1bfcaf93e2e1d313a629aeb9466fba7f7b1afbd9
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-06-17 12:05:29 +0200

    Reformat text input classes

--- services/site/src/components/common/inputs/TextInput.tsx
+++ services/site/src/components/common/inputs/TextInput.tsx
@@ -1,4 +1,5 @@
 import Exclamation from "app/components/icons/Exclamation";
+import clsx from "clsx";
 import React from "react";
 
 export interface TextInputProps {
@@ -24,14 +25,28 @@ const TextInput: React.FC<TextInputProps> = ({ label, value, type, inputRef, onC
         <label>
           <input
             ref={inputRef}
-            className="peer h-full w-full rounded border border-t-transparent border-white bg-transparent px-3.5 py-4 !pr-9 font-sans text-sm font-normal text-white outline outline-0 transition-none hover:border-2 placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-4 focus:border-white focus:border-t-transparent focus:outline-0"
+            className={clsx(
+              "peer h-full w-full rounded border border-t-transparent border-white bg-transparent px-3.5 py-4 !pr-9 font-sans text-sm font-normal text-white outline outline-0 transition-none",
+              "hover:border-2",
+              "placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white",
+              "focus:border-4 focus:border-white focus:border-t-transparent focus:outline-0",
+            )}
             placeholder={placeholder || " "}
             onChange={onChange}
             value={value}
             type={type || "text"}
             aria-invalid={error ? "true" : undefined}
           />
-          <div className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl before:border-t before:border-l before:border-white before:transition-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:border-t after:rounded-tr after:border-r after:border-white after:transition-none peer-placeholder-shown:text-[16px] peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:before:border-t-4 peer-focus:before:border-l-4 peer-focus:before:border-white peer-focus:after:border-t-4 peer-focus:after:border-r-4 peer-focus:after:border-white peer-hover:before:border-t-2 peer-hover:before-l-2 peer-hover:after:border-t-2 peer-hover:after:border-r-2">
+          <div
+            className={clsx(
+              "pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all",
+              "before:content[' '] before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl before:border-t before:border-l before:border-white before:transition-none",
+              "after:content[' '] after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:border-t after:rounded-tr after:border-r after:border-white after:transition-none",
+              "peer-placeholder-shown:text-[16px] peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent",
+              "peer-focus:text-[11px] peer-focus:leading-tight peer-focus:before:border-t-4 peer-focus:before:border-l-4 peer-focus:before:border-white peer-focus:after:border-t-4 peer-focus:after:border-r-4 peer-focus:after:border-white",
+              "peer-hover:before:border-t-2 peer-hover:before-l-2 peer-hover:after:border-t-2 peer-hover:after:border-r-2",
+            )}
+          >
             {label}
           </div>
         </label>
