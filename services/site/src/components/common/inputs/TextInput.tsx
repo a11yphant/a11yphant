aac07d52087844bbc8cb1a31ad87a2c14a8b20f4
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-06-17 11:46:38 +0200

    Replace MUI input with material tailwind input

--- services/site/src/components/common/inputs/TextInput.tsx
+++ services/site/src/components/common/inputs/TextInput.tsx
@@ -1,6 +1,4 @@
-import PriorityHigh from "@mui/icons-material/PriorityHigh";
-import { InputAdornment, TextField } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import Exclamation from "app/components/icons/Exclamation";
 import React from "react";
 
 export interface TextInputProps {
@@ -14,71 +12,32 @@ export interface TextInputProps {
   inputRef?: React.Ref<any>;
 }
 
-const StyledTextField = styled(TextField)({
-  "& .MuiOutlinedInput-root": {
-    fontFamily: '"IBM Plex Sans", sans-serif',
-    color: "white",
-    "& fieldset": {
-      borderColor: "white",
-      color: "white",
-    },
-    "&.Mui-error fieldset": {
-      borderColor: "white",
-      color: "white",
-    },
-    "&:hover fieldset": {
-      borderColor: "white",
-      borderWidth: "2px",
-    },
-    "&:focus-within fieldset": {
-      borderColor: "white",
-      borderWidth: "4px",
-    },
-  },
-  "& .MuiInputLabel-outlined": {
-    letterSpacing: "0.025em",
-    color: "white",
-  },
-  "& .MuiInputLabel-outlined.Mui-focused": {
-    color: "white",
-  },
-  "& .MuiInputLabel-outlined.Mui-error": {
-    color: "white",
-  },
-  "& .MuiOutlinedInput-root.Mui-focused fieldset": {
-    color: "white",
-    borderColor: "white",
-
-    "& legend span": {
-      color: "white",
-    },
-  },
-  "& .MuiInputAdornment-root": {
-    color: "white",
-  },
-  "& .MuiFormHelperText-root.Mui-error": {
-    color: "white",
-    fontSize: "1rem",
-  },
-});
-
-const TextInput: React.FC<TextInputProps> = ({ label, value, type, ...props }) => {
+const TextInput: React.FC<TextInputProps> = ({ label, value, type, inputRef, onChange = () => undefined, helperText, error, placeholder }) => {
   return (
-    <StyledTextField
-      label={label}
-      value={value}
-      type={type || "text"}
-      variant="outlined"
-      InputProps={{
-        endAdornment: props.error ? (
-          <InputAdornment position="end">
-            <PriorityHigh />
-          </InputAdornment>
-        ) : undefined,
-      }}
-      {...props}
-      fullWidth
-    />
+    <div className="w-full">
+      <div className="relative h-[50px] w-full min-w-[200px]">
+        {error && (
+          <div className="absolute top-2/4 right-3 grid h-5 w-5 -translate-y-2/4 place-items-center text-white">
+            <Exclamation />
+          </div>
+        )}
+        <label>
+          <input
+            ref={inputRef}
+            className="peer h-full w-full rounded border border-t-transparent border-white bg-transparent px-3.5 py-4 !pr-9 font-sans text-sm font-normal text-white outline outline-0 transition-none hover:border-2 placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-4 focus:border-white focus:border-t-transparent focus:outline-0"
+            placeholder={placeholder || " "}
+            onChange={onChange}
+            value={value}
+            type={type || "text"}
+            aria-invalid={error ? "true" : undefined}
+          />
+          <div className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl before:border-t before:border-l before:border-white before:transition-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:border-t after:rounded-tr after:border-r after:border-white after:transition-none peer-placeholder-shown:text-[16px] peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:before:border-t-4 peer-focus:before:border-l-4 peer-focus:before:border-white peer-focus:after:border-t-4 peer-focus:after:border-r-4 peer-focus:after:border-white peer-hover:before:border-t-2 peer-hover:before-l-2 peer-hover:after:border-t-2 peer-hover:after:border-r-2">
+            {label}
+          </div>
+        </label>
+      </div>
+      <p className="px-3.5 mt-1">{helperText}</p>
+    </div>
   );
 };
 
--- services/site/src/lib/emotion/createEmotionCache.ts
+++
@@ -1,5 +0,0 @@
-import createCache, { EmotionCache } from "@emotion/cache";
-
-export default function createEmotionCache(): EmotionCache {
-  return createCache({ key: "css" });
-}
