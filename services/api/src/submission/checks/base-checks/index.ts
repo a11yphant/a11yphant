058ba45a46ba435401900f24c43f81f217e28df2
Michael Hinterhofer <michael@hntrh.fr>
2023-08-12 19:52:41 +0200

    feat(api): add css value greater than check

--- services/api/src/submission/checks/base-checks/index.ts
+++ services/api/src/submission/checks/base-checks/index.ts
@@ -4,6 +4,7 @@ import { DocumentStartsWithHtml5Doctype } from "./document-starts-with-html5-doc
 import { ElementContainsText } from "./element-contains-text.check";
 import { ElementExists } from "./element-exists.check";
 import { ElementHasCssPropertyValue } from "./element-has-css-property-value.check";
+import { ElementHasCssPropertyValueGreaterThan } from "./element-has-css-property-value-greater-than.check";
 import { ElementNotContainsText } from "./element-not-contains-text.check";
 import { ElementNotExists } from "./element-not-exists.check";
 import { HtmlIsValid } from "./html-is-valid.check";
@@ -15,6 +16,7 @@ export {
   ElementContainsText,
   ElementExists,
   ElementHasCssPropertyValue,
+  ElementHasCssPropertyValueGreaterThan,
   ElementNotContainsText,
   ElementNotExists,
   HtmlIsValid,
