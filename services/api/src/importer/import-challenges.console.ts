e2ab5849c2ae4c31b46bf0ea73b15c3e04f6eb73
Michael Hinterhofer <michael@hntrh.fr>
2023-08-12 18:18:36 +0200

    feat(api): add env flag for challenge importer

--- services/api/src/importer/import-challenges.console.ts
+++ services/api/src/importer/import-challenges.console.ts
@@ -15,6 +15,10 @@ export class ImportChallenges {
     description: "Imports challenges from the filesystem into the database",
   })
   async importChallenges(): Promise<void> {
+    if (!this.config.get<boolean>("api.importer-enabled")) {
+      return;
+    }
+
     await this.importer.importAllFromFolder(this.config.get<string>("api.challenges-location"));
   }
 }
