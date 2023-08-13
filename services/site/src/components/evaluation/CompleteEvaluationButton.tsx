3c4213a18b7375d6b875a8ac98285e3f27c493f6
Daniela Kubesch <daniela.kubesch@outlook.com>
2023-08-13 20:01:04 +0200

    Update services/site/src/components/evaluation/CompleteEvaluationButton.tsx
    Co-authored-by: Thomas Dax <thomas.dax@vivid-planet.com>

--- services/site/src/components/evaluation/CompleteEvaluationButton.tsx
+++ services/site/src/components/evaluation/CompleteEvaluationButton.tsx
@@ -41,7 +41,7 @@ export const CompleteEvaluationButton = ({
             router.back();
           }
         }}
-        className={clsx(disabled && "opacity-50 cursor-not-allowed hover:bg-primary hover:border-primary", className, "ml-auto self-end")}
+        className={clsx(disabled && "opacity-50 cursor-not-allowed hover:bg-primary hover:border-primary", "ml-auto self-end", className)}
         loading={loadingAnimation}
         srTextLoading="The request is being processed."
       >
