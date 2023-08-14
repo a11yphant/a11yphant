b292dba4be60d6faa39962fb88d9ae7a779cab1b
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-14 09:58:49 +0200

    test(ColorContrastGreaterThanOrEqual): remove commented tests

--- services/api/tests/unit/submission/checks/color-contrast-greater-than-or-equal.check.spec.ts
+++ services/api/tests/unit/submission/checks/color-contrast-greater-than-or-equal.check.spec.ts
@@ -156,25 +156,4 @@ describe("color-contrast-greater-than-or-equal", () => {
     expect(result).toHaveProperty("id", rule.id);
     expect(result).toHaveProperty("status", "error");
   });
-
-  // it("errors if the language is not specified", async () => {
-  //   submission.html = `<!doctype html><html></html>`;
-  //   const check = new DocumentLanguageIsSpecified(createMock<Logger>());
-  //
-  //   const result = await check.run(submission, rule);
-  //
-  //   expect(result).toHaveProperty("id", rule.id);
-  //   expect(result).toHaveProperty("status", "failed");
-  // });
-  //
-  // it("errors if the language is missing on the rule", async () => {
-  //   const rule = createRule({ key: "document-language-is-specified", options: {} });
-  //   submission.html = `<!doctype html><html lang="en"></html>`;
-  //   const check = new DocumentLanguageIsSpecified(createMock<Logger>());
-  //
-  //   const result = await check.run(submission, rule);
-  //
-  //   expect(result).toHaveProperty("id", rule.id);
-  //   expect(result).toHaveProperty("status", "error");
-  // });
 });
