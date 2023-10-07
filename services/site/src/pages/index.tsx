5d678e9ddb73190291bbbb46a28daaed89547aca
Daniela Kubesch <daniela.kubesch@outlook.com>
2023-10-07 14:40:55 +0200

    add: new components

--- services/site/src/pages/index.tsx
+++ services/site/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { useFlashMessageApi } from "app/components/common/flashMessage/FlashMessageContext";
 import { FlashMessageEnum, getFlashMessage } from "app/components/common/flashMessage/messages/getFlashMessage";
 import Footer from "app/components/Footer";
-import ChallengeHeader from "app/components/homepage/ChallengeHeader";
 import ChallengeList from "app/components/homepage/ChallengeList";
 import { ChallengeModal } from "app/components/homepage/challengeModal/ChallengeModal";
 import Hero from "app/components/homepage/Hero";
-import Legend from "app/components/homepage/Legend";
-import InTextLink from "app/components/links/InTextLink";
+import SignUpSection from "app/components/homepage/SignUpSection";
 import Navigation from "app/components/Navigation";
 import { ChallengeDifficulty, ChallengesDocument, ChallengeStatus, useChallengesQuery } from "app/generated/graphql";
 import { useCurrentUser } from "app/hooks/useCurrentUser";
@@ -55,10 +53,10 @@ const Home: React.VoidFunctionComponent<HomeProps> = ({ fmType }) => {
   return (
     <>
       <Head>
-        <title>a11yphant</title>
+        <title>a11yphant - The easy way to learn web accessibility</title>
         <meta
           name="description"
-          content="a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes."
+          content="a11yphant teaches the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes. Completely free."
         />
         {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://a11yphant.com" />
@@ -67,7 +65,7 @@ const Home: React.VoidFunctionComponent<HomeProps> = ({ fmType }) => {
         <meta property="og:title" content="a11yphant" />
         <meta
           property="og:description"
-          content="a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes."
+          content="a11yphant teaches the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes. Completely free."
         />
         <meta property="og:image" content="https://a11yphant.com/images/SEO/mockups-social-media.jpg" />
         <meta
@@ -80,68 +78,29 @@ const Home: React.VoidFunctionComponent<HomeProps> = ({ fmType }) => {
         <meta name="twitter:title" content="a11yphant" />
         <meta
           property="twitter:description"
-          content="a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes."
+          content="a11yphant teaches the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes. Completely free."
         />
         <meta name="twitter:image" content="https://a11yphant.com/images/SEO/mockups-social-media.jpg" />
         {/* <!-- General Meta Tags --> */}
         <meta name="theme-color" content="#121212" media="(prefers-color-scheme: dark)" />
         <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
       </Head>
-      <Navigation displayBreadcrumbs />
-      <main>
-        <h1 className={clsx("sr-only")} aria-label="Allyphant">
-          a11yphant
-        </h1>
-        <div className={clsx("w-full h-full")}>
-          {!currentUser?.isRegistered && <Hero />}
-          <section
-            id="challenges"
-            className={clsx("max-w-screen-3xl pt-10 mx-8 mt-22 mb-4", "sm:mx-12 sm:mt-18 sm:mb-12", "md:mx-24 md:mb-24", "2xl:mx-auto")}
-          >
-            <ChallengeHeader className={clsx("2xl:mx-24")} userLoggedIn={currentUser?.isRegistered} />
+      <Navigation />
+      <main className="bg-texture bg-repeat-y bg-contain bg-origin-border bg-top">
+        <div className={clsx("h-full max-w-screen-3xl px-8", "sm:px-12", "md:px-24", "xl:px-24", "2xl:mx-auto")}>
+          <Hero />
+          {!currentUser?.isRegistered && <SignUpSection />}
 
-            <Legend className={clsx("2xl:mx-24")} />
-            <p className={clsx("mb-12 text-grey-middle", "2xl:mx-24")}>
-              <span className="sr-only">Disclaimer:</span>
-              <span aria-hidden="true" className="text-xl text-grey-middle">
-                *
-              </span>{" "}
-              Currently, we only offer easy challenges. However, our small team is dedicated to producing more content for you. We will announce new
-              challenges on our{" "}
-              <InTextLink
-                href="https://twitter.com/a11yphant"
-                opensInNewTab
-                overrideClassName
-                className="text-grey-middle font-sans font-normal border-grey-middle
-        transition-colors duration-300
-        hover:text-primary-light hover:border-transparent
-        focus-rounded-instead-of-underline"
-              >
-                Twitter account
-              </InTextLink>
-              . <br />
-            </p>
-
-            {dataChallengesInProgress?.challenges.length > 0 && (
-              <ChallengeList
-                className={clsx("2xl:mx-24")}
-                heading={"Continue where you left"}
-                challenges={dataChallengesInProgress.challenges}
-                displayCompleted={false}
-              />
-            )}
-
-            {easyChallenges.length > 0 && (
-              <ChallengeList
-                className={clsx("2xl:mx-24")}
-                heading={dataChallengesInProgress?.challenges.length === 0 ? "All challenges" : "Other challenges"}
-                challenges={easyChallenges}
-              />
-            )}
-          </section>
+          {easyChallenges.length > 0 && (
+            <ChallengeList
+              className={clsx("2xl:mx-24")}
+              heading={dataChallengesInProgress?.challenges.length === 0 ? "All challenges" : "Other challenges"}
+              challenges={easyChallenges}
+            />
+          )}
         </div>
-        <ChallengeModal open={!!router.query.challenge} onClose={onCloseModal} challengeSlug={router.query.challenge as string} />
       </main>
+      <ChallengeModal open={!!router.query.challenge} onClose={onCloseModal} challengeSlug={router.query.challenge as string} />
       <Footer />
     </>
   );
