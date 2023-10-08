7208102c83f677e2cb9a847075d082fc61b0c75a
Daniela Kubesch <daniela.kubesch@outlook.com>
2023-10-08 10:52:31 +0200

    refactor: responsiveness

--- services/site/src/components/homepage/TopChallenge.tsx
+++ services/site/src/components/homepage/TopChallenge.tsx
@@ -11,12 +11,12 @@ interface TopChallengeProps {
 }
 const TopChallenge: React.FunctionComponent<React.PropsWithChildren<TopChallengeProps>> = ({ challenge, timesCompleted, userFavorite }) => {
   return (
-    <div className={clsx("px-4 flex flex-col items-center")}>
+    <div className={clsx("px-4 py-4 flex flex-col items-center")}>
       <Card
         isMobileFriendly={challenge.isMobileFriendly}
         isUserFavorite={userFavorite}
         key={challenge.id}
-        className={clsx("mb-12")}
+        className={clsx("mb-6 lg:mb-12", "h-56 sm:h-64")}
         challengeSlug={challenge.slug}
         heading={challenge.name}
         levels={challenge.numberOfLevels}
@@ -24,7 +24,7 @@ const TopChallenge: React.FunctionComponent<React.PropsWithChildren<TopChallenge
         difficulty={challenge.difficulty}
         challengeStatus={challenge.status}
       />
-      <p className={clsx("block py-1.5 px-4 text-light font-medium not-italic uppercase tracking-[0.18rem] text-center text-sm")}>
+      <p className={clsx("block py-1.5 sm:px-4 text-light font-medium not-italic uppercase tracking-[0.18rem] text-center text-sm")}>
         {timesCompleted}+ times completed
       </p>
     </div>
