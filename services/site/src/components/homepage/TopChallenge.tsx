b2c38ae0548f787a5d7ee7647f5c875d040b6426
Daniela Kubesch <daniela.kubesch@outlook.com>
2023-10-08 12:50:09 +0200

    add: change query for top challenges

--- services/site/src/components/homepage/TopChallenge.tsx
+++ services/site/src/components/homepage/TopChallenge.tsx
@@ -13,19 +13,21 @@ interface TopChallengeProps {
 const TopChallenge: React.FunctionComponent<React.PropsWithChildren<TopChallengeProps>> = ({ challenge, timesCompleted, isUserFavorite, isNew }) => {
   return (
     <div className={clsx("px-4 py-4 flex flex-col items-center")}>
-      <Card
-        isMobileFriendly={challenge.isMobileFriendly}
-        isUserFavorite={isUserFavorite}
-        isNew={isNew}
-        key={challenge.id}
-        className={clsx("mb-6 lg:mb-12", "h-56 sm:h-64")}
-        challengeSlug={challenge.slug}
-        heading={challenge.name}
-        levels={challenge.numberOfLevels}
-        finishedLevels={challenge.numberOfFinishedLevels}
-        difficulty={challenge.difficulty}
-        challengeStatus={challenge.status}
-      />
+      {challenge && (
+        <Card
+          isMobileFriendly={challenge.isMobileFriendly}
+          isUserFavorite={isUserFavorite}
+          isNew={isNew}
+          key={challenge.id}
+          className={clsx("mb-6 lg:mb-12", "h-56 sm:h-64")}
+          challengeSlug={challenge.slug}
+          heading={challenge.name}
+          levels={challenge.numberOfLevels}
+          finishedLevels={challenge.numberOfFinishedLevels}
+          difficulty={challenge.difficulty}
+          challengeStatus={challenge.status}
+        />
+      )}
       <p className={clsx("block py-1.5 sm:px-4 text-light font-medium not-italic uppercase tracking-[0.18rem] text-center text-sm")}>
         {timesCompleted}+ times completed
       </p>
