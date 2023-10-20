d473e0daf7ef692fe347227eddf65c30e23717b7
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-10-20 21:21:16 +0200

    refactor(site): inline component for level list item

--- services/site/src/app/challenge/[challengeSlug]/page.tsx
+++ services/site/src/app/challenge/[challengeSlug]/page.tsx
@@ -1,12 +1,12 @@
 import Footer from "app/components/Footer";
+import Check from "app/components/icons/Check";
 import Navigation from "app/components/Navigation";
 import { ChallengeDetailsBySlugDocument, ChallengeDetailsBySlugQuery, LevelStatus } from "app/generated/graphql";
 import { getApolloClient } from "app/lib/apollo-client/rsc";
 import clsx from "clsx";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import sanitizeHtml from "sanitize-html";
-
-import { LevelCard } from "./LevelCard";
 
 type PageProps = {
   params: {
@@ -42,20 +42,28 @@ const Challenge = async ({ params: { challengeSlug } }: PageProps): Promise<Reac
           <h1 className="mb-8 h2 md:h1">{challenge?.name}</h1>
           <p className="prose" dangerouslySetInnerHTML={{ __html: sanitizeHtml(challenge.introduction) }} />
 
-          <div className={clsx("gap-4 pt-8", "grid grid-cols-1 gap-y-4", "xs:grid-cols-2", "lg:grid-cols-3", "2xl:grid-cols-4")}>
+          <ul className="gap-4 pt-8 grid grid-cols-1 gap-y-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
             {challenge.levels.map((level) => {
+              const isFirstUnfinishedLevel = level.id === firstUnfinishedLevel?.id;
               return (
-                <LevelCard
-                  key={level.id}
-                  challengeSlug={challengeSlug}
-                  levelNumber={level.order}
-                  status={level.status}
-                  type={level.__typename || ""}
-                  isFirstUnfinishedLevel={level.id === firstUnfinishedLevel?.id}
-                />
+                <li className="m-0 p-0" key={level.id}>
+                  <Link
+                    href={`/challenge/${challengeSlug}/level/${String(level.order).padStart(2, "0")}`}
+                    className={clsx(
+                      "relative block border border-solid rounded-lg px-4 py-3 w-full h-18",
+                      "hover:bg-primary-dark hover:border-primary-dark",
+                      !isFirstUnfinishedLevel && "border-grey-dark",
+                      isFirstUnfinishedLevel && "border-primary bg-primary",
+                    )}
+                  >
+                    <span className="h3 text-base text-grey block">Level {String(level.order).padStart(2, "0")}</span>
+                    <span className="font-normal text-grey-middle mb-0 block">{level.__typename === "QuizLevel" ? "Quiz" : "Coding"}</span>
+                    {level.status === LevelStatus.Finished && <Check className="h-7 w-10 absolute top-4 right-5 text-light" />}
+                  </Link>
+                </li>
               );
             })}
-          </div>
+          </ul>
         </div>
       </main>
       <Footer />
