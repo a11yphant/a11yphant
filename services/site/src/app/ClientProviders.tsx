e2cc25c40647353c308f89802aba65856192639a
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-10-20 20:25:18 +0200

    feat(site): nprogress for app router

--- services/site/src/app/ClientProviders.tsx
+++ services/site/src/app/ClientProviders.tsx
@@ -6,6 +6,16 @@ import { FlashMessageContextProvider } from "app/components/common/flashMessage/
 import { UserAccountModalProvider } from "app/components/user/UserAccountModalProvider";
 import { createApolloClientSSR } from "app/lib/apollo-client";
 import { ClientConfig, ConfigProvider } from "app/lib/config";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+NProgress.configure({
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const ClientProviders: React.FC<React.PropsWithChildren<{ config: ClientConfig }>> = ({ config, children }) => {
   const { errorDialog, errorDialogApi } = useErrorDialog();
