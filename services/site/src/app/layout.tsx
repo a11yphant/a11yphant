6c7d145add6861275c4c97ba9c99cf0eeabf99a3
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2023-10-20 20:53:46 +0200

    feat(site): add base metadata for pages

--- services/site/src/app/layout.tsx
+++ services/site/src/app/layout.tsx
@@ -6,6 +6,7 @@ import "focus-visible/dist/focus-visible";
 
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 import { getClientConfig } from "app/lib/config/rsc";
+import { Metadata } from "next";
 import Script from "next/script";
 
 import ClientProviders from "./ClientProviders";
@@ -28,3 +29,32 @@ const RootLayout: React.FunctionComponent<React.PropsWithChildren> = ({ children
 };
 
 export default RootLayout;
+
+export const metadata: Metadata = {
+  description:
+    "a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes.",
+  openGraph: {
+    url: "https://a11yphant.com",
+    type: "website",
+    locale: "en",
+    title: "a11yphant",
+    description:
+      "a11yphant is the easy way to learn the basics of web accessibility. Learn step by step by completing short, interactive coding challenges and quizzes.",
+    images: [
+      {
+        url: "https://a11yphant.com/images/SEO/mockups-social-media.jpg",
+        alt: "A coding challenge in a11yphant with an instruction section, a code editor and a preview section to view the code you have just written.",
+      },
+    ],
+  },
+  twitter: {
+    site: "@a11yphant",
+    title: "a11yphant",
+    description: "a11yphant is an interactive online course for web accessibility.",
+    images: [{ url: "https://a11yphant.com/images/SEO/mockups-social-media.jpg" }],
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#121212" },
+    { media: "(prefers-color-scheme: light)", color: "#FFFFFF" },
+  ],
+};
