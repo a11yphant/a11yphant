97633ce8af6d06766a086b5b9f34e9c10445761c
Daniela Kubesch <daniela.kubesch@outlook.com>
2024-05-03 14:11:45 +0200

    fix(TopChallenge): change type

--- services/site/src/components/homepage/TopChallenge.tsx
+++ services/site/src/components/homepage/TopChallenge.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import Card from "../challengePage/Card";
 
 interface TopChallengeProps {
-  challenge: ChallengeOverviewFragment;
+  challenge?: ChallengeOverviewFragment;
   timesCompleted: string;
   isUserFavorite?: boolean;
   isNew?: boolean;
@@ -13,20 +13,22 @@ interface TopChallengeProps {
 const TopChallenge: React.FunctionComponent<React.PropsWithChildren<TopChallengeProps>> = ({ challenge, timesCompleted, isUserFavorite, isNew }) => {
   return (
     <li className={clsx("px-4 py-4 flex flex-col items-center")}>
-      <Card
-        isMobileFriendly={challenge.isMobileFriendly}
-        isUserFavorite={isUserFavorite}
-        isNew={isNew}
-        key={challenge.id}
-        className={clsx("mb-6 lg:mb-12", "h-56 sm:h-64")}
-        challengeSlug={challenge.slug}
-        heading={challenge.name}
-        levels={challenge.numberOfLevels}
-        finishedLevels={challenge.numberOfFinishedLevels}
-        difficulty={challenge.difficulty}
-        challengeStatus={challenge.status}
-        isTopChallenge
-      />
+      {challenge && (
+        <Card
+          isMobileFriendly={challenge.isMobileFriendly}
+          isUserFavorite={isUserFavorite}
+          isNew={isNew}
+          key={challenge.id}
+          className={clsx("mb-6 lg:mb-12", "h-56 sm:h-64")}
+          challengeSlug={challenge.slug}
+          heading={challenge.name}
+          levels={challenge.numberOfLevels}
+          finishedLevels={challenge.numberOfFinishedLevels}
+          difficulty={challenge.difficulty}
+          challengeStatus={challenge.status}
+          isTopChallenge
+        />
+      )}
       <p className={clsx("block py-1.5 sm:px-4 text-light font-medium not-italic uppercase tracking-[0.18rem] text-center text-sm")}>
         {timesCompleted}+ times completed
       </p>
