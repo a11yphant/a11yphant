035c9224ff944d11cbf9f02ea86089675bb1a83f
Daniela Kubesch <daniela.kubesch@outlook.com>
2024-05-03 12:16:35 +0200

    fix: textual description of animation button test

--- services/site/tests/components/buttons/AnimationButton.spec.tsx
+++ services/site/tests/components/buttons/AnimationButton.spec.tsx
@@ -3,13 +3,13 @@ import AnimationButton from "app/components/buttons/AnimationButton";
 import React from "react";
 
 describe("Button Loading", () => {
-  it("renders button with Start text", () => {
+  it("renders button with text 'Stop'", () => {
     render(<AnimationButton animation={true} onClick={jest.fn}></AnimationButton>);
 
     expect(screen.getByRole("button").innerHTML).toContain("Stop");
   });
 
-  it("renders button with Stop text", () => {
+  it("renders button with text 'Start'", () => {
     render(<AnimationButton animation={false} onClick={jest.fn}></AnimationButton>);
 
     expect(screen.getByRole("button").innerHTML).toContain("Start");
