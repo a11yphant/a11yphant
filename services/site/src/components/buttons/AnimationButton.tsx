4467edd97d03e26f48231c69dd264f4aeb53937e
Daniela Kubesch <daniela.kubesch@outlook.com>
2024-05-03 17:58:37 +0200

    Fix: Screen-Reader text for Animation Button

--- services/site/src/components/buttons/AnimationButton.tsx
+++ services/site/src/components/buttons/AnimationButton.tsx
@@ -10,13 +10,7 @@ export interface AnimationButtonProps extends ButtonProps {
 
 const AnimationButton: React.FunctionComponent<React.PropsWithChildren<AnimationButtonProps>> = ({ animation, onClick, ...props }) => {
   return (
-    <Button
-      srText={animation ? "Stop animation" : "Start animation"}
-      overrideClassName
-      animation
-      className={clsx("mb-52 xs:ml-6 md:ml-0 md:mb-0")}
-      onClick={onClick}
-    >
+    <Button srText="animation" overrideClassName animation className={clsx("mb-52 xs:ml-6 md:ml-0 md:mb-0")} onClick={onClick}>
       {animation ? "Stop" : "Start"}
     </Button>
   );
