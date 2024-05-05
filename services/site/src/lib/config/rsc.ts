b9f737bbc267f7e5f04885899c64fb482ea7a81a
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2024-05-05 11:11:07 +0200

    chore: set env variables

--- services/site/src/lib/config/rsc.ts
+++ services/site/src/lib/config/rsc.ts
@@ -7,6 +7,12 @@ export type ClientConfig = {
   baseUrl: string;
 };
 
+function getVercelUrl(suffix: string = ""): string | null {
+  if (!process.env.VERCEL_URL) return null;
+
+  return `https://${process.env.VERCEL_URL}/${suffix}`;
+}
+
 export const getClientConfig = (): ClientConfig => ({
   isDevelopmentMode: process.env.NODE_ENV === "development",
   graphqlEndpointClient: process.env.SITE_GRAPHQL_ENDPOINT_CLIENT || warnMissingEnvVariable("SITE_GRAPHQL_ENDPOINT_CLIENT"),
@@ -29,14 +35,15 @@ export function getConfig(): {
 } {
   return {
     isDevelopmentMode: process.env.NODE_ENV === "development",
-    host: process.env.SITE_HOST || "localhost",
+    host: process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.SITE_HOST || "localhost",
     port: Number(process.env.PORT) || 3001,
-    graphqlEndpointServer: process.env.SITE_GRAPHQL_ENDPOINT_SERVER || warnMissingEnvVariable("SITE_GRAPHQL_ENDPOINT_CLIENT"),
-    graphqlEndpointClient: process.env.SITE_GRAPHQL_ENDPOINT_CLIENT || warnMissingEnvVariable("SITE_GITHUB_LOGIN_ENDPOINT"),
+    graphqlEndpointServer:
+      getVercelUrl("/api/graphql") || process.env.SITE_GRAPHQL_ENDPOINT_SERVER || warnMissingEnvVariable("SITE_GRAPHQL_ENDPOINT_CLIENT"),
+    graphqlEndpointClient: process.env.SITE_GRAPHQL_ENDPOINT_CLIENT || warnMissingEnvVariable("SITE_GRAPHQL_ENDPOINT_CLIENT"),
     githubLoginEndpoint: process.env.SITE_GITHUB_LOGIN_ENDPOINT || warnMissingEnvVariable("SITE_TWITTER_LOGIN_ENDPOINT"),
     twitterLoginEndpoint: process.env.SITE_TWITTER_LOGIN_ENDPOINT || warnMissingEnvVariable("SITE_TWITTER_LOGIN_ENDPOINT"),
     plausibleBaseUrl: process.env.SITE_PLAUSIBLE_BASE_URL,
-    baseUrl: process.env.SITE_BASE_URL || "http://localhost:3001",
+    baseUrl: getVercelUrl() || process.env.SITE_BASE_URL || "http://localhost:3001",
   };
 }
 
