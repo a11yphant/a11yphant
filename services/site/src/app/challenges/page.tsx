db589ce60062761aada2404e54ed9cd4ea393a14
Daniela Kubesch <daniela.kubesch@outlook.com>
2024-05-05 11:08:51 +0200

    add: SEO descriptions for each challenge

--- services/site/src/app/challenges/page.tsx
+++ services/site/src/app/challenges/page.tsx
@@ -86,4 +86,24 @@ export default Challenges;
 
 export const metadata: Metadata = {
   title: "Challenges | a11yphant",
+  description:
+    "Web accessibility topics are split up into short challenges. Solve coding levels and single-choice quizzes, with each level teaching you one thing at a time.",
+  openGraph: {
+    url: "https://a11yphant.com/challenges",
+    title: "Challenges | a11yphant",
+    description:
+      "Web accessibility topics are split up into short challenges. Solve coding levels and single-choice quizzes, with each level teaching you one thing at a time.",
+    images: [
+      {
+        url: "https://a11yphant.com/images/SEO/mockups-social-media.jpg",
+        alt: "A coding challenge in a11yphant with an instruction section, a code editor and a preview section to view the code you have just written.",
+      },
+    ],
+  },
+  twitter: {
+    title: "Challenges | a11yphant",
+    description:
+      "Web accessibility topics are split up into short challenges. Solve coding levels and single-choice quizzes, with each level teaching you one thing at a time.",
+    images: [{ url: "https://a11yphant.com/images/SEO/mockups-social-media.jpg" }],
+  },
 };
