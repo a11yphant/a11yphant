ad882697a0e2a85dec73f99062e4cd452d0534b3
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2024-05-07 20:56:52 +0200

    feat: fix errors in dev after hot reloads

--- services/site/src/api/main.ts
+++ services/site/src/api/main.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as TypeMetadataStorage from "@nestjs/graphql/dist/schema-builder/storages/type-metadata.storage";
 import cookieParser from "cookie-parser";
 
 import { AppModule } from "./app.module";
@@ -10,6 +11,13 @@ export function configureApp(app: INestApplication): void {
 }
 
 export async function bootstrap(): Promise<INestApplication> {
+  // We manually rest the type metadata storage for the graphql module before initializtion
+  // of the application. This needs to be done because NestJS creates an instance of
+  // the storage in the module. This leads to duplicate entries if the application
+  // is instantiated multiple times witout the module cache being cleared in between.
+  // see: https://github.com/nestjs/graphql/blob/7615c80504f8973934614e57c9304f3f663160ed/packages/graphql/lib/schema-builder/storages/type-metadata.storage.ts#L505
+  // @ts-expect-error see above
+  TypeMetadataStorage.TypeMetadataStorage = new TypeMetadataStorage.TypeMetadataStorageHost();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   configureApp(app);
