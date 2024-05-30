f13ce7bb8c6b36d615d9efb3915f5db90b2f49d1
Luca Pircher <lpircher.mmt-m2020@fh-salzburg.ac.at>
2024-05-30 16:18:37 +0200

    feat: rename db env variable

--- services/site/src/api/config/database.config.ts
+++ services/site/src/api/config/database.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("database", () => {
   return {
-    url: process.env.DB_PRISMA_URL || process.env.DB_URL || process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   };
 });
