b1dd9319fb126da86cb90d9a37c25037cc2af778
Michael Hinterhofer <github@hntrh.fr>
2024-09-07 10:12:52 +0200

    Add vercel speedinsights package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "app/styles/custom.scss";
 
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { getClientConfig, getConfig } from "app/lib/config/rsc";
 import { Metadata, Viewport } from "next";
 import { headers } from "next/headers";
@@ -27,6 +28,7 @@ const RootLayout: React.FunctionComponent<React.PropsWithChildren> = ({ children
       <body>
         <ClientProviders config={getClientConfig(host)}>{children}</ClientProviders>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
