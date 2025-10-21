09efb905a1ad75480c4f062f3531ced064cdb197
Luca Pircher <-g>
2025-10-21 20:15:14 +0200

    Disable twitter and github auth

--- src/components/user/LoginBox.tsx
+++ src/components/user/LoginBox.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { useClientConfig } from "app/lib/config";
+// import { useClientConfig } from "app/lib/config";
 import clsx from "clsx";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-import Github from "../icons/Github";
-import XTwitter from "../icons/XTwitter";
+// import Github from "../icons/Github";
+// import XTwitter from "../icons/XTwitter";
 import LoginForm from "./LoginForm";
-import ThirdPartyAuthLink from "./ThirdPartyAuthLink";
+// import ThirdPartyAuthLink from "./ThirdPartyAuthLink";
 import UnderlinedTextButton from "./UnderlinedTextButton";
 import { useUserAccountModalApi } from "./useUserAccountModalApi";
 
 const LoginBox: React.FC = () => {
-  const { githubLoginEndpoint, twitterLoginEndpoint } = useClientConfig();
+  // const { githubLoginEndpoint, twitterLoginEndpoint } = useClientConfig();
   const userAccountModalApi = useUserAccountModalApi();
   const router = useRouter();
 
@@ -26,14 +26,14 @@ const LoginBox: React.FC = () => {
     <>
       <LoginForm onAfterSubmit={onSuccessfulLogin} />
       <div className="mt-4 mb-8">
-        <ThirdPartyAuthLink href={githubLoginEndpoint || "/auth/github"}>
+        {/** <ThirdPartyAuthLink href={githubLoginEndpoint || "/auth/github"}>
           {"Log in via Github"}
           <Github className={clsx("inline-block h-6 -m-2 ml-6 -mt-3 w-auto text-light", "transition duration-300", "group-hover:text-primary")} />
         </ThirdPartyAuthLink>
         <ThirdPartyAuthLink href={twitterLoginEndpoint || "/auth/twitter"}>
           {"Log in via X/Twitter"}
           <XTwitter className={clsx("inline-block h-5 -m-2 ml-4 -mt-3 w-auto text-light", "transition duration-300", "group-hover:text-primary")} />
-        </ThirdPartyAuthLink>
+        </ThirdPartyAuthLink> */}
       </div>
       <UnderlinedTextButton onClick={() => userAccountModalApi.show("reset-password")}>Forgot your password? Reset it here.</UnderlinedTextButton>
       <UnderlinedTextButton className={clsx("mt-4")} onClick={() => userAccountModalApi.show("signup")}>
